17e9fe3cffc115641b92b6f6095cfabfea412f1f
Collin Van Dyck <collinvandyck@gmail.com>
2023-06-25 10:41:06 -0400

    wip

--- chat-0/src/lib.rs
+++ chat-0/src/lib.rs
@@ -1,5 +1,10 @@
-use std::io::Error;
+use std::{
+    io::Error,
+    net::{SocketAddr, TcpListener, TcpStream},
+    thread,
+};
 
+#[derive(Clone)]
 pub struct Server {
     port: usize,
 }
@@ -10,6 +15,19 @@ impl Server {
     }
 
     pub fn run(&self) -> Result<(), Error> {
-        Ok(())
+        let addr = format!("0.0.0.0:{}", self.port);
+        println!("Listening on {}", addr);
+        let listener = TcpListener::bind(addr)?;
+        loop {
+            let (stream, addr) = listener.accept()?;
+            let server = self.clone();
+            thread::spawn(move || {
+                server.handle(stream, addr);
+            });
+        }
+    }
+
+    fn handle(&self, stream: TcpStream, addr: SocketAddr) {
+        println!("Connected to {}", addr);
     }
 }
