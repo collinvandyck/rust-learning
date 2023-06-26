7638540a4d5ab2fa2bebf71540929022c7f5dfb0
Collin Van Dyck <collinvandyck@gmail.com>
2023-06-26 09:18:45 -0400

    wip

--- rust-echo-3/src/main.rs
+++ rust-echo-3/src/main.rs
@@ -1,3 +1,36 @@
+use std::{
+    io::{self},
+    net::{TcpListener, TcpStream},
+    thread,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let s = Server::new(3000);
+    s.start().expect("failure");
+}
+
+type Result<T> = io::Result<T>;
+
+#[derive(Clone)]
+struct Server {
+    port: u32,
+}
+
+impl Server {
+    fn new(port: u32) -> Server {
+        Server { port }
+    }
+
+    fn start(&self) -> Result<()> {
+        let addr = format!("0.0.0.0:{}", self.port);
+        let listener = TcpListener::bind(addr)?;
+        for (id, stream) in listener.incoming().enumerate() {
+            let stream = stream?;
+            let server = self.clone();
+            thread::spawn(move || server.handle(id, stream));
+        }
+        Ok(())
+    }
+
+    fn handle(&self, id: usize, stream: TcpStream) {}
 }
