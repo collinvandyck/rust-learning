93d64f004a17d5b8f90083f7a7954777f937b0cb
Collin Van Dyck <collinvandyck@gmail.com>
2023-06-25 23:14:11 -0400

    wip

--- tokio-mini-redis-0/src/main.rs
+++ tokio-mini-redis-0/src/main.rs
@@ -1,5 +1,26 @@
+use mini_redis::{Connection, Frame};
+use tokio::net::{TcpListener, TcpStream};
+
 #[tokio::main]
 async fn main() {
-    println!("Hi")
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        process(socket).await;
+    }
+}
+
+async fn process(socket: TcpStream) {
+    // allows us to write frames instead of bytes
+    let mut conn = Connection::new(socket);
+
+    // read a frame
+    if let Some(frame) = conn.read_frame().await.unwrap() {
+        println!("Got: {:?}", frame);
+
+        // respond with an error
+        let response = Frame::Error("unimplemented".to_string());
+        conn.write_frame(&response).await.unwrap();
+    }
 }
 
