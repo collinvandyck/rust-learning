269d2b9c2a8e659b93bb109b2449f3968bb2e8be
Collin Van Dyck <collinvandyck@gmail.com>
2023-06-26 16:39:55 -0400

    wip

--- chat-4/src/lib.rs
+++ chat-4/src/lib.rs
@@ -1,5 +1,12 @@
-use std::io::Error;
+use std::{
+    io,
+    net::{TcpListener, TcpStream},
+    sync::mpsc::{self, RecvError, SendError, Sender},
+    thread,
+};
+use thiserror::Error;
 
+#[derive(Clone)]
 pub struct Server {
     port: u32,
 }
@@ -9,7 +16,55 @@ impl Server {
         Self { port }
     }
 
-    pub fn run(&self) -> Result<(), Error> {
+    // the main control thread
+    pub fn run(&self) -> Result<(), ServerError> {
+        let (tx, rx) = mpsc::channel();
+
+        let server = self.clone();
+        let tx1 = tx.clone();
+        thread::spawn(move || server.accept(tx1));
+
+        drop(tx);
+        loop {
+            let Event(msg, sender) = rx.recv()?;
+            dbg!(msg);
+            sender.send(Ok(()))?;
+        }
+    }
+
+    pub fn accept(&self, tx: Sender<Event>) -> Result<(), ServerError> {
+        let addr = format!("0.0.0.0:{}", self.port);
+        let listener = TcpListener::bind(addr)?;
+        for stream in listener.incoming() {
+            let stream = stream?;
+            let (etx, erx) = mpsc::channel();
+            let msg = Message::Stream(stream);
+            let event = Event(msg, etx);
+            tx.send(event)?;
+            let _ = erx.recv()?;
+        }
         Ok(())
     }
+}
+
+#[derive(Error, Debug)]
+pub enum ServerError {
+    #[error("accept failed")]
+    Accept(#[from] io::Error),
+
+    #[error("chan send failure")]
+    SendFailure(#[from] mpsc::SendError<Event>),
+
+    #[error("tx chan closed")]
+    TxClosed(#[from] RecvError),
+
+    #[error("rx chan closed")]
+    RxClosed(#[from] SendError<Result<(), RecvError>>),
+}
+
+pub struct Event(Message, Sender<Result<(), RecvError>>);
+
+#[derive(Debug)]
+pub enum Message {
+    Stream(TcpStream),
 }
