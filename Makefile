76743a18d5e2cf2e698f434d6ab6ad9a08adee81
Collin Van Dyck <collinvandyck@gmail.com>
2023-06-26 08:54:07 -0400

    wip

--- Makefile
+++ Makefile
@@ -1,2 +1,9 @@
 clean:
-	echo "not implemented yet"
+	@for x in `fd -t d -d 1`; do \
+		if [ -d $$x/target ]; then \
+		    echo $$x; \
+			cd $$x; \
+			cargo clean; \
+			cd ..; \
+		fi \
+	done
--- tokio-mini-redis-0/src/bin/client.rs
+++ tokio-mini-redis-0/src/bin/client.rs
@@ -1,10 +1,72 @@
+use bytes::Bytes;
 use mini_redis::{client, Result};
+use tokio::sync::mpsc;
+use tokio::sync::oneshot;
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    let mut client = client::connect("127.0.0.1:6379").await?;
-    client.set("hello", "world".into()).await?;
-    let result = client.get("hello").await?;
-    println!("Got value: {:?}", result);
-    Ok(())
+async fn main() {
+    // create a chan
+    let (tx, mut rx) = mpsc::channel(32);
+
+    let manager = tokio::spawn(async move {
+        let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+        while let Some(cmd) = rx.recv().await {
+            match cmd {
+                Command::Get { key, resp } => {
+                    let res = client.get(&key).await;
+                    let _ = resp.send(res);
+                }
+                Command::Set { key, val, resp } => {
+                    let res = client.set(&key, val).await;
+                    let _ = resp.send(res);
+                }
+            }
+        }
+    });
+
+    let ttx = tx.clone();
+    let t1 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
+        let cmd = Command::Get {
+            key: "foo".to_string(),
+            resp: resp_tx,
+        };
+        ttx.send(cmd).await.unwrap();
+        let res = resp_rx.await;
+        println!("Got: {:?}", res);
+    });
+
+    let ttx = tx.clone();
+    let t2 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
+        let cmd = Command::Set {
+            key: "foo".to_string(),
+            val: "bar".into(),
+            resp: resp_tx,
+        };
+        ttx.send(cmd).await.unwrap();
+        let res = resp_rx.await;
+        println!("Got: {:?}", res);
+    });
+
+    drop(tx);
+
+    t2.await.unwrap();
+    t1.await.unwrap();
+    manager.await.unwrap();
+}
+
+type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
+
+#[derive(Debug)]
+enum Command {
+    Get {
+        key: String,
+        resp: Responder<Option<Bytes>>,
+    },
+    Set {
+        key: String,
+        val: Bytes,
+        resp: Responder<()>,
+    },
 }
