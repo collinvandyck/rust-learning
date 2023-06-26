35499b43056c1b7aeb71052c879423d23fb8fa5e
Collin Van Dyck <collinvandyck@gmail.com>
2023-06-26 00:09:20 -0400

    wip

--- tokio-mini-redis-0/src/bin/client.rs
+++ tokio-mini-redis-0/src/bin/client.rs
@@ -1,1 +1,10 @@
-fn main() {}
+use mini_redis::{client, Result};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut client = client::connect("127.0.0.1:6379").await?;
+    client.set("hello", "world".into()).await?;
+    let result = client.get("hello").await?;
+    println!("Got value: {:?}", result);
+    Ok(())
+}
