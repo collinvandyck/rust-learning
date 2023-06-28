b6b8de1298159ec37a33b76e40aff5af4a44d34b
Collin Van Dyck <collinvandyck@gmail.com>
2023-06-28 12:56:04 -0400

    wip

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -6,6 +6,45 @@ fn main() {
     println!("Hello, world!");
 }
 
+/// Given the row and column of a pixel in the output image, return the
+/// corresponding point on the complex plane.
+///
+/// bounds is a pair giving width/height of the image in pixels
+/// pixel is a (column, row) pair indicating the pixel in that image
+/// upper_left and lower_right are points on the complex plane our image covers
+fn pixel_to_point(
+    bounds: (usize, usize),
+    pixel: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) -> Complex<f64> {
+    let (width, height) = (
+        lower_right.re - upper_left.re,
+        upper_left.im - lower_right.im,
+    );
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64,
+        // we subtract here because the imaginary comonent increases as we go up
+    }
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(
+        pixel_to_point(
+            (100, 200),
+            (25, 175),
+            Complex { re: -1.0, im: 1.0 },
+            Complex { re: 1.0, im: -1.0 }
+        ),
+        Complex {
+            re: -0.5,
+            im: -0.75
+        },
+    )
+}
+
 fn parse_complex(s: &str) -> Option<Complex<f64>> {
     match parse_pair(s, ',') {
         Some((re, im)) => Some(Complex { re, im }),
