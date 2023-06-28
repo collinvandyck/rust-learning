cc1054ea50f56f5dc91ac7c997d7853359857724
Collin Van Dyck <collinvandyck@gmail.com>
2023-06-28 12:50:14 -0400

    wip

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -6,6 +6,25 @@ fn main() {
     println!("Hello, world!");
 }
 
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        Some((re, im)) => Some(Complex { re, im }),
+        None => None,
+    }
+}
+
+#[test]
+fn test_parse_complex() {
+    assert_eq!(
+        parse_complex("1.25,-0.0625"),
+        Some(Complex {
+            re: 1.25,
+            im: -0.0625
+        })
+    );
+    assert_eq!(parse_complex(",-0.0625"), None);
+}
+
 fn parse_pair<T: FromStr>(s: &str, sep: char) -> Option<(T, T)> {
     match s.find(sep) {
         None => None,
