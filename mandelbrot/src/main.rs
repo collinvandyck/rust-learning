7a9d66d8215087f186d62be72919a19e1234a369
Collin Van Dyck <collinvandyck@gmail.com>
2023-06-28 12:43:57 -0400

    wip

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,3 +1,42 @@
+use std::str::FromStr;
+
+use num::Complex;
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn parse_pair<T: FromStr>(s: &str, sep: char) -> Option<(T, T)> {
+    match s.find(sep) {
+        None => None,
+        Some(index) => match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+            (Ok(l), Ok(r)) => Some((l, r)),
+            _ => None,
+        },
+    }
+}
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("", ','), None);
+}
+
+// returns now many times it took to escape. none returned if
+// it never escaped.
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    for i in 0..limit {
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+        z = z * z + c;
+    }
+    None
+}
+
+fn complex_square_add_loop(c: Complex<f64>) {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    loop {
+        z = z * z + c;
+    }
 }
