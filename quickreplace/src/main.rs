4c0c9d1d01a0356368f2c0289efa093bfef41fa4
Collin Van Dyck <collinvandyck@gmail.com>
2023-06-28 13:42:10 -0400

    wip

--- quickreplace/src/main.rs
+++ quickreplace/src/main.rs
@@ -1,3 +1,8 @@
+use std::env;
+
+use text_colorizer::*;
+
+#[derive(Debug)]
 struct Arguments {
     target: String,
     replacment: String,
@@ -5,6 +10,34 @@ struct Arguments {
     output: String,
 }
 
+fn print_usage() {
+    eprintln!(
+        "{} - change occurrences of one string into another",
+        "quickreplace".green()
+    );
+    eprintln!("Usage: quickreplace <target> <replacement> <input> <output>");
+}
+
+fn parse_args() -> Arguments {
+    let args: Vec<String> = env::args().skip(1).collect();
+    if args.len() != 4 {
+        print_usage();
+        eprintln!(
+            "{} wrong number of args. expected 4, got {}",
+            "Error:".red().bold(),
+            args.len()
+        );
+        std::process::exit(1);
+    }
+    Arguments {
+        target: args[0].clone(),
+        replacment: args[1].clone(),
+        filename: args[2].clone(),
+        output: args[3].clone(),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = parse_args();
+    dbg!(args);
 }
