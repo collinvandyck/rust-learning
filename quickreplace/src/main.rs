177d4131183134da70a5f082f17f32c519fa3088
Collin Van Dyck <collinvandyck@gmail.com>
2023-06-28 13:48:13 -0400

    wip

--- quickreplace/src/main.rs
+++ quickreplace/src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs};
 
 use text_colorizer::*;
 
@@ -39,5 +39,30 @@ fn parse_args() -> Arguments {
 
 fn main() {
     let args = parse_args();
-    println!("{:?}", args);
+
+    let data = match fs::read_to_string(&args.filename) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!(
+                "{} failed to read from file '{}': {:?}",
+                "Error:".red().bold(),
+                args.filename,
+                e
+            );
+            std::process::exit(1);
+        }
+    };
+
+    match fs::write(&args.output, &data) {
+        Ok(_) => {}
+        Err(e) => {
+            eprintln!(
+                "{} failed to write to file '{}': {:?}",
+                "Error:".red().bold(),
+                args.filename,
+                e
+            );
+            std::process::exit(1);
+        }
+    }
 }
