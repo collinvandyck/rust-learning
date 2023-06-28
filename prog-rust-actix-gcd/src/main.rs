33f9f405fb44e0f42e30d0c8b4cd6e8c0fd64582
Collin Van Dyck <collinvandyck@gmail.com>
2023-06-28 12:23:11 -0400

    wip

--- prog-rust-actix-gcd/src/main.rs
+++ prog-rust-actix-gcd/src/main.rs
@@ -1,13 +1,24 @@
 use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
 
 fn main() {
-    let server = HttpServer::new(|| App::new().route("/", web::get().to(get_index)));
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(get_index))
+            .route("/gcd", web::post().to(post_gcd))
+    });
     println!("Serving on http://localhost:3000");
     server
         .bind("127.0.0.1:3000")
         .expect("bind error")
         .run()
         .expect("run error");
+}
+
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m: u64,
 }
 
 fn get_index() -> HttpResponse {
@@ -21,4 +32,38 @@ fn get_index() -> HttpResponse {
                 </form>
             "#,
     )
+}
+
+fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.m == 0 || form.n == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("zero not allowed");
+    }
+    let response = format!(
+        "The gcd of {} and {} is <b>{}</b>\n",
+        form.m,
+        form.n,
+        gcd(form.m, form.n)
+    );
+    HttpResponse::Ok().content_type("text/html").body(response)
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let tmp = m;
+            m = n;
+            n = tmp;
+        }
+        m = m % n;
+    }
+    n
+}
+
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11);
 }
