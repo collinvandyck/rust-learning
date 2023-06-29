9e586055ba6a0c574e5a69cc93fa800a898b4241
Collin Van Dyck <collinvandyck@gmail.com>
2023-06-29 08:26:59 -0400

    wip

--- binary-tree-1/src/main.rs
+++ binary-tree-1/src/main.rs
@@ -1,3 +1,34 @@
 fn main() {
-    println!("Hello, world!");
+    let mut tree = Tree::Empty;
+    tree.add(5);
+    dbg!(tree);
+}
+
+#[derive(Debug)]
+enum Tree<T> {
+    Empty,
+    Value(Box<Node<T>>),
+}
+
+impl<T: Ord> Tree<T> {
+    fn add(&mut self, val: T) {
+        match self {
+            Tree::Empty => {
+                let node = Node {
+                    item: val,
+                    left: Tree::Empty,
+                    right: Tree::Empty,
+                };
+                *self = Tree::Value(Box::new(node));
+            }
+            Tree::Value(node) => {}
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Node<T> {
+    item: T,
+    left: Tree<T>,
+    right: Tree<T>,
 }
