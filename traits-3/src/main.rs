0f8b25de868ccc0d0b4dc1f4cdf8789d2b82cee0
Collin Van Dyck <collinvandyck@gmail.com>
2023-06-29 13:07:50 -0400

    wip

---
+++ traits-3/src/main.rs
@@ -0,0 +1,41 @@
+use std::ops::{self, Neg};
+
+fn main() {
+    println!("Hello, world!");
+    let mut f = Bool { val: true };
+    f = dbg!(f);
+    let g = -f;
+    dbg!(g);
+
+    let mut any = Any { val: 5 };
+    any = dbg!(any);
+    let any = -any;
+    dbg!(any);
+}
+
+#[derive(Debug)]
+struct Any<T> {
+    val: T,
+}
+
+impl<T> ops::Neg for Any<T>
+where
+    T: ops::Neg<Output = T>,
+{
+    type Output = Any<T>;
+    fn neg(self) -> Self::Output {
+        Any { val: -self.val }
+    }
+}
+
+#[derive(Debug)]
+struct Bool {
+    val: bool,
+}
+
+impl ops::Neg for Bool {
+    type Output = Bool;
+    fn neg(self) -> Self::Output {
+        Self { val: !self.val }
+    }
+}
