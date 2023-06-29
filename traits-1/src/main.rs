68dfc60973fb579285e0b45cc3f26db71aa85a43
Collin Van Dyck <collinvandyck@gmail.com>
2023-06-29 08:55:21 -0400

    wip

--- traits-1/src/main.rs
+++ traits-1/src/main.rs
@@ -2,7 +2,12 @@ use std::io::Write;
 
 fn main() {
     let mut buf: Vec<u8> = vec![];
+
+    // this is a reference to a trait type, which is a trait object.
+    // it must be a pointer because the size of whatever the Write
+    // implementation cannot be known usually at compile time.
     let writer: &mut dyn Write = &mut buf;
+
     writer.write(b"123").expect("write failure");
     println!("{}", buf.len());
 }
