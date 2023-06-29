cf53ce6d6a9fece91bb055a3e06ac123afdcdb04
Collin Van Dyck <collinvandyck@gmail.com>
2023-06-29 09:10:44 -0400

    wip

--- traits-1/src/main.rs
+++ traits-1/src/main.rs
@@ -1,4 +1,7 @@
-use std::io::Write;
+use std::{
+    fmt::Debug,
+    io::{self, Write},
+};
 
 fn main() {
     let mut buf: Vec<u8> = vec![];
@@ -10,4 +13,24 @@ fn main() {
 
     writer.write(b"123").expect("write failure");
     println!("{}", buf.len());
+
+    say_hello(&mut buf).expect("failed to write to buf");
+    println!("{}", buf.len());
+
+    let mut s = String::from_utf8(buf.clone()).expect("convert to utf8");
+    s = s.trim().to_string();
+    println!("{}", s);
+
+    top_ten(&buf);
+}
+
+fn say_hello<W: Write>(out: &mut W) -> io::Result<()> {
+    out.write_all(b"Hello, world!\n")?;
+    out.flush()
+}
+
+fn top_ten<T: Debug>(vals: &Vec<T>) {
+    for ele in vals.iter().take(10) {
+        dbg!(ele);
+    }
 }
