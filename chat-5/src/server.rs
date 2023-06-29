107d5fff075c952f227435d57ac7d031224eadef
Collin Van Dyck <collinvandyck@gmail.com>
2023-06-29 11:22:19 -0400

    wip

--- chat-5/src/server.rs
+++ chat-5/src/server.rs
@@ -2,7 +2,7 @@ use serde::Serialize;
 use serde_json;
 use std::{
     collections::HashMap,
-    io::{self, BufRead, BufReader, BufWriter},
+    io::{self, BufRead, BufReader, BufWriter, Write},
     net::{TcpListener, TcpStream},
     thread,
 };
@@ -30,9 +30,16 @@ impl Server {
 
     fn handle(&self, mut stream: TcpStream) {
         stream = dbg!(stream);
-        let reader = BufReader::new(stream.try_clone().unwrap());
-        let writer = BufWriter::new(stream);
-        self.to_bs().unwrap();
+        let reader = Box::new(BufReader::new(stream.try_clone().unwrap()));
+        let reader = Reader::new(reader);
+        let writer = Box::new(BufWriter::new(stream));
+        let writer = Writer::new(writer);
+        self.handle_rw(reader, writer).expect("fail")
+    }
+
+    fn handle_rw(&self, mut reader: Reader, mut writer: Writer) -> io::Result<()> {
+        writer.write("hello there!")?;
+        Ok(())
     }
 
     fn to_bs(&self) -> io::Result<()> {
@@ -43,5 +50,38 @@ impl Server {
         m.serialize(&mut serializer)?;
         dbg!(m);
         Ok(())
+    }
+}
+
+struct Writer {
+    w: Box<dyn Write>,
+}
+
+impl Writer {
+    fn new(w: Box<dyn Write>) -> Self {
+        Self { w }
+    }
+
+    fn write(&mut self, val: &str) -> io::Result<()> {
+        self.w.write_all(val.as_bytes())?;
+        self.w.write_all(b"\n")?;
+        self.w.flush()?;
+        Ok(())
+    }
+}
+
+struct Reader {
+    r: Box<dyn BufRead>,
+}
+
+impl Reader {
+    fn new(r: Box<dyn BufRead>) -> Reader {
+        Reader { r }
+    }
+
+    fn read_line(&mut self) -> io::Result<String> {
+        let mut buf = String::new();
+        self.r.read_line(&mut buf)?;
+        Ok(buf.trim().to_string())
     }
 }
