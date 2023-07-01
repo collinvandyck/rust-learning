6b5407009811d3b6e5c4be52a6ef3403cbf984ff
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-01 19:55:38 -0400

    wip

--- iterators-2/src/main.rs
+++ iterators-2/src/main.rs
@@ -2,9 +2,26 @@
 
 fn main() {
     println!("Hello, world!");
+    let range = I32Range { start: 0, end: 10 };
+    for x in range {
+        println!("x: {}", x);
+    }
 }
 
 struct I32Range {
     start: i32,
     end: i32,
+}
+
+impl Iterator for I32Range {
+    type Item = i32;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.start >= self.end {
+            None
+        } else {
+            let result = Some(self.start);
+            self.start += 1;
+            result
+        }
+    }
 }
