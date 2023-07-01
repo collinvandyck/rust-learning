023a01a0187445b05fc305041c5527a4e9838b68
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-01 15:01:05 -0400

    wip

--- iterators-1/src/main.rs
+++ iterators-1/src/main.rs
@@ -1,3 +1,36 @@
 fn main() {
-    println!("Hello, world!");
+    let x = triangle(5);
+    println!("X: {}", x);
+
+    let vals = Values::from(vec![1, 2, 3]);
+    for x in vals {
+        println!("x: {}", x)
+    }
+}
+
+fn triangle(n: i32) -> i32 {
+    (1..=n).fold(0, |sum, item| sum + item)
+}
+
+struct Values<T> {
+    vals: Vec<T>,
+    idx: usize,
+}
+
+impl<T> From<Vec<T>> for Values<T> {
+    fn from(vals: Vec<T>) -> Self {
+        Values { vals, idx: 0 }
+    }
+}
+
+impl<T> Iterator for Values<T>
+where
+    T: Copy,
+{
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        let res = self.vals.get(self.idx);
+        self.idx += 1;
+        res.copied()
+    }
 }
