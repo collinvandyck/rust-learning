c6addf92ab4e4dcd5d210230b32634721053e898
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-01 23:40:27 -0400

    wip

--- io-1/src/main.rs
+++ io-1/src/main.rs
@@ -1,3 +1,28 @@
+use std::io::{self, ErrorKind, Read, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let mut src: Vec<u8> = vec![1, 2, 3, 4, 5];
+    let mut dst: Vec<u8> = Vec::default();
+    copy(&mut src, &mut dst).expect("copy failed");
+}
+
+const DEFAULT_BUF_SIZE: usize = 8 * 1024;
+
+pub fn copy<R, W>(reader: &mut R, writer: &mut W) -> io::Result<u64>
+where
+    R: Read + ?Sized,
+    W: Write + ?Sized,
+{
+    let mut buf = [0; DEFAULT_BUF_SIZE];
+    let mut written = 0;
+    loop {
+        let len = match reader.read(&mut buf) {
+            Ok(0) => return Ok(written),
+            Ok(len) => len,
+            Err(ref e) if e.kind() == ErrorKind::Interrupted => continue,
+            Err(e) => return Err(e),
+        };
+        writer.write_all(&buf[..len])?;
+        written += len as u64;
+    }
 }
