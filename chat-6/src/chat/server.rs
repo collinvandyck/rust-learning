ecb74df2317776a209cae1d7ab588d0be845ff64
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-02 13:10:42 -0400

    wip

--- chat-6/src/chat/server.rs
+++ chat-6/src/chat/server.rs
@@ -1,5 +1,9 @@
 use crate::chat::error::Error;
-use std::net::TcpListener;
+use std::{
+    net::{TcpListener, TcpStream},
+    sync::mpsc::{self, Receiver},
+    thread,
+};
 
 pub struct Server {
     port: u32,
@@ -15,16 +19,28 @@ impl Server {
     }
 
     pub fn serve(&self) -> Result<(), Error> {
+        let (tx, rx) = mpsc::channel();
+        let coord = thread::spawn(move || Self::coordinator(rx));
         let addr = format!("0.0.0.0:{}", self.port);
         let listener = TcpListener::bind(&addr).or_else(|e| Err(Error::BindFailure(e)))?;
         println!("Listening on {}", addr);
         for conn in listener.incoming() {
-            let conn = match conn {
-                Ok(c) => Ok(c),
-                Err(e) => Err(Error::AcceptFailure(e)),
-            }?;
-            dbg!(conn);
+            match conn {
+                Err(e) => return Err(Error::AcceptFailure(e)),
+                Ok(conn) => {
+                    if tx.send(conn).is_err() {
+                        break;
+                    }
+                }
+            }
         }
+        let _ = coord.join();
         Ok(())
+    }
+
+    fn coordinator(rx: Receiver<TcpStream>) {
+        for stream in rx {
+            dbg!(stream);
+        }
     }
 }
