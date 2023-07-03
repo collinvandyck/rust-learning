4d8e3d16ff4e673163107799ef2f4f754c1da077
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-03 15:54:44 -0400

    wip

--- pragprog/dungeoncrawl/src/systems/mod.rs
+++ pragprog/dungeoncrawl/src/systems/mod.rs
@@ -4,6 +4,7 @@ mod collisions;
 mod entity_render;
 mod map_render;
 mod player_input;
+mod random_move;
 
 pub fn build_scheduler() -> Schedule {
     Schedule::builder()
@@ -11,5 +12,6 @@ pub fn build_scheduler() -> Schedule {
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
         .add_system(collisions::collisions_system())
+        .add_system(random_move::random_move_system())
         .build()
 }
