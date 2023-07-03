39bdbf249ba577baaadba95499dfc19d708d80e7
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-02 23:43:23 -0400

    wip

--- pragprog/flappy/src/main.rs
+++ pragprog/flappy/src/main.rs
@@ -2,6 +2,10 @@
 
 use bracket_lib::prelude::*;
 
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0;
+
 struct Player {
     x: i32,
     y: i32,
@@ -80,9 +84,21 @@ impl State {
         }
     }
 
-    fn play(&mut self, _ctx: &mut BTerm) {
-        // TODO: fill in later
-        self.mode = GameMode::End;
+    fn play(&mut self, ctx: &mut BTerm) {
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms;
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.gravity_and_move();
+        }
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+        self.player.render(ctx);
+        ctx.print(0, 0, "Press SPACE to flap.");
+        if self.player.y > SCREEN_HEIGHT {
+            self.mode = GameMode::End;
+        }
     }
 
     fn restart(&mut self) {
