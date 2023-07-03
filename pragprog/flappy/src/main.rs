199ba9dc3eaf9e6496529652c37eefbd227b3b27
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-02 23:32:58 -0400

    wip

--- pragprog/flappy/src/main.rs
+++ pragprog/flappy/src/main.rs
@@ -2,14 +2,53 @@
 
 use bracket_lib::prelude::*;
 
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
+}
+
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Self {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@'));
+    }
+
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+
+    fn flap(&mut self) {
+        self.velocity = -2.0;
+    }
+}
+
 struct State {
+    player: Player,
+    frame_time: f32,
     mode: GameMode,
 }
 
 impl State {
     fn new() -> Self {
         Self {
+            player: Player::new(5, 25),
             mode: GameMode::Menu,
+            frame_time: 0.0,
         }
     }
 
@@ -47,6 +86,8 @@ impl State {
     }
 
     fn restart(&mut self) {
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
         self.mode = GameMode::Playing;
     }
 }
