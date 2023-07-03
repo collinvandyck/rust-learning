43c5b6e414ec97a2f962f75963951163343de16a
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-03 12:53:37 -0400

    wip

--- pragprog/dungeoncrawl/src/main.rs
+++ pragprog/dungeoncrawl/src/main.rs
@@ -44,6 +44,12 @@ fn main() -> BError {
     let ctx = BTermBuilder::simple80x50()
         .with_title("Dungeon Crawler")
         .with_fps_cap(30.0)
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .with_tile_dimensions(32, 32)
+        .with_resource_path("resources/")
+        .with_font("dungeonfont.png", 32, 32)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .build()?;
     main_loop(ctx, State::new())
 }
