5eda037e35e5600554ceb4ba2e639f18735f29c4
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-03 00:59:47 -0400

    wip

--- pragprog/dungeoncrawl/src/main.rs
+++ pragprog/dungeoncrawl/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use bracket_lib::prelude::*;
+
+struct State {}
+
+impl State {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello, world!");
+    }
+}
+
+fn main() -> BError {
+    let ctx = BTermBuilder::simple80x50()
+        .with_title("Dungeon Crawler")
+        .build()?;
+    main_loop(ctx, State::new())
 }
