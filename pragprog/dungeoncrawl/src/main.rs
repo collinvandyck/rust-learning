f05e7e3f888b251849c17668202841e5f78c295d
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-03 12:51:02 -0400

    wip

--- pragprog/dungeoncrawl/src/main.rs
+++ pragprog/dungeoncrawl/src/main.rs
@@ -6,6 +6,8 @@ mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
+    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
     pub use crate::map::*;
     pub use crate::map_builder::*;
     pub use crate::player::*;
