6475debe2852ad429b3453d3b07f55c2e17ca8f0
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-02 22:17:06 -0400

    wip

--- pragprog/treehouse/src/main.rs
+++ pragprog/treehouse/src/main.rs
@@ -1,19 +1,37 @@
 #![warn(clippy::all, clippy::pedantic)]
 
 use std::io::stdin;
-fn main() {
-    let visitors = ["bert", "collin", "steve"];
-    println!("name:");
-    let name = what_name();
-    let mut found = false;
-    for n in &visitors {
-        if n == &name {
-            println!("Welcome {name}");
-            found = true;
+
+struct Visitor {
+    name: String,
+    greeting: String,
+}
+
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_lowercase(),
         }
     }
-    if !found {
-        println!("Not welcome {name:?}");
+
+    fn greet(&self) {
+        println!("{}", self.greeting);
+    }
+}
+
+fn main() {
+    let visitor_list = [
+        Visitor::new("bert", "welp hi there"),
+        Visitor::new("collin", "Welcome"),
+        Visitor::new("steve", "huh?"),
+    ];
+    println!("name:");
+    let name = what_name();
+    let known = visitor_list.iter().find(|v| v.name == name);
+    match known {
+        Some(visitor) => visitor.greet(),
+        _ => println!("Not welcome {name}"),
     }
 }
 
