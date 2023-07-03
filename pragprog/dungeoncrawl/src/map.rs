0dafd1ed6c51e67d5a183806150d359f1b633244
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-03 14:20:38 -0400

    wip

--- pragprog/dungeoncrawl/src/map.rs
+++ pragprog/dungeoncrawl/src/map.rs
@@ -22,11 +22,20 @@ impl Map {
             tiles: vec![TileType::Floor; NUM_TILES],
         }
     }
+    pub fn in_bounds(point: Point) -> bool {
+        point.x >= 0 && point.x < SCREEN_WIDTH && point.y >= 0 && point.y < SCREEN_HEIGHT
+    }
     pub fn render(&self, ctx: &mut BTerm, camera: &Camera) {
         ctx.set_active_console(0);
+        for y in 0..SCREEN_HEIGHT {
+            for x in 0..SCREEN_WIDTH {
+                let glyph = to_cp437('#');
+                ctx.set(x, y, WHITE, BLACK, glyph);
+            }
+        }
         for y in camera.top_y..camera.bottom_y {
             for x in camera.left_x..camera.right_x {
-                if !self.in_bounds(Point::new(x, y)) {
+                if !Self::in_bounds(Point::new(x, y)) {
                     continue;
                 }
                 let idx = map_idx(x, y);
@@ -45,14 +54,11 @@ impl Map {
             }
         }
     }
-    pub fn in_bounds(&self, point: Point) -> bool {
-        point.x >= 0 && point.x < SCREEN_WIDTH && point.y >= 0 && point.y < SCREEN_HEIGHT
-    }
     pub fn can_enter_tile(&self, point: Point) -> bool {
-        self.in_bounds(point) && self.tiles[map_idx(point.x, point.y)] == TileType::Floor
+        Self::in_bounds(point) && self.tiles[map_idx(point.x, point.y)] == TileType::Floor
     }
     pub fn try_idx(&self, point: Point) -> Option<usize> {
-        if self.in_bounds(point) {
+        if Self::in_bounds(point) {
             Some(map_idx(point.x, point.y))
         } else {
             None
