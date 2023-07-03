818129a23a322d9d0575e5df6615c2333e372889
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-02 22:04:40 -0400

    wip

---
+++ pragprog/treehouse/src/main.rs
@@ -0,0 +1,14 @@
+#![warn(clippy::all, clippy::pedantic)]
+
+use std::io::stdin;
+fn main() {
+    println!("name:");
+    let name = what_name();
+    println!("Hello {name}");
+}
+
+fn what_name() -> String {
+    let mut name = String::new();
+    stdin().read_line(&mut name).expect("read name");
+    name
+}
