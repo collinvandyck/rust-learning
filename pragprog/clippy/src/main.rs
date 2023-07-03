eec12f38ebc024e17dfba9573bb86f9c4fa30619
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-02 20:33:06 -0400

    wip

--- pragprog/clippy/src/main.rs
+++ pragprog/clippy/src/main.rs
@@ -1,7 +1,8 @@
+#![warn(clippy::all, clippy::pedantic)]
 fn main() {
     let my_list = ["One", "Two", "Three"];
     for num in my_list {
-        println!("{}", num);
+        println!("{num}");
     }
 }
 
