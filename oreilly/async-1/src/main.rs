156d7ae74404ffed8b41bd748b911ac7cb02f70a
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-05 14:10:46 -0400

    wip

--- oreilly/async-1/src/main.rs
+++ oreilly/async-1/src/main.rs
@@ -1,24 +1,35 @@
 use std::{
-    io,
+    io, thread,
     time::{self, SystemTime},
 };
+
+use async_std::task::block_on;
 
 fn main() -> io::Result<()> {
     use async_std::task;
     let res = task::block_on(foo());
     assert_eq!(res, "Foo");
 
-    let v = (0..2)
-        .map(|_| task::spawn_local(time()))
-        .collect::<Vec<_>>();
-
-    dbg!(v);
+    block_on(get_times(10));
 
     Ok(())
 }
 
+async fn get_times(num: i32) {
+    use async_std::task;
+    let v = (0..num)
+        .map(|_| task::spawn_local(time()))
+        .collect::<Vec<_>>();
+    for handle in v {
+        handle.await;
+    }
+}
+
 async fn time() -> SystemTime {
-    time::SystemTime::now()
+    let id = thread::current().id();
+    let res = time::SystemTime::now();
+    println!("{id:?} produced {res:?}");
+    res
 }
 
 async fn foo() -> String {
