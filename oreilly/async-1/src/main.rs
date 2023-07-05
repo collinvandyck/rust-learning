7361c657a601044a6b90dc637ee7c98e7e8fbc38
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-05 14:00:30 -0400

    wip

--- oreilly/async-1/src/main.rs
+++ oreilly/async-1/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main() -> io::Result<()> {
+    use async_std::task;
+    let res = task::block_on(foo());
+    assert_eq!(res, "Foo");
+    Ok(())
+}
+
+async fn foo() -> String {
+    "Foo".into()
 }
