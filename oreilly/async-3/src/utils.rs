bfc3b69e2dd2bba47862428599b2efe3cb06e798
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-05 18:17:12 -0400

    wip

--- oreilly/async-3/src/utils.rs
+++ oreilly/async-3/src/utils.rs
@@ -1,12 +1,17 @@
 use std::error::Error;
 
 use async_std::net::TcpStream;
+use serde::Serialize;
 
 use crate::FromClient;
 
 pub type ChatError = Box<dyn Error + Send + Sync + 'static>;
 pub type ChatResult<T> = Result<T, ChatError>;
 
-pub async fn send_as_json(to_server: &mut TcpStream, req: &FromClient) -> ChatResult<()> {
+pub async fn send_as_json<S, P>(to_server: &mut S, packet: &P) -> ChatResult<()>
+where
+    S: async_std::io::Write + Unpin,
+    P: Serialize,
+{
     Ok(())
 }
