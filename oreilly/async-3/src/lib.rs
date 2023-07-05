411885a62f7c3390fd6d46229b55f14172bd3cae
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-05 15:25:25 -0400

    wip

--- oreilly/async-3/src/lib.rs
+++ oreilly/async-3/src/lib.rs
@@ -23,3 +23,18 @@ pub enum FromServer {
     },
     Error(String),
 }
+
+#[test]
+fn test_fromclient_json() {
+    use std::sync::Arc;
+
+    let from_client = FromClient::Post {
+        group_name: Arc::new("Dogs".to_string()),
+        message: Arc::new("Samoyeds rock!".to_string()),
+    };
+    let json = serde_json::to_string(&from_client).unwrap();
+    assert_eq!(
+        json,
+        r#"{"Post":{"group_name":"Dogs","message":"Samoyeds rock!"}}"#
+    )
+}
