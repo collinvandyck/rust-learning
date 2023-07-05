6eb9be1f137c322fd5c1fa05891a3648345ece71
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-05 00:43:29 -0400

    wip

--- newtype-1/src/main.rs
+++ newtype-1/src/main.rs
@@ -1,3 +1,26 @@
+use std::ops::Deref;
+
 fn main() {
-    println!("Hello, world!");
+    let things = Things::new(&[1, 2, 3]);
+    for x in things.iter() {
+        println!("{x}")
+    }
+}
+
+struct Things<T>(Vec<T>);
+
+impl<T> Things<T>
+where
+    T: Clone + Copy,
+{
+    fn new(vals: &[T]) -> Self {
+        Self(vals.into())
+    }
+}
+
+impl<T> Deref for Things<T> {
+    type Target = Vec<T>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
