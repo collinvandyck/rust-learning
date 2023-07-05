1ce2524bc2037fda551dc6466953e8743d0aafea
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-05 18:25:52 -0400

    wip

--- oreilly/async-3/src/utils.rs
+++ oreilly/async-3/src/utils.rs
@@ -1,7 +1,10 @@
 use std::error::Error;
 
-use async_std::io::WriteExt;
-use serde::Serialize;
+use async_std::{
+    io::{prelude::BufReadExt, WriteExt},
+    stream::{Stream, StreamExt},
+};
+use serde::{de::DeserializeOwned, Serialize};
 
 pub type ChatError = Box<dyn Error + Send + Sync + 'static>;
 pub type ChatResult<T> = Result<T, ChatError>;
@@ -15,4 +18,16 @@ where
     json.push('\n');
     outbound.write_all(json.as_bytes()).await?;
     Ok(())
+}
+
+pub fn receive_as_json<S, P>(inbound: S) -> impl Stream<Item = ChatResult<P>>
+where
+    S: async_std::io::BufRead + Unpin,
+    P: DeserializeOwned,
+{
+    inbound.lines().map(|line| -> ChatResult<P> {
+        let line = line?;
+        let parsed = serde_json::from_str::<P>(&line)?;
+        Ok(parsed)
+    })
 }
