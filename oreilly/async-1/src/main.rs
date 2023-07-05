b4e7006ddafe8a2b1cabda8cecef9f1acba013bd
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-05 14:04:41 -0400

    wip

--- oreilly/async-1/src/main.rs
+++ oreilly/async-1/src/main.rs
@@ -1,10 +1,25 @@
-use std::io;
+use std::{
+    io,
+    time::{self, SystemTime},
+};
 
 fn main() -> io::Result<()> {
     use async_std::task;
     let res = task::block_on(foo());
     assert_eq!(res, "Foo");
+
+    let v = (0..10)
+        .into_iter()
+        .map(|_| task::spawn_local(time()))
+        .collect::<Vec<_>>();
+
+    dbg!(v);
+
     Ok(())
+}
+
+async fn time() -> SystemTime {
+    time::SystemTime::now()
 }
 
 async fn foo() -> String {
