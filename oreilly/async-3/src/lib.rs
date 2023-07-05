1c11b496353499e93a136c1874fd2baeff0ed813
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-05 15:21:58 -0400

    wip

---
+++ oreilly/async-3/src/lib.rs
@@ -0,0 +1,25 @@
+use std::sync::Arc;
+
+use serde::{Deserialize, Serialize};
+
+pub mod utils;
+
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
+pub enum FromClient {
+    Join {
+        group_name: Arc<String>,
+    },
+    Post {
+        group_name: Arc<String>,
+        message: Arc<String>,
+    },
+}
+
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
+pub enum FromServer {
+    Message {
+        group_name: Arc<String>,
+        message: Arc<String>,
+    },
+    Error(String),
+}
--- oreilly/async-3/src/utils.rs
+++ oreilly/async-3/src/utils.rs
@@ -1,1 +1,4 @@
 use std::error::Error;
+
+pub type ChatError = Box<dyn Error + Send + Sync + 'static>;
+pub type ChatResult<T> = Result<T, ChatError>;
