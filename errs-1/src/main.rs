d61e93c92d63761e67a42a05cab8f206562c768c
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-06 15:23:10 -0400

    wip

--- errs-1/src/main.rs
+++ errs-1/src/main.rs
@@ -21,21 +21,21 @@ fn file_double<T: AsRef<Path>>(p: T) -> Result<i32, CliError> {
     let mut buf = String::new();
     file.read_to_string(&mut buf).map_err(CliError::IO)?;
     let buf = buf.trim().to_string();
-    let res = buf.parse::<i32>().map_err(|e| CliError::Parse(buf, e))?;
+    let res = buf.parse::<i32>().map_err(CliError::Parse)?;
     Ok(res * 2)
 }
 
 #[derive(Debug)]
 enum CliError {
     IO(io::Error),
-    Parse(String, num::ParseIntError),
+    Parse(num::ParseIntError),
 }
 
 impl fmt::Display for CliError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match *self {
             CliError::IO(ref err) => write!(f, "IO Error: {}", err),
-            CliError::Parse(ref s, ref err) => write!(f, "Parse error for '{s}': {}", err),
+            CliError::Parse(ref err) => write!(f, "Parse error: {}", err),
         }
     }
 }
@@ -44,7 +44,19 @@ impl Error for CliError {
     fn cause(&self) -> Option<&dyn Error> {
         match *self {
             CliError::IO(ref err) => Some(err),
-            CliError::Parse(_, ref err) => Some(err),
+            CliError::Parse(ref err) => Some(err),
         }
+    }
+}
+
+impl From<io::Error> for CliError {
+    fn from(value: io::Error) -> Self {
+        CliError::IO(value)
+    }
+}
+
+impl From<num::ParseIntError> for CliError {
+    fn from(value: num::ParseIntError) -> Self {
+        CliError::Parse(value)
     }
 }
