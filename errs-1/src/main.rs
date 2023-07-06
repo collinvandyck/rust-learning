530e710820b6fbb32304e1287191f4a908e4190f
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-06 14:51:21 -0400

    wip

--- errs-1/src/main.rs
+++ errs-1/src/main.rs
@@ -16,16 +16,16 @@ fn main() {
     println!("Test fail: {:?}", file_double("does not exist"));
 }
 
-fn file_double<T: AsRef<Path>>(p: T) -> Result<i32, Error> {
-    let mut file = File::open(p).map_err(Error::IO)?;
+fn file_double<T: AsRef<Path>>(p: T) -> Result<i32, CliError> {
+    let mut file = File::open(p).map_err(CliError::IO)?;
     let mut buf = String::new();
-    file.read_to_string(&mut buf).map_err(Error::IO)?;
-    let res = buf.trim().parse::<i32>().map_err(Error::Parse)?;
+    file.read_to_string(&mut buf).map_err(CliError::IO)?;
+    let res = buf.trim().parse::<i32>().map_err(CliError::Parse)?;
     Ok(res * 2)
 }
 
 #[derive(Debug)]
-enum Error {
+enum CliError {
     IO(io::Error),
     Parse(num::ParseIntError),
 }
