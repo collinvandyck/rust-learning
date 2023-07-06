e8c4e5044fa05883d3c6183268077170509477c0
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-06 16:16:15 -0400

    wip

--- anyhow-1/src/main.rs
+++ anyhow-1/src/main.rs
@@ -1,14 +1,29 @@
+use anyhow::anyhow;
+use anyhow::Context;
 use anyhow::Result;
 
 fn main() {
-    let path = "src/main.rs";
+    let path = "src/mainf.rs";
     match read_file(path) {
+        Ok(content) => println!("{content}"),
+        Err(e) => println!("Error: {:?}", e),
+    }
+
+    let res = return_err();
+    match res {
         Ok(content) => println!("{content}"),
         Err(e) => println!("Error: {}", e),
     }
 }
 
+fn return_err() -> Result<String> {
+    let err = anyhow!("Error Happened!!");
+    let err = Err(err);
+    err
+}
+
 fn read_file(path: &str) -> Result<String> {
-    let content = std::fs::read_to_string(path)?;
+    let content =
+        std::fs::read_to_string(path).with_context(|| format!("Failed to read file {}", path))?;
     Ok(content)
 }
