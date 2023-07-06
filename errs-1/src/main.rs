8c321eb89a0b064225aae19b0ad92abb257ab071
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-06 14:39:40 -0400

    wip

--- errs-1/src/main.rs
+++ errs-1/src/main.rs
@@ -1,13 +1,11 @@
 use std::{path::Path, process};
 
 fn main() {
-    match file_double("data.txt") {
-        Ok(v) => println!("Got value: {v}"),
-        Err(e) => {
-            eprintln!("Failure: {e:?}");
-            process::exit(1);
-        }
-    }
+    let res = file_double("data.txt").unwrap_or_else(|e| {
+        eprintln!("Failure: {e:?}");
+        process::exit(1);
+    });
+    println!("Res: {res}");
 }
 
 fn file_double<T: AsRef<Path>>(p: T) -> Result<i32, String> {
