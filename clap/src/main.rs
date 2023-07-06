9bd3f3dd27425c997f07dc23099d7133a2d4c1be
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-06 15:46:39 -0400

    wip

--- clap/src/main.rs
+++ clap/src/main.rs
@@ -13,6 +13,10 @@ struct Args {
 
     /// other thing idk
     other: Option<String>,
+
+    /// things that foo
+    #[arg(short)]
+    foos: Vec<String>,
 }
 
 fn main() {
@@ -22,4 +26,11 @@ fn main() {
     }
     let other: Option<&str> = args.other.as_deref();
     dbg!(other);
+
+    for x in &args.foos {
+        println!("foo: {x}")
+    }
+    for x in &args.foos {
+        println!("foo: {x}")
+    }
 }
