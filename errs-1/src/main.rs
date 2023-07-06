3ec72063d6a71028f9777520043ae17da34ab4a1
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-06 14:38:51 -0400

    wip

--- errs-1/src/main.rs
+++ errs-1/src/main.rs
@@ -1,3 +1,15 @@
+use std::{path::Path, process};
+
 fn main() {
-    println!("Hello, world!");
+    match file_double("data.txt") {
+        Ok(v) => println!("Got value: {v}"),
+        Err(e) => {
+            eprintln!("Failure: {e:?}");
+            process::exit(1);
+        }
+    }
+}
+
+fn file_double<T: AsRef<Path>>(p: T) -> Result<i32, String> {
+    Ok(5)
 }
