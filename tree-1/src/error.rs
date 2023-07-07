d40cb5087a0395c09e4031a7ec67e00e741a3d6f
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-07 12:19:34 -0400

    wip

--- tree-1/src/error.rs
+++ tree-1/src/error.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Display, io};
+use std::{error, fmt::Display, io};
 
 pub type WalkResult<T> = Result<T, Error>;
 
@@ -10,7 +10,7 @@ pub enum Error {
 
 impl Error {}
 
-impl std::error::Error for Error {}
+impl error::Error for Error {}
 
 impl Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
