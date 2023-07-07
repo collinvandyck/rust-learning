08c007c3fdea6e7d7c4987e1e44cc560b2521a2b
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-07 18:30:50 -0400

    wip

--- n-queens/src/main.rs
+++ n-queens/src/main.rs
@@ -24,8 +24,106 @@ impl Board {
             vals: vec![false; n * n],
         }
     }
+    // attempt to solve the board. we must place N pieces on the board.
     fn solve(&mut self) -> bool {
+        self.solve_n(self.n)
+    }
+    // solve for remain queens
+    fn solve_n(&mut self, remain: usize) -> bool {
+        if remain == 0 {
+            return true;
+        }
+        for col in 0..self.n {
+            for row in 0..self.n {
+                let occupied = self.at(row, col);
+                if occupied {
+                    continue;
+                }
+                // the the piece
+                self.set(row, col, true);
+
+                // check if valid
+                let valid = self.valid();
+
+                // if not valid, unset and continue
+                if !valid {
+                    self.set(row, col, false);
+                    continue;
+                }
+
+                // if valid, recurse
+                let success = self.solve_n(remain - 1);
+                if success {
+                    return true;
+                }
+
+                // didn't work, unset and continue
+                self.set(row, col, false);
+            }
+        }
+        // if we got to the end, we failed
         false
+    }
+    // ensures that all placed pieces are in a valid position
+    fn valid(&self) -> bool {
+        for col in 0..self.n {
+            for row in 0..self.n {
+                if self.at(row, col) {
+                    if !self.check(row, col) {
+                        return false;
+                    }
+                }
+            }
+        }
+        true
+    }
+    // checks that the piece at (row,col) cannot reach any other pieces
+    fn check(&self, row: usize, col: usize) -> bool {
+        // sanity check
+        assert_eq!(true, self.at(row, col));
+
+        // check upper left
+        if self.check_deltas(row, col, -1, -1) {
+            return false;
+        }
+
+        // check upper right
+        if self.check_deltas(row, col, -1, 1) {
+            return false;
+        }
+
+        // check lower left
+        if self.check_deltas(row, col, 1, -1) {
+            return false;
+        }
+
+        // check lower right
+        if self.check_deltas(row, col, 1, 1) {
+            return false;
+        }
+        true
+    }
+    fn check_deltas(&self, row: usize, col: usize, row_delta: i32, col_delta: i32) -> bool {
+        let mut row = row as i32;
+        let mut col = col as i32;
+        while row >= 0
+            && row < self.n.try_into().unwrap()
+            && col >= 0
+            && col < self.n.try_into().unwrap()
+        {
+            if self.at(row as usize, col as usize) {
+                return false;
+            }
+            row += row_delta;
+            col += col_delta;
+        }
+        true
+    }
+    fn set(&mut self, row: usize, col: usize, val: bool) {
+        self.vals[row * self.n + col] = val
+    }
+    fn at(&self, row: usize, col: usize) -> bool {
+        self.vals[row * self.n + col]
     }
 }
 
