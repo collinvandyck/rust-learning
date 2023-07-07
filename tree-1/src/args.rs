d71513585217bdfa83c606e83e9e4c3de5af6fb4
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-07 10:53:13 -0400

    wip

--- tree-1/src/args.rs
+++ tree-1/src/args.rs
@@ -2,6 +2,10 @@ pub use clap::Parser;
 
 #[derive(Parser, Debug)]
 pub struct Args {
+    /// How many levels of depth to search for files
     #[arg(short, long)]
     depth: Option<u32>,
+
+    /// The directory to search
+    dir: String,
 }
