ddd91c7a87b3191dce63e673ef99e9fb94dd5ad8
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-09 12:50:18 -0400

    wip

--- aoc-2022/day-01/src/main.rs
+++ aoc-2022/day-01/src/main.rs
@@ -5,18 +5,47 @@ use std::{
 };
 
 fn main() {
-    if let Err(e) = run() {
-        eprintln!("{e}");
-        process::exit(1);
-    }
+    let elves = match run() {
+        Ok(elves) => elves,
+        Err(e) => {
+            eprintln!("{e}");
+            process::exit(1);
+        }
+    };
+    dbg!(elves);
 }
 
-fn run() -> Result<u32, io::Error> {
+#[derive(Debug, Clone)]
+struct Elf {
+    id: u32,
+    presents: u32,
+}
+
+fn run() -> Result<Vec<Elf>, io::Error> {
     let file = File::open("input.txt")?;
     let read = BufReader::new(file);
+    let mut elves = vec![];
+    let mut elf_id = 1 as u32;
+    let mut count = 0 as u32;
     for line in read.lines() {
         let line = line?;
-        println!("{line}");
+        if line == "" {
+            elves.push(Elf {
+                id: elf_id,
+                presents: count,
+            });
+            elf_id += 1;
+            count = 0;
+            continue;
+        }
+        let delta: u32 = line.parse().unwrap();
+        count += delta;
     }
-    Ok(0)
+    if count != 0 {
+        elves.push(Elf {
+            id: elf_id,
+            presents: count,
+        })
+    }
+    Ok(elves)
 }
