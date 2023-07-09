15c7339b3140fb52f0bc1ee1d88361c78bb719e1
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-08 21:50:36 -0400

    wip

--- aoc-2022/day-01/src/main.rs
+++ aoc-2022/day-01/src/main.rs
@@ -1,3 +1,22 @@
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+    process,
+};
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = run() {
+        eprintln!("{e}");
+        process::exit(1);
+    }
+}
+
+fn run() -> Result<u32, io::Error> {
+    let file = File::open("input.txt")?;
+    let read = BufReader::new(file);
+    for line in read.lines() {
+        let line = line?;
+        println!("{line}");
+    }
+    Ok(0)
 }
