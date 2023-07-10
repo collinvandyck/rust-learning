aa73ed8bb5ff0379488573a455520aeed16502a3
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-09 23:44:28 -0400

    wip

--- aoc-2022/day-05/src/main.rs
+++ aoc-2022/day-05/src/main.rs
@@ -1,5 +1,29 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
 fn main() {
-    println!("Hello, world!");
+    run("example.txt");
+}
+
+fn run(filename: &str) {
+    let file = File::open(filename).unwrap();
+    let file = BufReader::new(file);
+    let mut iter = file.lines();
+    loop {
+        let line = iter.next().unwrap().unwrap();
+        if line == "" {
+            break;
+        }
+        let chars = &mut line.chars();
+        loop {
+            let part: String = chars.take(3).collect();
+            if part.len() < 3 {
+                break;
+            }
+        }
+    }
 }
 
 #[allow(dead_code)]
