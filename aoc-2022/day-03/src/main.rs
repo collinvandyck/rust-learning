63d4125c0d3bebbb7306039f93c8a3e458fe143d
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-09 21:11:47 -0400

    wip

--- aoc-2022/day-03/src/main.rs
+++ aoc-2022/day-03/src/main.rs
@@ -6,21 +6,44 @@ use std::{
 
 fn main() {
     run("example.txt");
+    run("input.txt");
+    badges("input.txt");
 }
 
 fn run(filename: &str) {
     let file = File::open(filename).unwrap();
     let read = BufReader::new(file);
+    let mut result: u32 = 0;
     for line in read.lines() {
         let line = line.unwrap();
         let (first, second) = line.split_at(line.len() / 2);
         let first = Compartment::new(first);
         let second = Compartment::new(second);
+        let common = first.intersect(&second);
+        result += common.priority();
     }
+    println!("Result for {filename}: {result}");
 }
 
+fn badges(filename: &str) {
+    let file = File::open(filename).unwrap();
+    let read = BufReader::new(file);
+    let mut lines = read.lines().collect::<Vec<_>>();
+    lines.chunks(3).into_iter().for_each(|chunk| {
+        println!("Group!");
+        let res = chunk
+            .iter()
+            .flatten()
+            .map(|x| Compartment::new(x))
+            .reduce(|first, second| first.intersect(&second))
+            .unwrap();
+        dbg!(res);
+    })
+}
+
+#[derive(Debug)]
 struct Compartment {
-    items: HashMap<Item, u32>,
+    items: HashMap<Item, bool>,
 }
 
 impl Compartment {
@@ -28,20 +51,30 @@ impl Compartment {
         let mut items = HashMap::new();
         input.chars().for_each(|c| {
             let item = Item(c);
-            let entry = items.entry(item).or_insert(0);
-            *entry += 1;
+            items.insert(item, true);
         });
         Compartment { items }
     }
+    fn intersect(&self, other: &Compartment) -> Compartment {
+        let mut res = Compartment::new("");
+        self.items.keys().for_each(|k| {
+            if other.items.contains_key(k) {
+                res.items.insert(*k, true);
+            }
+        });
+        res
+    }
+    fn priority(&self) -> u32 {
+        self.items.keys().map(Item::priority).sum::<u32>()
+    }
 }
 
-#[derive(Hash, PartialEq, Eq)]
+#[derive(Hash, PartialEq, Eq, Copy, Clone, Debug)]
 struct Item(char);
 
 impl Item {
     fn priority(&self) -> u32 {
-        let mut av = self.0 as u32;
-        dbg!((self.0, av));
+        let av = self.0 as u32;
         if av <= 96 {
             av - 38
         } else {
