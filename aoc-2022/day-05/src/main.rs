cf1675fa7086c7875e116d1132554ee06d6efc6d
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-10 17:44:53 -0400

    wip

--- aoc-2022/day-05/src/main.rs
+++ aoc-2022/day-05/src/main.rs
@@ -1,6 +1,8 @@
 use std::{
+    collections::VecDeque,
     fs::File,
     io::{BufRead, BufReader},
+    ops::{Deref, Index},
 };
 
 fn main() {
@@ -17,6 +19,11 @@ fn run(filename: &str) {
             break;
         }
         let mut chars = line.chars();
+        let mut stack_idx = 0;
+        // loop over the entries in the line. we'll add items into
+        // this vector, and if it's not empty at the end we'll add them
+        // into the ship.
+        let mut ship = Ship::new();
         loop {
             let part: String = chars.by_ref().take(3).collect();
             if part.len() < 3 {
@@ -24,16 +31,43 @@ fn run(filename: &str) {
                 println!("Done");
                 break;
             }
-            dbg!(part);
+            if part.starts_with('[') && part.ends_with(']') {
+                let crt = Crate(part.chars().nth(1).unwrap());
+                ship.insert_crate(stack_idx, crt);
+            }
             // discard the space
             chars.next();
+            stack_idx += 1;
         }
     }
 }
 
-#[allow(dead_code)]
 struct Ship(Vec<Stack>);
 
-struct Stack(Vec<Crate>);
+impl Ship {
+    fn new() -> Self {
+        Self(Vec::new())
+    }
+    fn insert_crate(&mut self, stack_idx: usize, crt: Crate) {
+        if self.0.len() <= stack_idx {
+            let grow = stack_idx - self.0.len() + 1;
+            dbg!(grow);
+            for _ in 0..grow {
+                self.0.push(Stack::new());
+            }
+        }
+        let stack = self.0.get_mut(stack_idx).unwrap();
+        stack.0.push_front(crt);
+    }
+}
 
+struct Stack(VecDeque<Crate>);
+
+impl Stack {
+    fn new() -> Self {
+        Self(VecDeque::new())
+    }
+}
+
+#[derive(Debug)]
 struct Crate(char);
