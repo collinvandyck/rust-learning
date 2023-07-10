938da40045e80a37300e9f43001b777d8085872f
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-10 11:40:18 -0400

    wip

--- hashmap-1/src/hashmap.rs
+++ hashmap-1/src/hashmap.rs
@@ -1,24 +1,46 @@
+use std::{
+    collections::hash_map::DefaultHasher,
+    hash::{Hash, Hasher},
+};
+
 #[allow(dead_code)]
 pub struct HashMap<K, V> {
     buckets: Vec<Bucket<K, V>>,
 }
 
 #[allow(dead_code)]
-impl<K, V> HashMap<K, V> {
+impl<K, V> HashMap<K, V>
+where
+    K: Hash,
+{
     pub fn new() -> Self {
         Self::new_size(8)
     }
     fn new_size(size: usize) -> Self {
-        Self {
-            buckets: Vec::with_capacity(size),
+        let mut buckets = vec![];
+        for _ in 0..size {
+            buckets.push(Bucket::new());
         }
+        Self { buckets }
     }
-    pub fn add(&mut self, key: K, val: V) {}
+    pub fn add(&mut self, key: K, val: V) {
+        let mut h = DefaultHasher::new();
+        key.hash(&mut h);
+        let hash = h.finish();
+        let idx = hash % self.buckets.len() as u64;
+        dbg!(idx);
+    }
 }
 
 #[allow(dead_code)]
 struct Bucket<K, V> {
     items: Vec<Item<K, V>>,
+}
+
+impl<K, V> Bucket<K, V> {
+    fn new() -> Self {
+        Self { items: vec![] }
+    }
 }
 
 #[allow(dead_code)]
