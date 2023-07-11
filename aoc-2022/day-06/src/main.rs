7e644e8cf0ef7c53049c01644f1fd8eaf1f78aae
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-10 22:01:05 -0400

    wip

--- aoc-2022/day-06/src/main.rs
+++ aoc-2022/day-06/src/main.rs
@@ -2,11 +2,11 @@ use std::fs;
 
 fn main() {
     let s = fs::read_to_string("input.txt").unwrap();
-    let res = first_marker(&s);
+    let res = start_of_packet(&s);
     println!("{res:?}")
 }
 
-fn first_marker(s: &str) -> Option<usize> {
+fn start_of_packet(s: &str) -> Option<usize> {
     let chars = s.chars().collect::<Vec<_>>();
     let idx = chars.windows(4).enumerate().find(|(_i, chars)| {
         let mut chs = chars.to_vec();
@@ -18,8 +18,14 @@ fn first_marker(s: &str) -> Option<usize> {
 
 #[test]
 fn test_first_marker() {
-    assert_eq!(first_marker("bvwbjplbgvbhsrlpgdmjqwftvncz"), Some(5));
-    assert_eq!(first_marker("nppdvjthqldpwncqszvftbrmjlhg"), Some(6));
-    assert_eq!(first_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), Some(10));
-    assert_eq!(first_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), Some(11));
+    assert_eq!(start_of_packet("bvwbjplbgvbhsrlpgdmjqwftvncz"), Some(5));
+    assert_eq!(start_of_packet("nppdvjthqldpwncqszvftbrmjlhg"), Some(6));
+    assert_eq!(
+        start_of_packet("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"),
+        Some(10)
+    );
+    assert_eq!(
+        start_of_packet("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"),
+        Some(11)
+    );
 }
