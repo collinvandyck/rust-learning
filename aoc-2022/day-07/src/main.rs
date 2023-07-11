106699fccff2695384ab020306a9bd163dd9b961
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-10 22:34:12 -0400

    wip

--- aoc-2022/day-07/src/main.rs
+++ aoc-2022/day-07/src/main.rs
@@ -1,7 +1,6 @@
 use std::{
     fs::File,
     io::{BufRead, BufReader},
-    process,
 };
 
 fn main() {
@@ -9,10 +8,43 @@ fn main() {
 }
 
 fn process(filename: &str) {
+    let exprs = parse(filename);
+    dbg!(exprs);
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+enum Expr {
+    CmdCd { name: String },
+    CmdLs,
+    DirHeader { name: String },
+    FileInfo { size: u64, name: String },
+}
+
+fn parse(filename: &str) -> Vec<Expr> {
     let file = File::open(filename).unwrap();
     let read = BufReader::new(file);
-    for line in read.lines() {
-        let line = line.unwrap();
-        println!("{line}");
-    }
+    read.lines()
+        .map(|line| {
+            let line = line.unwrap();
+            let split = line.split(' ').collect::<Vec<_>>();
+            match split[..] {
+                ["$", "cd", name] => Expr::CmdCd {
+                    name: name.to_string(),
+                },
+                ["$", "ls"] => Expr::CmdLs,
+                ["dir", name] => Expr::DirHeader {
+                    name: name.to_string(),
+                },
+                [size, name] => {
+                    let size = size.parse::<u64>().unwrap();
+                    Expr::FileInfo {
+                        size: size,
+                        name: name.to_string(),
+                    }
+                }
+                _ => panic!("boom"),
+            }
+        })
+        .collect()
 }
