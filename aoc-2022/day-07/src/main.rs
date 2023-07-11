2cdb439fd331d5a52d6b97678508e06ffefae645
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-10 23:08:47 -0400

    wip

--- aoc-2022/day-07/src/main.rs
+++ aoc-2022/day-07/src/main.rs
@@ -8,20 +8,85 @@ fn main() {
 }
 
 fn process(filename: &str) {
-    let exprs = parse(filename);
-    dbg!(exprs);
+    let mut state = State::new();
+    let tokens = lex(filename);
+    let mut iter = tokens.into_iter().peekable();
+    loop {
+        let token = iter.next();
+        if token.is_none() {
+            break;
+        }
+        let token = token.unwrap();
+        match token {
+            Token::CmdCd { ref name } => state.cd(name.to_string()),
+            Token::CmdLs => {
+                // consume as long as the next token is a dir header or file info
+                loop {
+                    let peek = iter.peek();
+                    match peek {
+                        Some(Token::DirHeader { name }) => state.add_dir(name.to_string()),
+                        Some(Token::FileInfo { size, name }) => {
+                            state.add_file(name.to_string(), *size)
+                        }
+                        _ => break,
+                    }
+                    iter.next();
+                }
+            }
+            _ => panic!("Invalid token sequence"),
+        }
+    }
 }
+
+struct State {
+    root: FSDir,
+    pwd: Option<String>,
+}
+
+impl State {
+    fn new() -> Self {
+        Self {
+            root: FSDir {
+                name: "/".into(),
+                children: vec![],
+            },
+            pwd: None,
+        }
+    }
+    fn cd(&mut self, path: String) {
+        self.pwd = Some(path);
+    }
+    fn add_dir(&mut self, dir: String) {}
+    fn add_file(&mut self, name: String, size: u64) {}
+}
+
+struct FSDir {
+    name: String,
+    children: Vec<FS>,
+}
+
+struct FSFile {
+    name: String,
+    size: usize,
+}
+
+enum FS {
+    Dir(FSDir),
+    File(FSFile),
+}
+
+impl FS {}
 
 #[allow(dead_code)]
 #[derive(Debug)]
-enum Expr {
+enum Token {
     CmdCd { name: String },
     CmdLs,
     DirHeader { name: String },
     FileInfo { size: u64, name: String },
 }
 
-fn parse(filename: &str) -> Vec<Expr> {
+fn lex(filename: &str) -> Vec<Token> {
     let file = File::open(filename).unwrap();
     let read = BufReader::new(file);
     read.lines()
@@ -29,16 +94,16 @@ fn parse(filename: &str) -> Vec<Expr> {
             let line = line.unwrap();
             let split = line.split(' ').collect::<Vec<_>>();
             match split[..] {
-                ["$", "cd", name] => Expr::CmdCd {
+                ["$", "cd", name] => Token::CmdCd {
                     name: name.to_string(),
                 },
-                ["$", "ls"] => Expr::CmdLs,
-                ["dir", name] => Expr::DirHeader {
+                ["$", "ls"] => Token::CmdLs,
+                ["dir", name] => Token::DirHeader {
                     name: name.to_string(),
                 },
                 [size, name] => {
                     let size = size.parse::<u64>().unwrap();
-                    Expr::FileInfo {
+                    Token::FileInfo {
                         size: size,
                         name: name.to_string(),
                     }
