dcb8a5cbc27ded2e4fb2dee507b9a4d1ea162c48
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-10 21:56:48 -0400

    wip

---
+++ aoc-2022/day-06/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+fn first_marker(s: &str) -> Option<usize> {
+    let chars = s.chars().collect::<Vec<_>>();
+    let idx = chars.windows(4).enumerate().find(|(i, chars)| {
+        let mut chs = chars.to_vec();
+        chs.sort();
+        chs.windows(2).all(|v| v[0] != v[1])
+    });
+    idx.map(|i| i.0 + 4)
+}
+
+#[test]
+fn test_first_marker() {
+    assert_eq!(first_marker("bvwbjplbgvbhsrlpgdmjqwftvncz"), Some(5));
+}
