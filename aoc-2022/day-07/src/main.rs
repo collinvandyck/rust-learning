0b819a35b4468eb45840d3ed2d4b75a8608caf03
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-11 17:23:06 -0400

    wip

--- aoc-2022/day-07/src/main.rs
+++ aoc-2022/day-07/src/main.rs
@@ -1,2 +1,13 @@
-fn main() {}
+mod shell;
+
+mod prelude {
+    pub use crate::shell::*;
+}
+
+use prelude::*;
+
+fn main() {
+    let lines = parse_lines("example.txt");
+    dbg!(lines);
+}
 
---
+++ aoc-2022/day-07/src/shell.rs
@@ -0,0 +1,31 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+#[derive(Debug)]
+#[allow(dead_code)]
+pub enum Line {
+    Cd(String),
+    Ls(),
+    Dir(String),
+    File(u64, String),
+}
+
+pub fn parse_lines(filename: &str) -> Vec<Line> {
+    let file = File::open(filename).unwrap();
+    let read = BufReader::new(file);
+    read.lines()
+        .flatten()
+        .map(|s| {
+            let parts = s.as_str().split(' ').collect::<Vec<_>>();
+            match parts[..] {
+                ["$", "cd", dir] => Line::Cd(dir.to_string()),
+                ["$", "ls"] => Line::Ls(),
+                ["dir", dir] => Line::Dir(dir.to_string()),
+                [size, name] => Line::File(size.parse::<u64>().unwrap(), name.to_string()),
+                _ => panic!("parse error"),
+            }
+        })
+        .collect::<Vec<_>>()
+}
