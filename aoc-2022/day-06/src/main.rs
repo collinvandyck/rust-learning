01735a80868ab1975c5f4887223b5ff3343864fd
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-10 22:05:01 -0400

    wip

--- aoc-2022/day-06/src/main.rs
+++ aoc-2022/day-06/src/main.rs
@@ -16,6 +16,31 @@ fn start_of_packet(s: &str) -> Option<usize> {
     idx.map(|i| i.0 + 4)
 }
 
+fn start_of_message(s: &str) -> Option<usize> {
+    let chars = s.chars().collect::<Vec<_>>();
+    let idx = chars.windows(14).enumerate().find(|(_i, chars)| {
+        let mut chs = chars.to_vec();
+        chs.sort();
+        chs.windows(2).all(|v| v[0] != v[1])
+    });
+    idx.map(|i| i.0 + 14)
+}
+
+#[test]
+fn test_start_of_message() {
+    assert_eq!(start_of_message("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), Some(19));
+    assert_eq!(start_of_message("bvwbjplbgvbhsrlpgdmjqwftvncz"), Some(23));
+    assert_eq!(start_of_message("nppdvjthqldpwncqszvftbrmjlhg"), Some(23));
+    assert_eq!(
+        start_of_message("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"),
+        Some(29)
+    );
+    assert_eq!(
+        start_of_message("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"),
+        Some(26)
+    );
+}
+
 #[test]
 fn test_first_marker() {
     assert_eq!(start_of_packet("bvwbjplbgvbhsrlpgdmjqwftvncz"), Some(5));
