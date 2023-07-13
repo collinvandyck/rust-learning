829db98a0dbfc4b97df64b962ed255c9f1b7ac67
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-13 16:34:32 -0400

    wip

--- aoc-2022/day-07/src/fs.rs
+++ aoc-2022/day-07/src/fs.rs
@@ -29,9 +29,9 @@ pub enum Node {
 }
 
 impl Node {
-    fn add(&mut self, mut parts: Vec<String>, node: Node) {
-        if let Some(part) = parts.get(0).cloned() {
-            parts.remove(0);
+    fn add(&mut self, mut path: Vec<String>, node: Node) {
+        if let Some(part) = path.get(0).cloned() {
+            path.remove(0);
             match self {
                 Node::Dir(_, children) => {
                     let child = match children.iter_mut().find(|c| c.name() == &part) {
@@ -41,7 +41,7 @@ impl Node {
                             children.last_mut().unwrap()
                         }
                     };
-                    child.add(parts, node);
+                    child.add(path, node);
                 }
                 _ => panic!("File found, expected dir"),
             }
@@ -52,43 +52,9 @@ impl Node {
             }
         }
     }
-    fn mkdirs_p(&mut self, mut parts: Vec<String>) {
-        if let Some(part) = parts.get(0).cloned() {
-            parts.remove(0);
-            if let Node::Dir(name, children) = self {
-                if name == &part {
-                    if parts.is_empty() {
-                        return;
-                    }
-                    let part = parts.remove(0);
-                    let next = children.iter_mut().find(|c| c.name() == &part);
-                    let next = match next {
-                        Some(next) => next,
-                        _ => {
-                            children.push(Node::Dir(part.to_string(), vec![]));
-                            children.last_mut().unwrap()
-                        }
-                    };
-                    next.mkdirs_p(parts);
-                } else {
-                    // the name didn't match. add a child directory
-                    children.push(Node::Dir(part.to_string(), vec![]));
-                    let next = children.last_mut().unwrap();
-                    next.mkdirs_p(parts);
-                }
-            }
-        }
-    }
     fn name(&self) -> &String {
         match self {
             Node::Dir(name, _) | Node::File(name, _) => name,
         }
     }
-}
-
-#[test]
-fn test_node_mkdirs_p() {
-    let mut node = Node::Dir("/".to_string(), vec![]);
-    node.mkdirs_p(vec!["/".to_string(), "foo".to_string(), "bar".to_string()]);
-    dbg!(node);
 }
