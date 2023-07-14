bf4fde2e7291f58e0ece96aea928aece224e79c5
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-13 21:19:37 -0400

    wip

--- aoc-2022/day-08/src/forest.rs
+++ aoc-2022/day-08/src/forest.rs
@@ -1,3 +1,5 @@
+use std::slice::SliceIndex;
+
 #[derive(Debug)]
 pub struct Forest {
     trees: Vec<Vec<Tree>>,
@@ -11,7 +13,29 @@ impl Forest {
         self.trees.push(line);
     }
     pub fn visible(&self) -> u32 {
-        0
+        let mut count: u32 = 0;
+        let rows = self.rows();
+        let cols = self.cols();
+        for row in 0..rows {
+            for col in 0..cols {
+                if self.tree_visible(row, col) {
+                    count += 1;
+                }
+            }
+        }
+        count
+    }
+    fn tree_visible(&self, row: usize, col: usize) -> bool {
+        true
+    }
+    fn rows(&self) -> usize {
+        self.trees.len()
+    }
+    fn cols(&self) -> usize {
+        if self.rows() == 0 {
+            return 0;
+        }
+        self.trees.get(0).unwrap().len()
     }
 }
 
