f290ed5f3e90e2cafd114700042df6feff5218d6
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-15 18:52:16 -0400

    wip

--- bignums-1/src/main.rs
+++ bignums-1/src/main.rs
@@ -1,5 +1,3 @@
-use std::collections::VecDeque;
-
 fn main() {
     println!("Hello, world!");
 }
@@ -8,24 +6,39 @@ fn main() {
 struct Num(Vec<u64>);
 
 impl Num {
+    fn new() -> Self {
+        Self(vec![])
+    }
+    #[allow(dead_code)]
     fn from(mut v: u64) -> Self {
-        let mut vd = Vec::new();
+        let mut res = Self::new();
         let divisor: u64 = 10;
         loop {
             println!("v: {v} divisor: {divisor}");
             let part = v % divisor;
-            vd.insert(0, part);
+            res.insert(part);
             v = v / divisor;
             if v == 0 {
                 break;
             }
         }
-        Self(vd)
+        res
+    }
+    fn insert(&mut self, v: u64) {
+        self.0.insert(0, v)
+    }
+    fn add(&self, other: &Num) -> Self {
+        let v1 = &self.0;
+        let v2 = &other.0;
+        for n1 in v1.iter().rev() {
+            for n2 in v2.iter().rev() {}
+        }
+        Self::new()
     }
 }
 
 #[test]
-fn test_num() {
+fn test_num_from() {
     assert_eq!(Num::from(0), Num(vec![0]));
     assert_eq!(Num::from(643), Num(vec![6, 4, 3]));
     assert_eq!(
