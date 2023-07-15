475fa653cef3fd854d610455054307ae629294bf
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-15 16:16:46 -0400

    wip

--- aoc-2022/day-11/src/item.rs
+++ aoc-2022/day-11/src/item.rs
@@ -12,5 +12,16 @@ impl Item {
     pub fn inspect(&mut self, op: &Op) {
         // first, mutate the worry value on inspection
         self.worry = op.calculate(self.worry);
+
+        // divide by three b/c no items are damaged
+        self.worry /= 3;
     }
+}
+
+#[test]
+fn test_divide() {
+    assert_eq!(0 / 3, 0);
+    assert_eq!(1 / 3, 0);
+    assert_eq!(3 / 3, 1);
+    assert_eq!(4 / 3, 1);
 }
