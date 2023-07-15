6cb4305d0559f1fb10e8690f9f563a0a3753cfaa
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-15 14:59:10 -0400

    wip

--- aoc-2022/day-11/src/monkey.rs
+++ aoc-2022/day-11/src/monkey.rs
@@ -1,7 +1,25 @@
-pub struct Monkey {}
+#[derive(Debug)]
+pub struct Monkey {
+    idx: usize,
+}
 
 impl Monkey {
     pub fn load(iter: &mut impl Iterator<Item = String>) -> Option<Self> {
-        None
+        let idx = Self::parse_monkey(iter.next().unwrap());
+        iter.next();
+        iter.next();
+        iter.next();
+        iter.next();
+        iter.next();
+        dbg!(Some(Self { idx }))
+    }
+    fn parse_monkey(input: String) -> usize {
+        let parts = &input.split(' ').collect::<Vec<&str>>()[..];
+        if let ["Monkey", num] = parts {
+            if let Some(num) = num.split(':').next() {
+                return num.parse::<usize>().unwrap();
+            }
+        }
+        panic!("invalid monkey: {input}");
     }
 }
