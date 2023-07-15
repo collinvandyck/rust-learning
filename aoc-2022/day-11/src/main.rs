6eaa76d28522f5e66de4dcd7c9e97eb983532848
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-15 17:01:18 -0400

    wip

--- aoc-2022/day-11/src/main.rs
+++ aoc-2022/day-11/src/main.rs
@@ -23,17 +23,17 @@ mod prelude {
 use prelude::*;
 
 fn main() {
-    run_example(3, "example.txt");
-    run_example(3, "input.txt");
+    run_example(3, 20, "example.txt");
+    run_example(3, 20, "input.txt");
 }
 
-fn run_example(worry_divisor: u64, filename: &str) {
+fn run_example(worry_divisor: u64, rounds: usize, filename: &str) {
     let file = File::open(filename).unwrap();
     let read = BufReader::new(file);
     let iter = read.lines().map(std::result::Result::unwrap);
     let mut game = Game::new(worry_divisor, iter);
     println!("Start:\n{game}");
-    let monkey_business = game.run(20);
+    let monkey_business = game.run(rounds);
     println!("Finish:\n{game}");
     println!("Monkey business: {monkey_business}");
 }
