b4f269f868e721837b790923a89929f20cb88c93
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-15 14:18:17 -0400

    wip

--- aoc-2022/day-10/src/crt.rs
+++ aoc-2022/day-10/src/crt.rs
@@ -1,19 +1,27 @@
 pub struct Crt {
     cols: usize,
-    pos: usize,
+    pos: i64,
 }
 
 impl Crt {
     pub fn new() -> Self {
         Self { cols: 40, pos: 0 }
     }
-    pub fn draw(&mut self, tick: usize, val: i64) {
-        self.pos += 1;
-        if self.pos > self.cols {
-            self.pos = 1;
+    pub fn draw(&mut self, _tick: usize, val: i64) {
+        //println!("tick: {tick} val: {val} pos:{}", self.pos);
+
+        // TODO: print the pixel or not
+        if self.pos >= val - 1 && self.pos <= val + 1 {
+            print!("{}", '#');
+        } else {
+            print!("{}", '.');
         }
-        println!("tick: {tick} val: {val} pos:{}", self.pos);
-        if self.pos % self.cols == 0 {
+
+        self.pos += 1;
+        if self.pos == self.cols.try_into().unwrap() {
+            self.pos = 0;
+        }
+        if self.pos == 0 {
             println!();
         }
     }
