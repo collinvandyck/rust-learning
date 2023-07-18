87649f07409b74d868fcb113e61a8992c7f0c064
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-18 16:20:44 -0400

    wip

--- aoc-2022/day-12/src/main.rs
+++ aoc-2022/day-12/src/main.rs
@@ -18,29 +18,88 @@ struct Point(usize, usize); // row,col
 #[derive(Debug)]
 struct Path(Vec<Point>);
 
+impl Path {
+    fn last(&self) -> Option<&Point> {
+        self.0.last()
+    }
+    fn add(&mut self, point: Point) {
+        self.0.push(point);
+    }
+}
+
 struct Visited(HashSet<Point>);
+
+impl Visited {
+    fn add(&mut self, point: Point) {
+        if !self.0.insert(point) {
+            panic!("visited already contains {point:?}");
+        }
+    }
+    fn contains(&self, point: &Point) -> bool {
+        self.0.contains(point)
+    }
+}
 
 struct Solver<'a> {
     map: &'a Map,
     path: Path,
     visited: Visited,
-    cur: Option<Point>,
 }
 
 impl<'a> Solver<'a> {
     fn new(map: &'a Map) -> Self {
         let path = Path(vec![]);
         let visited = Visited(HashSet::new());
-        Self {
-            map,
-            path,
-            visited,
-            cur: None,
-        }
+        Self { map, path, visited }
     }
     // solve attempts to find the shortest path from the start to the end.
     fn solve(&'a mut self) -> &'a Path {
+        for next in self.next_points() {
+            dbg!(next);
+            self.path.add(next);
+            self.visited.add(next);
+        }
         &self.path
+    }
+    fn next_points(&self) -> impl Iterator<Item = Point> {
+        let mut v = Vec::with_capacity(4);
+        match self.path.last() {
+            Some(cur @ &Point(row, col)) => {
+                if row > 0 {
+                    let point = Point(row - 1, col);
+                    if self.can_move(cur, &point) {
+                        v.push(point);
+                    }
+                }
+                if col > 0 {
+                    let point = Point(row, col - 1);
+                    if self.can_move(cur, &point) {
+                        v.push(point);
+                    }
+                }
+                if row < self.map.rows - 1 {
+                    let point = Point(row + 1, col);
+                    if self.can_move(cur, &point) {
+                        v.push(point);
+                    }
+                }
+                if col < self.map.cols - 1 {
+                    let point = Point(row, col + 1);
+                    if self.can_move(cur, &point) {
+                        v.push(point);
+                    }
+                }
+            }
+            None => v.push(self.map.start),
+        }
+        v.into_iter()
+    }
+    fn can_move(&self, from: &Point, to: &Point) -> bool {
+        if self.visited.contains(to) {
+            return false;
+        }
+        let distance = self.map.distance(from, to);
+        distance <= 1
     }
 }
 
@@ -58,6 +117,14 @@ impl Map {
         let mut solver = Solver::new(self);
         let path = solver.solve();
         println!("Solution: {path:?}");
+    }
+    fn distance(&self, from: &Point, to: &Point) -> i32 {
+        let from = self.get(from) as i32;
+        let to = self.get(to) as i32;
+        to - from
+    }
+    fn get(&self, p: &Point) -> char {
+        *self.tiles.get(p.0).unwrap().get(p.1).unwrap()
     }
     fn render(&self) -> String {
         self.tiles
