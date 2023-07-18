c8d0225a1e4c5232ce773dc1c94155bd479159fc
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-18 12:12:36 -0400

    wip

--- aoc-2022/day-12/src/main.rs
+++ aoc-2022/day-12/src/main.rs
@@ -1,3 +1,98 @@
+use std::{
+    fmt::Display,
+    fs::{self, File},
+    io::{BufRead, BufReader},
+};
+
 fn main() {
-    println!("Hello, world!");
+    run("example.txt");
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct Point(usize, usize); // row,col
+
+struct Map {
+    tiles: Vec<Vec<char>>,
+    start: Point,
+    finish: Point,
+    rows: usize,
+    cols: usize,
+}
+
+impl Map {
+    fn solve(&mut self) {
+        println!("Solve:\n{self}");
+    }
+    fn render(&self) -> String {
+        self.tiles
+            .iter()
+            .enumerate()
+            .map(|(row, rows)| {
+                rows.iter()
+                    .enumerate()
+                    .map(|(col, char)| {
+                        let point = Point(row, col);
+                        if self.start == point {
+                            &'S'
+                        } else if self.finish == point {
+                            &'E'
+                        } else {
+                            char
+                        }
+                    })
+                    .collect::<String>()
+            })
+            .collect::<Vec<String>>()
+            .join("\n")
+    }
+    fn new(tiles: Vec<Vec<char>>, start: Point, finish: Point) -> Self {
+        let rows = tiles.len();
+        let cols = tiles.get(0).map_or(0, |r| r.len());
+        Self {
+            tiles,
+            start,
+            finish,
+            rows,
+            cols,
+        }
+    }
+}
+
+impl Display for Map {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.render())
+    }
+}
+
+fn read_map(iter: impl Iterator<Item = String>) -> Map {
+    let mut tiles = vec![];
+    let mut start = Point(0, 0);
+    let mut finish = Point(0, 0);
+    for (row, line) in iter.enumerate() {
+        let row: Vec<char> = line
+            .chars()
+            .enumerate()
+            .map(|(col, c)| match c {
+                'S' => {
+                    start = Point(row, col);
+                    'a'
+                }
+                'E' => {
+                    finish = Point(row, col);
+                    'z'
+                }
+                c => c,
+            })
+            .collect();
+        tiles.push(row);
+    }
+    Map::new(tiles, start, finish)
+}
+
+fn run(filename: &str) {
+    let file = File::open(filename).unwrap();
+    let read = BufReader::new(file);
+    let lines = read.lines().flatten();
+    let mut map = read_map(lines);
+    map.solve();
 }
