21c17605852604f9dfc449a7655cba1a6985a978
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-19 13:43:18 -0400

    wip

--- aoc-2022/day-13/src/signal.rs
+++ aoc-2022/day-13/src/signal.rs
@@ -10,5 +10,25 @@ pub fn parse_packet(line: String) -> Packet {
 }
 
 fn parse_chars(chars: &mut impl Iterator<Item = char>) -> Packet {
-    Packet::Value(42)
+    let ch = chars.next().unwrap();
+    match ch {
+        '[' => {
+            let list = consume_list(chars);
+            Packet::List(list)
+        }
+        _ => Packet::Value(42),
+    }
+}
+
+fn consume_list(chars: &mut impl Iterator<Item = char>) -> Vec<Packet> {
+    let mut res = vec![];
+    loop {
+        let packet = parse_chars(chars);
+        res.push(packet);
+        match chars.next().unwrap() {
+            ',' => continue,
+            ']' => return res,
+            ch => panic!("Unexpected char: {ch}"),
+        }
+    }
 }
