0f317b28fb8ccbea5b4caa4aea1042f172ac2b20
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-19 14:10:14 -0400

    wip

--- aoc-2022/day-13/src/main.rs
+++ aoc-2022/day-13/src/main.rs
@@ -19,16 +19,24 @@ fn part_one(filename: &str) {
     let file = File::open(filename).unwrap();
     let read = BufReader::new(file);
     let mut iter = read.lines().flatten();
+    let mut pairs = vec![];
     loop {
         let one = iter.next().unwrap();
         let two = iter.next().unwrap();
         let pair = parse_pair(one, two);
-        println!("{pair:?}");
-        let ordered = pair.is_ordered();
-        println!("Ordered: {ordered}");
+        pairs.push(pair);
         if iter.next().is_none() {
             break;
         }
-        println!();
     }
+    pairs
+        .iter()
+        .enumerate()
+        .map(|(idx, pair)| (idx + 1, pair))
+        .map(|(idx, pair)| (idx, pair, pair.is_ordered()))
+        .for_each(|(idx, pair, ordered)| {
+            println!("idx: {idx}");
+            println!("pair: {pair:?}");
+            println!("ordered: {ordered}");
+        });
 }
