fc2caaa57982267d4429d63ba12686c0fd8b9bce
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-19 11:36:32 -0400

    wip

--- aoc-2022/day-12/src/dijkstra.rs
+++ aoc-2022/day-12/src/dijkstra.rs
@@ -1,15 +1,29 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use crate::{Map, Point, Solver};
 
 pub struct Dijkstra {
     map: Map,
-    unvisited: HashSet<Node>,
+    unvisited: HashMap<Point, Node>,
     current: Node,
 }
 
 impl Solver for Dijkstra {
     fn solve(&mut self) -> Option<Vec<crate::Point>> {
+        // get the next unvisited nodes from where we are
+        let nexts = self
+            .map
+            .next_moves_from(&self.current.point)
+            .into_iter()
+            .flatten()
+            .filter(|p| self.unvisited.contains_key(p))
+            .collect::<Vec<_>>();
+
+        for next in nexts {
+            let node = self.unvisited.get_mut(&next).unwrap();
+            dbg!(node);
+        }
+
         None
     }
 }
@@ -17,13 +31,13 @@ impl Solver for Dijkstra {
 impl Dijkstra {
     pub fn new(map: Map) -> Self {
         let current = Node::new(map.start, 0);
-        let mut unvisited = HashSet::new();
+        let mut unvisited = HashMap::new();
         for row in 0..map.rows {
             for col in 0..map.cols {
                 let point = Point(row, col);
                 if point != map.start {
                     let node = Node::new(point, i64::MAX);
-                    unvisited.insert(node);
+                    unvisited.insert(point, node);
                 }
             }
         }
