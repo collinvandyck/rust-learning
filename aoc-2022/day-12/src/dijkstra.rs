f3eb7db1584fcc4830965e4e19842d19dca1308b
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-19 12:29:22 -0400

    wip

--- aoc-2022/day-12/src/dijkstra.rs
+++ aoc-2022/day-12/src/dijkstra.rs
@@ -5,50 +5,61 @@ use crate::{Map, Point, Solver};
 pub struct Dijkstra {
     map: Map,
     unvisited: HashMap<Point, Node>,
-    visited: HashSet<Node>,
+    visited: HashMap<Point, Node>,
 }
 
 impl Solver for Dijkstra {
-    fn solve(&mut self) -> Option<Vec<crate::Point>> {
-        // get the current node
-        let cur_point = match self.next_point() {
-            Some(point) => point,
-            None => {
-                eprintln!("Ran out of nodes!");
-                return None;
+    fn solve(&mut self) -> Option<Vec<Point>> {
+        while let Some(cur_point) = self.next_point() {
+            let current = self.unvisited.remove(&cur_point).unwrap();
+            // get the next unvisited nodes from where we are
+            let nexts = self
+                .map
+                .next_moves_from(&cur_point)
+                .into_iter()
+                .flatten()
+                .filter(|p| self.unvisited.contains_key(p))
+                .collect::<Vec<_>>();
+            for next in nexts {
+                let node: &mut Node = self.unvisited.get_mut(&next).unwrap();
+                let distance = current.distance + 1;
+                // set the new distance on the node if it's shorter
+                if distance < node.distance {
+                    node.distance = distance;
+                }
             }
-        };
-        let current = self.unvisited.remove(&cur_point).unwrap();
-        dbg!(current);
-
-        if current.point == self.map.finish {
-            println!("We're done!");
-            return None;
-        }
-
-        // get the next unvisited nodes from where we are
-        let nexts = self
-            .map
-            .next_moves_from(&cur_point)
-            .into_iter()
-            .flatten()
-            .filter(|p| self.unvisited.contains_key(p))
-            .collect::<Vec<_>>();
-
-        for next in nexts {
-            let node: &mut Node = self.unvisited.get_mut(&next).unwrap();
-            let distance = current.distance + 1;
-            // set the new distance on the node if it's shorter
-            if distance < node.distance {
-                node.distance = distance;
+            self.visited.insert(current.point, current);
+            if current.point == self.map.finish {
+                println!("We're done!");
+                return Some(self.return_path());
             }
-            dbg!(node);
         }
         None
     }
 }
 
 impl Dijkstra {
+    // if successfully completed, return the shortest path from the start to finish
+    fn return_path(&self) -> Vec<Point> {
+        dbg!(self.visited.get(&self.map.finish).unwrap().distance);
+        let mut res = vec![];
+        let mut cur = self.map.start;
+        res.push(cur);
+        while cur != self.map.finish {
+            cur = self
+                .map
+                .next_moves_from(&cur)
+                .into_iter()
+                .flatten()
+                .filter(|p| !res.contains(&p))
+                .map(|p| self.visited.get(&p).unwrap())
+                .reduce(|n1, n2| if n1.distance < n2.distance { n1 } else { n2 })
+                .map(|n| n.point)
+                .unwrap();
+            res.push(cur);
+        }
+        res
+    }
     // find the smallest distance node in the unvisited set
     fn next_point(&self) -> Option<Point> {
         self.unvisited
@@ -71,7 +82,7 @@ impl Dijkstra {
                 unvisited.insert(point, node);
             }
         }
-        let visited = HashSet::new();
+        let visited = HashMap::new();
         Self {
             map,
             unvisited,
