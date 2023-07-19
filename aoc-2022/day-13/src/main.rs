d143aa3a281bff73bfcc0ae896bd075a0b1a1b0b
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-19 13:26:29 -0400

    wip

--- aoc-2022/day-13/src/main.rs
+++ aoc-2022/day-13/src/main.rs
@@ -1,3 +1,23 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
 fn main() {
-    println!("Hello, world!");
+    part_one("example.txt");
+}
+
+fn part_one(filename: &str) {
+    let file = File::open(filename).unwrap();
+    let read = BufReader::new(file);
+    let mut iter = read.lines().flatten();
+    loop {
+        let one = iter.next().unwrap();
+        let two = iter.next().unwrap();
+        println!("{}\n{}", one.trim(), two.trim());
+        if iter.next().is_none() {
+            break;
+        }
+        println!();
+    }
 }
