90a71e5ecf4a5c14e9a65709de0440937596a2c9
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-25 21:08:53 -0400

    wip

--- aoc-2022/day-18/src/main.rs
+++ aoc-2022/day-18/src/main.rs
@@ -8,8 +8,8 @@ use clap::Parser;
 fn main() {
     let args = Args::parse();
     for line in BufReader::new(File::open(&args.filename).unwrap()).lines() {
-        let line = line.unwrap();
-        println!("{line}");
+        let point = Point::parse(line.unwrap());
+        println!("{point:?}");
     }
 }
 
@@ -17,4 +17,21 @@ fn main() {
 struct Args {
     #[arg(short, default_value = "example.txt")]
     filename: String,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Point {
+    x: u64,
+    y: u64,
+    z: u64,
+}
+
+impl Point {
+    fn parse(line: String) -> Self {
+        let mut iter = line.split(',');
+        let x = iter.next().unwrap().parse::<u64>().unwrap();
+        let y = iter.next().unwrap().parse::<u64>().unwrap();
+        let z = iter.next().unwrap().parse::<u64>().unwrap();
+        Self { x, y, z }
+    }
 }
