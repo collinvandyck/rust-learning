dceabb9faae0be24a2619c99f4290cc4ba0af122
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-26 16:23:48 -0400

    wip

--- aoc-2022/day-18/src/main.rs
+++ aoc-2022/day-18/src/main.rs
@@ -19,7 +19,7 @@ struct Args {
     filename: String,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct Point(u32, u32, u32);
 impl Point {
     fn parse(line: String) -> Self {
@@ -76,5 +76,28 @@ impl Point {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[test]
+fn test_point_squares() {
+    let cube = Point(1, 1, 1);
+    let squares = cube.squares();
+    assert_eq!(
+        squares,
+        [
+            // front
+            Square(Point(0, 1, 1), Point(1, 0, 1)),
+            // back
+            Square(Point(0, 1, 0), Point(1, 0, 0)),
+            // top
+            Square(Point(0, 1, 0), Point(1, 1, 1)),
+            // bottom
+            Square(Point(0, 0, 0), Point(1, 0, 1)),
+            // left
+            Square(Point(0, 0, 0), Point(0, 1, 1)),
+            // right
+            Square(Point(1, 0, 0), Point(1, 1, 1)),
+        ]
+    )
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct Square(Point, Point);
