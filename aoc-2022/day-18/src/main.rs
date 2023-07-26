8881ef5cc6b81027882af0c1a3799f0d89983bc6
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-26 16:20:27 -0400

    wip

--- aoc-2022/day-18/src/main.rs
+++ aoc-2022/day-18/src/main.rs
@@ -8,8 +8,8 @@ use clap::Parser;
 fn main() {
     let args = Args::parse();
     for line in BufReader::new(File::open(&args.filename).unwrap()).lines() {
-        let cube = Cube::parse(line.unwrap());
-        println!("{cube:?}");
+        let point = Point::parse(line.unwrap());
+        println!("{point:?}");
     }
 }
 
@@ -20,24 +20,61 @@ struct Args {
 }
 
 #[derive(Debug, Clone, Copy)]
-struct Cube {
-    x: u64,
-    y: u64,
-    z: u64,
-}
-
-impl Cube {
+struct Point(u32, u32, u32);
+impl Point {
     fn parse(line: String) -> Self {
         let mut iter = line.split(',');
-        let x = iter.next().unwrap().parse::<u64>().unwrap();
-        let y = iter.next().unwrap().parse::<u64>().unwrap();
-        let z = iter.next().unwrap().parse::<u64>().unwrap();
-        Self { x, y, z }
+        let x = iter.next().unwrap().parse::<u32>().unwrap();
+        let y = iter.next().unwrap().parse::<u32>().unwrap();
+        let z = iter.next().unwrap().parse::<u32>().unwrap();
+        Self(x, y, z)
     }
+
     /// Each cube is comprised of 6 squares.
-    /// A square is defined by
     ///
-    /// (1,1,1) has the following squares,
+    /// (1,1,1) has the following squares
     ///
-    fn squares(&self) {}
+    /// (0,1,1) x (1,0,1) - front
+    /// (0,1,0) x (1,0,0) - back
+    /// (0,1,0) x (1,1,1) - top
+    /// (0,0,0) x (1,0,1) - bottom
+    /// (0,0,0) x (0,1,1) - left
+    /// (1,0,0) x (1,1,1) - right
+    fn squares(&self) -> [Square; 6] {
+        [
+            // front
+            Square(
+                Point(self.0 - 1, self.1, self.2),
+                Point(self.0, self.1 - 1, self.2),
+            ),
+            // back
+            Square(
+                Point(self.0 - 1, self.1, self.2 - 1),
+                Point(self.0, self.1 - 1, self.2 - 1),
+            ),
+            // top
+            Square(
+                Point(self.0 - 1, self.1, self.2 - 1),
+                Point(self.0, self.1, self.2),
+            ),
+            // bottom
+            Square(
+                Point(self.0 - 1, self.1 - 1, self.2 - 1),
+                Point(self.0, self.1 - 1, self.2),
+            ),
+            // left
+            Square(
+                Point(self.0 - 1, self.1 - 1, self.2 - 1),
+                Point(self.0 - 1, self.1, self.2),
+            ),
+            // right
+            Square(
+                Point(self.0, self.1 - 1, self.2 - 1),
+                Point(self.0, self.1, self.2),
+            ),
+        ]
+    }
 }
+
+#[derive(Debug, Clone, Copy)]
+struct Square(Point, Point);
