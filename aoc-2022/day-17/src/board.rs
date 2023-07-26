57b123ac4dbdd83f110df2cd172483f34cd151e6
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-25 20:18:38 -0400

    wip

--- aoc-2022/day-17/src/board.rs
+++ aoc-2022/day-17/src/board.rs
@@ -27,20 +27,28 @@ impl Board {
 
     /// alternates between gusting and dropping until the entity comes to rest,
     /// at which point the method will exit.
-    fn drop(&mut self, entity: Entity) {
+    fn drop(&mut self, mut entity: Entity) {
         if let Some(gust) = self.gusts.next() {
             let adjustment = match gust {
                 Gust::Left => Point(-1, 0),
                 Gust::Right => Point(1, 0),
             };
-            if self.is_space_available(&entity, adjustment) {
-                // make the adjustment
+            let candidate_points = entity.points.add(adjustment);
+            if self.is_space_available(&entity, &candidate_points) {
+                entity.points = candidate_points;
             }
         }
         self.entities.push(entity);
     }
 
-    fn is_space_available(&self, entity: &Entity, adjustment: Point) -> bool {
+    fn is_space_available(&self, entity: &Entity, points: &Points) -> bool {
+        // first check to make sure the points are in bounds
+        if !points.in_bounds(self.width) {
+            return false;
+        }
+        // next check to make sure that there is space on the board. if there
+        // are any points that overlap, we must return false.
+
         todo!()
     }
 
@@ -125,6 +133,24 @@ struct Entity {
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Point(pub i32, pub i32);
 
+impl Point {
+    fn in_bounds(&self, width: i32) -> bool {
+        self.0 > 0 && self.0 <= width && self.1 > 0
+    }
+}
+
+#[test]
+fn test_point_in_bounds() {
+    let width = 7_i32;
+    assert!(Point(1, 1).in_bounds(width));
+    assert!(Point(2, 2).in_bounds(width));
+    assert!(Point(3, 7).in_bounds(width));
+    assert!(Point(7, 9).in_bounds(width));
+    assert!(!Point(8, 9).in_bounds(width));
+    assert!(!Point(5, 0).in_bounds(width));
+    assert!(!Point(1, 0).in_bounds(width));
+}
+
 impl AddAssign for Point {
     fn add_assign(&mut self, rhs: Self) {
         self.0 += rhs.0;
@@ -136,6 +162,9 @@ impl AddAssign for Point {
 struct Points(Vec<Point>);
 
 impl Points {
+    pub fn in_bounds(&self, width: i32) -> bool {
+        self.0.iter().all(|p| p.in_bounds(width))
+    }
     pub fn add(&self, point: Point) -> Points {
         Points(
             self.0
