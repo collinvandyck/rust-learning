e513f21c49a2c078422c790fac5dfc5bde6932ab
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-26 15:58:56 -0400

    wip

--- aoc-2022/day-18/src/main.rs
+++ aoc-2022/day-18/src/main.rs
@@ -8,8 +8,8 @@ use clap::Parser;
 fn main() {
     let args = Args::parse();
     for line in BufReader::new(File::open(&args.filename).unwrap()).lines() {
-        let point = Point::parse(line.unwrap());
-        println!("{point:?}");
+        let cube = Cube::parse(line.unwrap());
+        println!("{cube:?}");
     }
 }
 
@@ -20,13 +20,13 @@ struct Args {
 }
 
 #[derive(Debug, Clone, Copy)]
-struct Point {
+struct Cube {
     x: u64,
     y: u64,
     z: u64,
 }
 
-impl Point {
+impl Cube {
     fn parse(line: String) -> Self {
         let mut iter = line.split(',');
         let x = iter.next().unwrap().parse::<u64>().unwrap();
@@ -34,4 +34,10 @@ impl Point {
         let z = iter.next().unwrap().parse::<u64>().unwrap();
         Self { x, y, z }
     }
+    /// Each cube is comprised of 6 squares.
+    /// A square is defined by
+    ///
+    /// (1,1,1) has the following squares,
+    ///
+    fn squares(&self) {}
 }
