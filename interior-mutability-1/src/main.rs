036c69279e68b4cd4ca03bb39d3f7f2024639c2e
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-27 20:30:18 -0400

    wip

--- interior-mutability-1/src/main.rs
+++ interior-mutability-1/src/main.rs
@@ -1,12 +1,12 @@
+#![allow(unused_variables, dead_code)]
 use std::rc::Rc;
 
-fn main() {
-    rc_panic_example();
-}
+fn main() {}
 
 fn rc_panic_example() {
     let mut r = Rc::new(String::new());
     let _r2 = r.clone();
+    // fails because we have another reference to the rc, so get_mut will return a None.
     Rc::get_mut(&mut r).unwrap().push_str("foo");
     println!("{}", r);
 }
