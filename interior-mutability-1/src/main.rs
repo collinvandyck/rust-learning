80c0417081006275ca186ccf10c730af408cea20
Collin Van Dyck <collinvandyck@gmail.com>
2023-07-27 20:29:18 -0400

    wip

--- interior-mutability-1/src/main.rs
+++ interior-mutability-1/src/main.rs
@@ -1,3 +1,12 @@
+use std::rc::Rc;
+
 fn main() {
-    println!("Hello, world!");
+    rc_panic_example();
+}
+
+fn rc_panic_example() {
+    let mut r = Rc::new(String::new());
+    let _r2 = r.clone();
+    Rc::get_mut(&mut r).unwrap().push_str("foo");
+    println!("{}", r);
 }
