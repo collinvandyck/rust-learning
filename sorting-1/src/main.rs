553162cf95c024e3fb15253bfcdc028c4887e353
Collin Van Dyck <collinvandyck@gmail.com>
2023-08-02 23:53:47 -0400

    wip

--- sorting-1/src/main.rs
+++ sorting-1/src/main.rs
@@ -17,8 +17,8 @@ mod tests {
     {
         let name = S::name();
         println!("Testing {name}");
-        let mut nums = [1, 5, 3, 2, 4];
+        let mut nums = [9, 1, 3, 4, 8, 7, 2, 6, 0, 5];
         S::sort(&mut nums);
-        assert_eq!(nums, [1, 2, 3, 4, 5]);
+        assert_eq!(nums, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
     }
 }
