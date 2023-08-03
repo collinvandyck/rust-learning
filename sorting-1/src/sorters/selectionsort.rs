c20dc3324f05ca87c875775c6e5005df30a11642
Collin Van Dyck <collinvandyck@gmail.com>
2023-08-03 17:59:09 -0400

    wip

--- sorting-1/src/sorters/selectionsort.rs
+++ sorting-1/src/sorters/selectionsort.rs
@@ -10,5 +10,19 @@ impl Sorter for SelectionSort {
     where
         T: Ord,
     {
+        for i in 0..slice.len() {
+            let x = slice[i..slice.len()].iter().enumerate().min_by_key(|f| f.1);
+            if let Some((idx, _)) = x {
+                let idx = idx + i;
+                slice.swap(i, idx);
+            }
+        }
     }
+}
+
+#[test]
+fn test_selection_sort() {
+    let mut nums = [3, 5];
+    SelectionSort::sort(&mut nums);
+    assert_eq!(nums, [3, 5]);
 }
