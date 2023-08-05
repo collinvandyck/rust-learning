a064d413dc137b3e221c401fa16e488f64fe6b40
Collin Van Dyck <collinvandyck@gmail.com>
2023-08-05 10:23:38 -0400

    wip

--- iter-3/src/tree.rs
+++ iter-3/src/tree.rs
@@ -3,6 +3,22 @@ pub enum Tree<K, V> {
     NonEmpty(Node<K, V>),
 }
 
+impl<K, V> Tree<K, V>
+where
+    K: Ord,
+    V: PartialEq,
+{
+    pub fn new() -> Self {
+        Self::Empty
+    }
+    pub fn size(&self) -> usize {
+        match *self {
+            Tree::Empty => 0,
+            Tree::NonEmpty(_) => 1,
+        }
+    }
+}
+
 pub struct Node<K, V> {
     key: K,
     val: V,
@@ -14,4 +30,15 @@ where
     K: Ord,
     V: PartialEq,
 {
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_tree() {
+        let t: Tree<&'static str, i32> = Tree::new();
+        assert_eq!(t.size(), 0);
+    }
 }
