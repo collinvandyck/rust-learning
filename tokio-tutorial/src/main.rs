a905b672ab9340d95c256c09ee9b71d536e0b09f
Collin Van Dyck <collinvandyck@gmail.com>
2023-08-06 11:54:56 -0400

    wip

--- tokio-tutorial/src/main.rs
+++ tokio-tutorial/src/main.rs
@@ -1,27 +1,48 @@
-use mini_redis::{Connection, Frame};
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use bytes::Bytes;
+
+use mini_redis::{Command, Connection, Frame};
 use tokio::net::{TcpListener, TcpStream};
-use tokio::task::yield_now;
+
+type Db = Arc<Mutex<HashMap<String,Bytes>>>;
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    let db = Arc::new(Mutex::new(HashMap::new()));
     loop {
         let (foobar, _addr) = listener.accept().await.unwrap();
+        let db = db.clone();
         tokio::spawn(async move {
-            process(foobar).await;
-        });
-        tokio::spawn(async {
-            println!("hi there") ;
-            yield_now().await;
+            process(db, foobar).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
+async fn process(db: Db, socket: TcpStream) {
     let mut conn = Connection::new(socket);
-    if let Some(frame) = conn.read_frame().await.unwrap() {
-        println!("Got: {:?}", frame);
-        let response = Frame::Error("unimplemented".to_string());
+    while let Some(frame) = conn.read_frame().await.unwrap() {
+        println!("Got frame: {frame:?}");
+        let response = match Command::from_frame(frame).unwrap() {
+            Command::Set(cmd) => {
+                println!("Set frame");
+                db.lock().unwrap().insert(cmd.key().to_string(), cmd.value().clone());
+                Frame::Simple("OK".to_string())
+            }
+            Command::Get(cmd) => {
+                println!("Get frame");
+                let db = db.lock().unwrap();
+                let value = db.get(cmd.key());
+                if let Some(value) = value {
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("Unsupported command: {:?}", cmd),
+        };
+        println!("Writing response: {response:?}");
         conn.write_frame(&response).await.unwrap();
     }
 }
