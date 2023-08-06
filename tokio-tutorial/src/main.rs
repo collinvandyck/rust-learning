39e4ff655a105460efe56b42095eed0154ee8107
Collin Van Dyck <collinvandyck@gmail.com>
2023-08-06 10:55:49 -0400

    wip

--- tokio-tutorial/src/main.rs
+++ tokio-tutorial/src/main.rs
@@ -1,10 +1,3 @@
-use mini_redis::{client,Result};
-
 #[tokio::main]
-async fn main() -> Result<()> {
-    let mut client = client::connect("127.0.0.1:6379").await?;
-    client.set("hello", "world".into()).await?;
-    let result = client.get("hello").await?;
-    println!("Got message from server; result={result:?}");
-    Ok(())
+async fn main() {
 }
