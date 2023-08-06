79a567da6130a8dc21ddcfee9b01dae869a96e96
Collin Van Dyck <collinvandyck@gmail.com>
2023-08-06 13:50:35 -0400

    wip

--- select-1/src/main.rs
+++ select-1/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::sync::oneshot;
+
+#[tokio::main]
+async fn main() {
+    let (tx1, rx1) = oneshot::channel();
+    let (tx2, rx2) = oneshot::channel();
+    tokio::spawn(async {
+        let _ = tx1.send("one");
+    });
+    tokio::spawn(async {
+        let _ = tx2.send("two");
+    });
+    tokio::select! {
+        val = rx1 => {
+            println!("rx1 first with {val:?}");
+        }
+        val = rx2 => {
+            println!("rx2 first with {val:?}");
+        }
+    }
 }
