2a98a32de494d785f44109f7d660fcb58beb176d
Collin Van Dyck <collinvandyck@gmail.com>
2023-08-06 11:13:13 -0400

    wip

--- tokio-tutorial/src/main.rs
+++ tokio-tutorial/src/main.rs
@@ -1,9 +1,21 @@
-use tokio::net::TcpListener;
+use mini_redis::{Connection, Frame};
+use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
     loop {
-        let (stream, socket) = listener.accept().await.unwrap();
+        let (foobar, _addr) = listener.accept().await.unwrap();
+        process(foobar).await;
     }
 }
+
+async fn process(socket: TcpStream) {
+    let mut conn = Connection::new(socket);
+    if let Some(frame) = conn.read_frame().await.unwrap() {
+        println!("Got: {:?}", frame);
+        let response = Frame::Error("unimplemented".to_string());
+        conn.write_frame(&response).await.unwrap();
+    }
+}
+
