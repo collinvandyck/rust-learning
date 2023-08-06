99dacafc17e34ba648c4ed85908c262aba1804c6
Collin Van Dyck <collinvandyck@gmail.com>
2023-08-06 14:18:19 -0400

    wip

--- select-1/src/main.rs
+++ select-1/src/main.rs
@@ -1,21 +1,31 @@
+use std::future::Future;
 use tokio::sync::oneshot;
 
 #[tokio::main]
 async fn main() {
-    let (tx1, rx1) = oneshot::channel();
-    let (tx2, rx2) = oneshot::channel();
+    let (tx1, mut rx1) = oneshot::channel();
+    let (tx2, mut rx2) = oneshot::channel();
     tokio::spawn(async {
         let _ = tx1.send("one");
     });
     tokio::spawn(async {
         let _ = tx2.send("two");
     });
+    let mut one = None;
+    let mut two = None;
+    while one.is_none() || two.is_none() {
+        tokio::select! {
+            val = (&mut rx1), if one.is_none() => one = Some(val),
+            val = (&mut rx2), if two.is_none() => two = Some(val),
+        }
+    }
+    println!("{one:?} {two:?}");
+}
+
+async fn get(rx1: impl Future<Output=&str>) {
     tokio::select! {
         val = rx1 => {
             println!("rx1 first with {val:?}");
-        }
-        val = rx2 => {
-            println!("rx2 first with {val:?}");
         }
     }
 }
