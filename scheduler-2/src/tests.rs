ceb6800e1d2a84afe45c8ea583d41700f918c4b3
Collin Van Dyck <collinvandyck@gmail.com>
2023-08-13 11:57:09 -0400

    wip

--- scheduler-2/src/tests.rs
+++ scheduler-2/src/tests.rs
@@ -1,3 +1,4 @@
+use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
 use crate::hooks::HookResult;
@@ -23,24 +24,29 @@ async fn test_scheduler() -> Result<()> {
 #[tokio::test]
 async fn test_scheduler_hooks() -> Result<()> {
     let hooks = TestHooks::new();
-    let sched = Scheduler::builder().hooks(hooks).build();
-    sched
-        .task("task", async {
-            sleep(Duration::from_millis(0)).await;
-            println!("task");
-        })
-        .await?;
+    let sched = Scheduler::builder().hooks(hooks.clone()).build();
+    for _ in 0..10 {
+        sched.task("task", async {}).await?;
+    }
     sched.wait().await?;
+    assert_eq!(10, hooks.get_count());
     Ok(())
 }
 
+#[derive(Clone)]
 struct TestHooks {
-    count: usize,
+    count: Arc<Mutex<usize>>,
 }
 
 impl TestHooks {
     fn new() -> Self {
-        TestHooks { count: 0 }
+        TestHooks {
+            count: Arc::new(Mutex::new(0)),
+        }
+    }
+    fn get_count(&self) -> usize {
+        let count = self.count.lock().unwrap();
+        *count
     }
 }
 
@@ -48,6 +54,8 @@ impl TestHooks {
 impl Hooks for TestHooks {
     async fn on_task_start(&mut self, typ: &TaskType) -> HookResult {
         println!("Hook: on_task_start: {:?}", typ);
+        let mut count = self.count.lock().unwrap();
+        *count = *count + 1;
         Ok(())
     }
 }
