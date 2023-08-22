b867ee8defb4d89f5cacc3441fd6b82152a41e95
Collin Van Dyck <collinvandyck@gmail.com>
2023-08-22 08:24:06 -0400

    wip

--- sqlx-migrate/src/main.rs
+++ sqlx-migrate/src/main.rs
@@ -3,7 +3,12 @@ use sqlx::sqlite::SqlitePoolOptions;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    create().await?;
     run().await?;
+    Ok(())
+}
+
+async fn create() -> Result<()> {
     Ok(())
 }
 
