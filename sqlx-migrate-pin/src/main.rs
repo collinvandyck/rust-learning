53be1e43bd9eaaa781afc322aea7c476fbb9a846
Collin Van Dyck <collinvandyck@gmail.com>
2023-08-23 10:35:49 -0400

    wip

--- sqlx-migrate-pin/src/main.rs
+++ sqlx-migrate-pin/src/main.rs
@@ -34,7 +34,7 @@ struct Response;
 
 #[async_trait]
 trait Migrator {
-    async fn migrate(&mut self);
+    async fn migrate(&mut self) -> Result<()>;
 }
 
 struct MyMigrator {
@@ -45,7 +45,14 @@ struct MyMigrator {
 impl MyMigrator {
     async fn run(&mut self) {
         while let Some(request) = self.mpsc.recv().await {
-            self.migrate().await;
+            let mr = self.migrate().await;
+            let Response = match mr {
+                Ok(_) => Response,
+                Err(e) => {
+                    eprintln!("migrate error: {:?}", e);
+                    Response
+                }
+            };
             request.tx.send(Response).unwrap();
         }
     }
@@ -53,15 +60,17 @@ impl MyMigrator {
 
 #[async_trait]
 impl Migrator for MyMigrator {
-    async fn migrate(&mut self) {
+    async fn migrate(&mut self) -> Result<()> {
         let fut = async move {
             let pool = self.pool.take().unwrap();
-            let conn = pool.acquire().await.unwrap();
-            let mut conn = conn.detach();
+            let pooled_conn = pool.acquire().await.unwrap();
+            let mut conn = pooled_conn.detach();
             let fut = sqlx::migrate!("./db").run_direct(&mut conn);
             let fut: Pin<Box<dyn Future<Output = Result<(), MigrateError>> + Send>> = Box::pin(fut);
-            let _ = fut.await;
+            let res = fut.await;
+            drop(conn);
+            res
         };
-        fut.await;
+        fut.await.map_err(|e| e.into())
     }
 }
