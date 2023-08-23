f0de55c1365e0a08b552e159cf486055de5721b3
Collin Van Dyck <collinvandyck@gmail.com>
2023-08-23 12:55:04 -0400

    wip

--- sqlx-migrate-mem/src/main.rs
+++ sqlx-migrate-mem/src/main.rs
@@ -3,53 +3,56 @@ use sqlx::{pool::PoolOptions, Pool, Sqlite};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    if let Err(e) = test_migrate_shared_conn().await {
-        println!("Shared conn test failed: {e}");
-    }
-    if let Err(e) = test_migrate_conn().await {
-        println!("Detached conn test failed: {e}");
+    let bs = [
+        MigrateBehavior::Shared,
+        MigrateBehavior::Detach,
+        MigrateBehavior::Leak,
+    ];
+    for b in bs {
+        if let Err(e) = test_migrate_conn(b.clone()).await {
+            println!("{b:?} conn test failed: {e}");
+        }
     }
     Ok(())
 }
 
-async fn migrate(pool: &mut Pool<Sqlite>, detach: bool) -> Result<()> {
+#[derive(Debug, Clone)]
+enum MigrateBehavior {
+    Shared,
+    Detach,
+    Leak,
+}
+
+async fn migrate(pool: &mut Pool<Sqlite>, behavior: MigrateBehavior) -> Result<()> {
     let mut conn = pool.acquire().await.context("acquire failed")?;
-    if detach {
-        let mut conn = conn.detach();
-        sqlx::migrate!("./migrations").run_direct(&mut conn).await?;
-    } else {
-        sqlx::migrate!("./migrations").run(&mut conn).await?;
+    match behavior {
+        MigrateBehavior::Shared => {
+            sqlx::migrate!("./migrations").run(&mut conn).await?;
+        }
+        MigrateBehavior::Detach => {
+            let mut conn = conn.detach();
+            sqlx::migrate!("./migrations").run_direct(&mut conn).await?;
+        }
+        MigrateBehavior::Leak => {
+            let mut conn = conn.leak();
+            sqlx::migrate!("./migrations").run_direct(&mut conn).await?;
+        }
     }
     Ok(())
 }
 
-async fn test_migrate_conn() -> Result<()> {
-    let mut pool: Pool<Sqlite> = PoolOptions::new()
+async fn new_pool() -> Result<Pool<Sqlite>> {
+    let pool = PoolOptions::new()
+        .min_connections(1)
         .max_connections(100)
         .connect("sqlite::memory:")
         .await?;
-    migrate(&mut pool, true).await?;
-
-    #[derive(PartialEq, Eq, Debug, sqlx::FromRow)]
-    struct Record(String);
-
-    let mut conn = pool.acquire().await?;
-    let records = sqlx::query_as::<_, Record>("select * from foo")
-        .fetch_all(&mut conn)
-        .await?;
-
-    assert_eq!(records.len(), 1);
-    assert_eq!(records.first(), Some(&Record("collin".to_string())));
-    Ok(())
+    Ok(pool)
 }
 
-async fn test_migrate_shared_conn() -> Result<()> {
-    let mut pool: Pool<Sqlite> = PoolOptions::new()
-        .max_connections(100)
-        .connect("sqlite::memory:")
-        .await?;
-
-    migrate(&mut pool, false).await?;
+async fn test_migrate_conn(b: MigrateBehavior) -> Result<()> {
+    let mut pool = new_pool().await?;
+    migrate(&mut pool, b).await?;
 
     #[derive(PartialEq, Eq, Debug, sqlx::FromRow)]
     struct Record(String);
