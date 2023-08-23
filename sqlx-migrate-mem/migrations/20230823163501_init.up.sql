33d9985c895eeef3f71190599ac2cfc6df0e43f2
Collin Van Dyck <collinvandyck@gmail.com>
2023-08-23 12:39:31 -0400

    wip

--- sqlx-migrate-mem/migrations/20230823163501_init.up.sql
+++ sqlx-migrate-mem/migrations/20230823163501_init.up.sql
@@ -1,3 +1,4 @@
 -- Add up migration script here
 
 create table foo(name string);
+insert into foo(name) values ('collin');
--- sqlx-migrate-mem/src/main.rs
+++ sqlx-migrate-mem/src/main.rs
@@ -9,5 +9,16 @@ async fn main() -> Result<()> {
         .await?;
     let mut conn = pool.acquire().await?;
     sqlx::migrate!("./migrations").run(&mut conn).await?;
+
+    #[derive(PartialEq, Eq, Debug, sqlx::FromRow)]
+    struct Record(String);
+
+    let mut conn = pool.acquire().await?;
+    let records = sqlx::query_as::<_, Record>("select * from foo")
+        .fetch_all(&mut conn)
+        .await?;
+
+    assert_eq!(records.len(), 1);
+    assert_eq!(records.first(), Some(&Record("collin".to_string())));
     Ok(())
 }
