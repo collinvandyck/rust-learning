bc110d48129a841dbc8b0b178818dd10d3e4fc24
Collin Van Dyck <collinvandyck@gmail.com>
2023-08-25 12:24:53 -0400

    wip

--- sqlx-migrate-mem-2/src/main.rs
+++ sqlx-migrate-mem-2/src/main.rs
@@ -1,24 +1,22 @@
-use anyhow::Result;
-use sqlx::{
-    sqlite::{SqliteConnectOptions, SqlitePoolOptions},
-    Pool, Sqlite,
-};
-use std::str::FromStr;
-
 fn main() {}
+
+#[derive(sqlx::FromRow, Debug, PartialEq, Eq)]
+struct Record(String);
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use anyhow::Result;
+    use sqlx::{
+        sqlite::{SqliteConnectOptions, SqlitePoolOptions},
+        Pool, Sqlite,
+    };
+    use std::str::FromStr;
 
-    use crate::connect;
     #[tokio::test]
     async fn test_migration() -> Result<()> {
         let pool = connect().await?;
         let mut conn = pool.acquire().await?;
-
-        #[derive(sqlx::FromRow, Debug, PartialEq, Eq)]
-        struct Record(String);
 
         let recs = sqlx::query_as::<_, Record>("select name from foo")
             .fetch_all(&mut conn)
@@ -28,24 +26,25 @@ mod tests {
 
         Ok(())
     }
-}
 
-async fn connect() -> Result<Pool<Sqlite>> {
-    let connection_str = "sqlite://?mode=memory&cache=private";
-    let opts = SqliteConnectOptions::from_str(connection_str)?;
-    let mut pool = SqlitePoolOptions::new()
-        .min_connections(1)
-        .max_connections(100)
-        .connect_with(opts)
-        .await?;
-    migrate(&mut pool).await?;
-    Ok(pool)
-}
+    async fn connect() -> Result<Pool<Sqlite>> {
+        let connection_str = "sqlite://?mode=memory&cache=private";
+        let opts = SqliteConnectOptions::from_str(connection_str)?;
+        let mut pool = SqlitePoolOptions::new()
+            .min_connections(2)
+            .max_connections(100)
+            .connect_with(opts)
+            .await?;
+        migrate(&mut pool).await?;
+        Ok(pool)
+    }
 
-async fn migrate(pool: &mut Pool<Sqlite>) -> Result<()> {
-    let conn = pool.acquire().await?;
-    let mut conn = conn.detach();
-    sqlx::migrate!("./migrations").run_direct(&mut conn).await?;
-    println!("migrated!");
-    Ok(())
+    async fn migrate(pool: &mut Pool<Sqlite>) -> Result<()> {
+        let conn = pool.acquire().await?;
+        let mut conn = conn.detach();
+        sqlx::migrate!("./migrations").run_direct(&mut conn).await?;
+        println!("migrated!");
+
+        Ok(())
+    }
 }
