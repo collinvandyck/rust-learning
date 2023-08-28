3f58dd5f7c880b616f663b9d382da93df8690714
Collin Van Dyck <collinvandyck@gmail.com>
2023-08-27 20:08:23 -0400

    wip

--- tracing-1/src/main.rs
+++ tracing-1/src/main.rs
@@ -1,3 +1,6 @@
+use std::time::Duration;
+
+use tokio::{sync::oneshot, time::sleep};
 use tracing::{event, info, Level};
 use tracing_attributes::instrument;
 use tracing_subscriber::FmtSubscriber;
@@ -7,7 +10,26 @@ async fn main() {
     let subscriber = FmtSubscriber::builder().finish();
     let guard = tracing::subscriber::set_default(subscriber);
     event!(Level::INFO, age = 48, "hi there");
+
+    let (tx, rx) = oneshot::channel();
+
+    tokio::spawn(async move {
+        tracing_subscriber::fmt::init();
+        info!("in async thing");
+        sleep(Duration::from_millis(10)).await;
+        info!("done async thing");
+        tx.send(()).unwrap();
+    });
+
+    rx.await.unwrap();
+
+    run().await;
+    sleep(Duration::from_millis(50)).await;
     drop(guard);
+}
+
+#[instrument]
+async fn run() {
     info!("Starting");
     do_stuff().await;
     info!("Done");
@@ -16,5 +38,5 @@ async fn main() {
 #[instrument]
 async fn do_stuff() {
     event!(Level::INFO, "foo bar");
-    info!("hi");
+    info!("do_stuff");
 }
