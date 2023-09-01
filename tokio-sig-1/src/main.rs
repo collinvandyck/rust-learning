049731a43b2bf2f5136d94efe269acf6a2c3502a
Collin Van Dyck <collinvandyck@gmail.com>
2023-09-01 18:48:46 -0400

    wip

--- tokio-sig-1/src/main.rs
+++ tokio-sig-1/src/main.rs
@@ -1,3 +1,68 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Duration;
+
+use anyhow::Result;
+use tokio::time::sleep;
+
+#[tokio::main]
+async fn main() {
+    let id = std::process::id();
+    println!("pid: {id}");
+    loop {
+        tokio::select! {
+            sig = sig::signals() => {
+                match sig {
+                    Ok(sig) => {
+                        println!("Got sig: {sig:?}");
+                    }
+                    Err(e) => {
+                        eprintln!("Could not listen for signals: {e}");
+                        sleep(Duration::from_secs(1)).await;
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Signal {
+    HUP,
+    INT,
+    TERM,
+}
+
+#[cfg(not(windows))]
+pub(crate) mod sig {
+    use super::*;
+    use tokio::signal::unix::{signal, SignalKind};
+    pub(crate) async fn signals() -> Result<Signal> {
+        let mut sig_hup = signal(SignalKind::hangup())?;
+        let mut sig_int = signal(SignalKind::interrupt())?;
+        let mut sig_term = signal(SignalKind::terminate())?;
+        loop {
+            tokio::select! {
+                _ = sig_hup.recv() => return Ok(Signal::HUP),
+                _ = sig_int.recv() => return Ok(Signal::INT),
+                _ = sig_term.recv() => return Ok(Signal::TERM),
+            }
+        }
+    }
+}
+
+#[cfg(windows)]
+pub(crate) mod sig {
+    use super::*;
+    use tokio::signal::unix::{signal, SignalKind};
+    pub(crate) async fn signals() -> Result<Signal> {
+        let mut sig_hup = signal(SignalKind::hangup())?;
+        let mut sig_int = signal(SignalKind::interrupt())?;
+        let mut sig_term = signal(SignalKind::terminate())?;
+        loop {
+            tokio::select! {
+                _ = sig_hup.recv() => return Ok(Signal::HUP),
+                _ = sig_int.recv() => return Ok(Signal::INT),
+                _ = sig_term.recv() => return Ok(Signal::TERM),
+            }
+        }
+    }
 }
