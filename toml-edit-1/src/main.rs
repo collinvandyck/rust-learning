0601c71647f1aacb8f80e473d63e5dbda38c2e2e
Collin Van Dyck <collinvandyck@gmail.com>
2023-09-02 20:32:38 -0400

    wip

--- toml-edit-1/src/main.rs
+++ toml-edit-1/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
+use anyhow::anyhow;
+use anyhow::bail;
+use anyhow::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let home = home::home_dir().ok_or_else(|| anyhow!("No home directory"))?;
+    let config = home.join(".aws").join("config");
+    if !config.is_file() {
+        bail!("expected {config:?} to exist");
+    }
     println!("Hello, world!");
+    Ok(())
 }
