76a2777fdb97bdd204ec3045c75a4c2b5985cecd
Collin Van Dyck <collinvandyck@gmail.com>
2023-09-05 19:53:56 -0400

    wip

--- macros-1/src/main.rs
+++ macros-1/src/main.rs
@@ -1,7 +1,12 @@
+mod macros;
+
 use anyhow::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-	println!("Hello, world!");
-	Ok(())
+    let _v = vec![1, 2, 3];
+    println!("Hello, world!");
+    let _v = myvec!(1, 2, 3);
+    println!("{_v:?}");
+    Ok(())
 }
