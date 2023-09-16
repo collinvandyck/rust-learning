397cb8fc553d65e88961aaf58c8d13018f2b52c4
Collin Van Dyck <collinvandyck@gmail.com>
2023-09-16 08:39:13 -0400

    wip

--- ratatui-1/src/main.rs
+++ ratatui-1/src/main.rs
@@ -4,7 +4,13 @@ use crossterm::{
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use ratatui::{prelude::CrosstermBackend, widgets::Paragraph, Frame, Terminal};
+use ratatui::{
+    prelude::{Constraint, CrosstermBackend},
+    style::{Color, Modifier, Style},
+    text::{Line, Span},
+    widgets::{Block, Cell, Paragraph, Row, Table},
+    Frame, Terminal,
+};
 use std::{
     io::{self, Stdout},
     process,
@@ -26,12 +32,63 @@ fn main() -> Result<()> {
 
 fn run(term: &mut Term) -> Result<()> {
     loop {
-        term.draw(render)?;
+        term.draw(render_table)?;
         if should_quit()? {
             break;
         }
     }
     Ok(())
+}
+
+fn render_table(frame: &mut Frame<CrosstermBackend<Stdout>>) {
+    let table = Table::new(vec![
+        // Row can be created from simple strings.
+        Row::new(vec!["Row11", "Row12", "Row13"]),
+        // You can style the entire row.
+        Row::new(vec!["Row21", "Row22", "Row23"]).style(Style::default().fg(Color::Blue)),
+        // If you need more control over the styling you may need to create Cells directly
+        Row::new(vec![
+            Cell::from("Row31"),
+            Cell::from("Row32").style(Style::default().fg(Color::Yellow)),
+            Cell::from(Line::from(vec![
+                Span::raw("Row"),
+                Span::styled("33", Style::default().fg(Color::Green)),
+            ])),
+        ]),
+        // If a Row need to display some content over multiple lines, you just have to change
+        // its height.
+        Row::new(vec![
+            Cell::from("Row\n41"),
+            Cell::from("Row\n42"),
+            Cell::from("Row\n43"),
+        ])
+        .height(2),
+    ])
+    // You can set the style of the entire Table.
+    .style(Style::default().fg(Color::White))
+    // It has an optional header, which is simply a Row always visible at the top.
+    .header(
+        Row::new(vec!["Col1", "Col2", "Col3"])
+            .style(Style::default().fg(Color::Yellow))
+            // If you want some space between the header and the rest of the rows, you can always
+            // specify some margin at the bottom.
+            .bottom_margin(1),
+    )
+    // As any other widget, a Table can be wrapped in a Block.
+    .block(Block::default().title("Table"))
+    // Columns widths are constrained in the same way as Layout...
+    .widths(&[
+        Constraint::Length(5),
+        Constraint::Length(5),
+        Constraint::Length(10),
+    ])
+    // ...and they can be separated by a fixed spacing.
+    .column_spacing(1)
+    // If you wish to highlight a row in any specific way when it is selected...
+    .highlight_style(Style::default().add_modifier(Modifier::BOLD))
+    // ...and potentially show a symbol in front of the selection.
+    .highlight_symbol(">>");
+    frame.render_widget(table, frame.size());
 }
 
 fn render(frame: &mut Frame<CrosstermBackend<Stdout>>) {
