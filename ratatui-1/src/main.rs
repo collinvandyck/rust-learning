f5f9c207ce54b998745c8a5deefe9280aafa94bc
Collin Van Dyck <collinvandyck@gmail.com>
2023-09-16 08:32:05 -0400

    wip

--- ratatui-1/src/main.rs
+++ ratatui-1/src/main.rs
@@ -1,18 +1,54 @@
 use anyhow::{Context, Result};
 use crossterm::{
+    event::{self, Event, KeyCode},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use ratatui::{prelude::CrosstermBackend, Terminal};
-use std::io::{self, Stdout};
+use ratatui::{prelude::CrosstermBackend, widgets::Paragraph, Frame, Terminal};
+use std::{
+    io::{self, Stdout},
+    process,
+    time::Duration,
+};
+
+type Term = ratatui::Terminal<CrosstermBackend<Stdout>>;
 
 fn main() -> Result<()> {
     let mut term = setup_terminal().context("setup term")?;
+    let res = run(&mut term).context("run");
     restore_terminal(&mut term).context("restore term")?;
+    if let Err(err) = res {
+        eprintln!("{err}");
+        process::exit(1);
+    }
     Ok(())
 }
 
-fn setup_terminal() -> Result<Terminal<CrosstermBackend<Stdout>>> {
+fn run(term: &mut Term) -> Result<()> {
+    loop {
+        term.draw(render)?;
+        if should_quit()? {
+            break;
+        }
+    }
+    Ok(())
+}
+
+fn render(frame: &mut Frame<CrosstermBackend<Stdout>>) {
+    let greeting = Paragraph::new("Hello World! (press 'q' to quit)");
+    frame.render_widget(greeting, frame.size());
+}
+
+fn should_quit() -> Result<bool> {
+    if event::poll(Duration::from_millis(250)).context("event poll")? {
+        if let Event::Key(key) = event::read().context("event read")? {
+            return Ok(KeyCode::Char('q') == key.code);
+        }
+    }
+    Ok(false)
+}
+
+fn setup_terminal() -> Result<Term> {
     let mut stdout = io::stdout();
     enable_raw_mode()?;
     execute!(stdout, EnterAlternateScreen).context("failed to enter alt screen")?;
@@ -20,7 +56,7 @@ fn setup_terminal() -> Result<Terminal<CrosstermBackend<Stdout>>> {
     Terminal::new(backend).context("create terminal")
 }
 
-fn restore_terminal(term: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<()> {
+fn restore_terminal(term: &mut Term) -> Result<()> {
     disable_raw_mode().context("disable raw mode")?;
     execute!(term.backend_mut(), LeaveAlternateScreen).context("leave alt screen")?;
     term.show_cursor().context("show cursor")
