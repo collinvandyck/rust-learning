3cc59fefece13066bbf6f95fd58af71c6eafbcd1
Collin Van Dyck <collinvandyck@gmail.com>
2023-09-17 09:42:45 -0400

    wip

--- rql/src/app.rs
+++ rql/src/app.rs
@@ -2,7 +2,7 @@ use std::{io::Stdout, time::Duration};
 
 use crate::{dao::BlockingDao, widgets::Tables};
 use anyhow::{Context, Result};
-use crossterm::event::{self, Event, KeyCode, KeyModifiers};
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyModifiers};
 use ratatui::{
     prelude::CrosstermBackend,
     widgets::{List, ListItem},
@@ -17,24 +17,26 @@ pub enum Tick {
 
 pub struct App {
     dao: BlockingDao,
+    tables: Tables,
 }
 
 impl App {
     pub fn new<P: AsRef<str>>(path: P) -> Result<Self> {
         let dao = BlockingDao::new(path)?;
-        Ok(Self { dao })
+        let tables = Tables::new(dao.tables()?);
+        Ok(Self { dao, tables })
     }
 
     pub fn draw(&mut self, term: &mut Term) -> Result<()> {
-        let mut tables = Tables::new(self.dao.tables()?);
         term.draw(move |frame| {
-            let items: Vec<ListItem> = tables
+            let items: Vec<ListItem> = self
+                .tables
                 .names
                 .iter()
                 .map(|n| ListItem::new(n.as_str()))
                 .collect();
             let list = List::new(items);
-            let state = &mut tables.state;
+            let state = &mut self.tables.state;
             frame.render_stateful_widget(list, frame.size(), state);
         })?;
         Ok(())
@@ -43,14 +45,28 @@ impl App {
     pub fn tick(&mut self) -> Result<Tick> {
         if event::poll(Duration::from_millis(250)).context("event poll failed")? {
             if let Event::Key(key) = event::read().context("event read failed")? {
-                if KeyCode::Char('c') == key.code && key.modifiers.contains(KeyModifiers::CONTROL) {
+                if Self::should_quit(key) {
                     return Ok(Tick::Quit);
                 }
-                if KeyCode::Char('q') == key.code {
-                    return Ok(Tick::Quit);
+                match key.code {
+                    KeyCode::Char('j') => {
+                        self.tables.next();
+                    }
+                    KeyCode::Char('k') => {
+                        self.tables.previous();
+                    }
+                    _ => {}
                 }
             }
         }
         Ok(Tick::Continue)
+    }
+
+    fn should_quit(key: KeyEvent) -> bool {
+        match key.code {
+            KeyCode::Char('c') if key.modifiers.contains(KeyModifiers::CONTROL) => return true,
+            KeyCode::Char('q') => return true,
+            _ => false,
+        }
     }
 }
