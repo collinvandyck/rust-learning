3230ff0b8c259346f63fe2d8d60afb518f523996
Collin Van Dyck <collinvandyck@gmail.com>
2023-09-17 15:28:13 -0400

    wip

--- rql/src/bin/main.rs
+++ rql/src/bin/main.rs
@@ -16,6 +16,9 @@ use std::{
 
 #[derive(clap::Parser)]
 struct Args {
+    #[arg(long, env, default_value = "rql.log")]
+    log: String,
+
     #[arg(env)]
     db_path: String,
 }
@@ -23,14 +26,25 @@ struct Args {
 type Term = ratatui::Terminal<CrosstermBackend<Stdout>>;
 
 fn main() {
-    if let Err(err) = setup_and_run() {
+    let args = Args::parse();
+    if let Err(err) = setup_and_run(&args) {
         eprintln!("{err:?}");
         process::exit(1);
     }
 }
 
-fn setup_and_run() -> Result<()> {
-    let args = Args::parse();
+fn init_tracing(args: &Args) -> Result<()> {
+    let log_file = args.log.clone();
+    let log_file = std::fs::File::create(&log_file)?;
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .with_writer(log_file)
+        .init();
+    Ok(())
+}
+
+fn setup_and_run(args: &Args) -> Result<()> {
+    init_tracing(args)?;
     let mut term = setup_terminal().context("term setup failed")?;
     let res = run(&args, &mut term);
     restore_terminal(&mut term).context("term restore failed")?;
@@ -38,6 +52,7 @@ fn setup_and_run() -> Result<()> {
 }
 
 fn run(args: &Args, term: &mut Term) -> Result<()> {
+    tracing::error!("Running");
     let db: DbType = DbType::Path(args.db_path.as_str());
     let mut app = App::new(db)?;
     loop {
