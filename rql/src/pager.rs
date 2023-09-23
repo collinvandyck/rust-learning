9964b70ddbe9084dc4fabe7f1721c9ee9d92278a
Collin Van Dyck <collinvandyck@gmail.com>
2023-09-23 18:26:12 -0400

    wip

--- rql/src/pager.rs
+++ rql/src/pager.rs
@@ -3,9 +3,33 @@ use crate::prelude::*;
 struct Pager<T> {
     items: Vec<T>,
     top: usize,
+    size: usize,
+}
+
+impl<T> Pager<T> {
+    fn size(&mut self, size: usize) {
+        self.size = size;
+    }
+}
+
+impl<T, I> From<I> for Pager<T>
+where
+    I: IntoIterator<Item = T>,
+{
+    fn from(items: I) -> Self {
+        let top = 0;
+        let size = 0;
+        let items: Vec<T> = items.into_iter().collect();
+        Self { items, top, size }
+    }
 }
 
 mod tests {
+    use super::*;
     #[test]
-    fn test_pager() {}
+    fn test_pager() {
+        let p: Pager<i32> = [1, 2, 3, 4, 5].into();
+        assert_eq!(p.top, 0);
+        assert_eq!(p.size, 0);
+    }
 }
