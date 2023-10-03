8cec0d4883006a2b317f39f4b8bd0a350478b3ae
Collin Van Dyck <collinvandyck@gmail.com>
2023-10-03 11:14:43 -0400

    wip

--- chat-1/protocol/src/lib.rs
+++ chat-1/protocol/src/lib.rs
@@ -1,2 +1,63 @@
-pub enum Message {}
+pub mod prelude {
+    pub use serde::{Deserialize, Serialize};
+    pub use std::ops::{Deref, DerefMut};
+    pub use std::time::Instant;
+    pub use time::OffsetDateTime;
+}
+use prelude::*;
 
+/// The client/server protocol consists of sending events
+#[derive(Serialize, Deserialize, Clone)]
+pub enum Event {
+    Client(ClientEvent),
+    Server(ServerEvent),
+}
+
+/// ClientEvent is sent by the client
+#[derive(Serialize, Deserialize, Clone)]
+pub enum ClientEvent {
+    /// The client has identified themselves. This ident remains valid for the duration of the
+    /// connection.
+    Ident(User),
+
+    /// The client has sent a message
+    Message(Message),
+}
+
+/// ServerEvent is sent by the server
+#[derive(Serialize, Deserialize, Clone)]
+pub enum ServerEvent {
+    /// Someone else sent a message
+    Message(Message),
+}
+
+/// Represents a message in the chat
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Message {
+    pub from: User,
+    pub text: String,
+    pub time: Timestamp,
+}
+
+/// Identifies a user
+#[derive(Serialize, Deserialize, Clone)]
+pub struct User {
+    pub name: String,
+}
+
+/// A wrapper around time crate so we can attach methods
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Timestamp(OffsetDateTime);
+
+impl Deref for Timestamp {
+    type Target = OffsetDateTime;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for Timestamp {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
