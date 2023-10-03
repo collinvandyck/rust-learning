6e0729f92fc55b27ffc82d82db923ddad0731bbf
Collin Van Dyck <collinvandyck@gmail.com>
2023-10-03 12:11:33 -0400

    wip

--- chat-1/client/src/main.rs
+++ chat-1/client/src/main.rs
@@ -1,6 +1,31 @@
+use anyhow::Result;
 use protocol::prelude::*;
+use std::{
+    io::{self, Write},
+    process,
+};
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    if let Err(err) = run().await {
+        eprintln!("{err:?}");
+        process::exit(1);
+    }
+}
+
+async fn run() -> Result<()> {
     let config = protocol::Config::parse();
     println!("Got addr: {}", &config.addr);
+    let name = get_name()?;
+    println!("hi {name}.");
+    Ok(())
+}
+
+fn get_name() -> Result<String> {
+    let mut name = String::new();
+    print!("Name: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut name).unwrap();
+    name = name.trim().to_string();
+    Ok(name)
 }
