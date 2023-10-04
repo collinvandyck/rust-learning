dcb911de885086a580b56b4039e1298374b1b797
Collin Van Dyck <collinvandyck@gmail.com>
2023-10-04 09:36:13 -0400

    wip

--- chat-1/client/src/main.rs
+++ chat-1/client/src/main.rs
@@ -17,7 +17,9 @@ use tokio::{
 
 #[tokio::main]
 async fn main() {
-    if let Err(err) = run().await {
+    let config = protocol::Config::parse();
+    let client = Client::new(config);
+    if let Err(err) = client.run().await {
         eprintln!("{err:?}");
         process::exit(1);
     }
@@ -41,39 +43,48 @@ enum ClientError {
     EmptyName,
 }
 
-async fn run() -> Result<()> {
-    let config = protocol::Config::parse();
-    let name = get_name()?;
-    let addr = &config.addr;
-    let socket_addr = addr
-        .to_socket_addrs()
-        .map_err(|e| ClientError::AddrParseFailure(addr.to_string(), e))?
-        .filter(|f| f.is_ipv4())
-        .next()
-        .ok_or(ClientError::NoIpV4Addrs)?;
-    let socket = TcpSocket::new_v4().map_err(ClientError::SocketCreate)?;
-    let tcp_stream = socket
-        .connect(socket_addr)
-        .await
-        .map_err(|e| ClientError::CouldNotConnect(addr.into(), e))?;
-    let mut user_input = read_user_input();
-    let (rx, tx) = tcp_stream.into_split();
-    let rx = BufReader::new(rx);
-    let mut tx = BufWriter::new(tx);
-    send_server(ClientEvent::Ident(protocol::User { name }), &mut tx).await?;
-    let mut server_input = read_server_input(rx).await;
-    loop {
-        tokio::select! {
-            Some(input) = server_input.recv() => {
-                println!("Server input: {input}");
-            }
-            Some(input) = user_input.recv() => {
-                println!("User input: {input}");
-            }
-            else => break,
-        }
+struct Client {
+    config: protocol::Config,
+}
+
+impl Client {
+    fn new(config: protocol::Config) -> Self {
+        Self { config }
     }
-    Ok(())
+
+    async fn run(&self) -> Result<()> {
+        let name = get_name()?;
+        let addr = &self.config.addr;
+        let socket_addr = addr
+            .to_socket_addrs()
+            .map_err(|e| ClientError::AddrParseFailure(addr.to_string(), e))?
+            .filter(|f| f.is_ipv4())
+            .next()
+            .ok_or(ClientError::NoIpV4Addrs)?;
+        let socket = TcpSocket::new_v4().map_err(ClientError::SocketCreate)?;
+        let tcp_stream = socket
+            .connect(socket_addr)
+            .await
+            .map_err(|e| ClientError::CouldNotConnect(addr.into(), e))?;
+        let mut user_rx = read_user_input();
+        let (server_rx, server_tx) = tcp_stream.into_split();
+        let server_rx = BufReader::new(server_rx);
+        let mut server_tx = BufWriter::new(server_tx);
+        send_server(ClientEvent::Ident(protocol::User { name }), &mut server_tx).await?;
+        let mut server_rx = read_server(server_rx).await;
+        loop {
+            tokio::select! {
+                Some(input) = server_rx.recv() => {
+                    println!("Server input: {input}");
+                }
+                Some(input) = user_rx.recv() => {
+                    println!("User input: {input}");
+                }
+                else => break,
+            }
+        }
+        Ok(())
+    }
 }
 
 async fn send_server(event: ClientEvent, writer: &mut BufWriter<OwnedWriteHalf>) -> Result<()> {
@@ -82,7 +93,7 @@ async fn send_server(event: ClientEvent, writer: &mut BufWriter<OwnedWriteHalf>)
     Ok(())
 }
 
-async fn read_server_input(mut reader: BufReader<OwnedReadHalf>) -> Receiver<String> {
+async fn read_server(mut reader: BufReader<OwnedReadHalf>) -> Receiver<String> {
     let (tx, rx) = mpsc::channel(1024);
     tokio::spawn(async move {
         loop {
