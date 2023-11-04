28984de117779cd4e0be9f0032802f8a19a0cf79
Collin Van Dyck <collinvandyck@gmail.com>
2023-11-04 17:41:36 -0400

    wip

--- aoc-2020/src/bin/day-04/main.rs
+++ aoc-2020/src/bin/day-04/main.rs
@@ -1,13 +1,15 @@
 use aoc_2020::prelude::*;
 
 fn main() -> Result<()> {
-    get_passports("example.txt")?;
+    let passports = get_passports("example.txt")?;
+    println!("Got passports: {passports:?}");
     Ok(())
 }
 
-fn get_passports(p: impl AsRef<Path>) -> Result<Passport> {
+fn get_passports(p: impl AsRef<Path>) -> Result<Vec<Passport>> {
     let p = PathBuf::from(file!()).parent().unwrap().join(p.as_ref());
     let mut lines = file_to_lines(p)?.into_iter();
+    let mut passports = vec![];
     loop {
         let line = lines
             .by_ref()
@@ -20,11 +22,21 @@ fn get_passports(p: impl AsRef<Path>) -> Result<Passport> {
         let map = line
             .split(" ")
             .map(|s| s.splitn(2, ":").collect::<Vec<_>>())
-            .map(|p| (p[0], p[1]))
+            .map(|p| (p[0].to_string(), p[1].to_string()))
             .collect::<HashMap<_, _>>();
-        println!("Got map: {map:?}");
+        let passport = Passport::new(map);
+        passports.push(passport);
     }
-    todo!()
+    Ok(passports)
 }
 
-struct Passport {}
+#[derive(Debug, Clone)]
+struct Passport {
+    values: HashMap<String, String>,
+}
+
+impl Passport {
+    fn new(values: HashMap<String, String>) -> Self {
+        Self { values }
+    }
+}
