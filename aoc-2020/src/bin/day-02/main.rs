2bfaa4cf0d05b39a51b2e1ed39c5be18f86ba171
Collin Van Dyck <collinvandyck@gmail.com>
2023-11-04 15:50:21 -0400

    wip

--- aoc-2020/src/bin/day-02/main.rs
+++ aoc-2020/src/bin/day-02/main.rs
@@ -1,11 +1,11 @@
 use aoc_2020::prelude::*;
 
 fn main() -> Result<()> {
-    let p1 = (run("src/bin/day-01/example.txt")?,);
+    let p1 = (num_valid_pws("src/bin/day-01/example.txt")?,);
     println!("p1={p1:?}");
     Ok(())
 }
 
-fn run(p: impl AsRef<Path>) -> Result<()> {
+fn num_valid_pws(p: impl AsRef<Path>) -> Result<usize> {
     todo!()
 }
