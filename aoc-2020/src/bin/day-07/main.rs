66a4c0a141d5f7ca3cc9a72b1ca857c57d6427fa
Collin Van Dyck <collinvandyck@gmail.com>
2023-11-05 10:14:43 -0500

    wip

--- aoc-2020/src/bin/day-07/main.rs
+++ aoc-2020/src/bin/day-07/main.rs
@@ -1,21 +1,48 @@
+#![allow(dead_code, unused)]
 use aoc_2020::prelude::*;
 
 fn main() -> Result<()> {
     Ok(())
 }
 
-enum Hue {
-    Red,
-    Orange,
-    White,
-    Yellow,
-    Gold,
-    Olive,
-    Plum,
-    Blue,
-    Black,
+fn process(p: impl AsRef<Path>) -> Result<()> {
+    let p = PathBuf::from(file!()).parent().unwrap().join(p.as_ref());
+    let lines = file_to_lines(p)?;
+    Ok(())
+}
+
+fn rules(p: impl AsRef<Path>) -> Result<Vec<Rule>> {
+    let p = PathBuf::from(file!()).parent().unwrap().join(p.as_ref());
+    let lines = file_to_lines(p)?;
+    Ok(vec![])
+}
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+struct Hue(String);
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+struct Shade(String);
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct Color {
+    shade: Shade,
+    hue: Hue,
+}
+
+struct Rule {
+    color: Color,
+    contains: Vec<(usize, Color)>,
 }
 
 struct Bag {}
 
-struct Rule {}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let rules = rules("example.txt").unwrap();
+        assert_eq!(rules.len(), 9);
+    }
+}
