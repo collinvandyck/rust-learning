682dd8b230d558dec5f08d9609b4d252840102d2
Collin Van Dyck <collinvandyck@gmail.com>
2023-11-05 10:32:52 -0500

    wip

--- aoc-2020/src/bin/day-07/main.rs
+++ aoc-2020/src/bin/day-07/main.rs
@@ -13,8 +13,10 @@ fn process(p: impl AsRef<Path>) -> Result<()> {
 
 fn rules(p: impl AsRef<Path>) -> Result<Vec<Rule>> {
     let p = PathBuf::from(file!()).parent().unwrap().join(p.as_ref());
-    let lines = file_to_lines(p)?;
-    Ok(vec![])
+    Ok(file_to_lines(p)?
+        .into_iter()
+        .map(|s| s.parse::<Rule>())
+        .collect::<StdResult<Vec<_>, _>>()?)
 }
 
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
@@ -32,6 +34,49 @@ struct Color {
 struct Rule {
     color: Color,
     contains: Vec<(usize, Color)>,
+}
+
+impl Rule {
+    fn new(color: Color) -> Self {
+        Self {
+            color,
+            contains: vec![],
+        }
+    }
+}
+
+impl FromStr for Rule {
+    type Err = anyhow::Error;
+    // vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+    // faded blue bags contain no other bags.
+    fn from_str(s: &str) -> StdResult<Self, Self::Err> {
+        static RE1: Lazy<Regex> =
+            Lazy::new(|| Regex::new(r"^(\w+) (\w+) bags contain (.*)$").unwrap());
+        let caps = RE1
+            .captures(s)
+            .ok_or_else(|| anyhow!("first regex failed"))?;
+        let shade = Shade(caps.get(1).unwrap().as_str().to_string());
+        let hue = Hue(caps.get(2).unwrap().as_str().to_string());
+        let color = Color { shade, hue };
+        let mut rule = Rule::new(color);
+        let res = caps.get(3).unwrap().as_str();
+        if res != "no other bags." {
+            for (num, color) in res
+                .split(", ")
+                .map(|p| p.split(" ").take(3).collect::<Vec<_>>())
+                .map(|p| {
+                    let num = p[0].parse::<usize>().unwrap();
+                    let shade = Shade(p[1].to_string());
+                    let hue = Hue(p[2].to_string());
+                    let color = Color { shade, hue };
+                    (num, color)
+                })
+            {
+                rule.contains.push((num, color));
+            }
+        }
+        Ok(rule)
+    }
 }
 
 struct Bag {}
