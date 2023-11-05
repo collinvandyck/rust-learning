b0741e93639fcc7b1a9e2cbae25aac96537c668d
Collin Van Dyck <collinvandyck@gmail.com>
2023-11-05 10:01:06 -0500

    wip

--- aoc-2020/src/bin/day-07/main.rs
+++ aoc-2020/src/bin/day-07/main.rs
@@ -3,3 +3,19 @@ use aoc_2020::prelude::*;
 fn main() -> Result<()> {
     Ok(())
 }
+
+enum Hue {
+    Red,
+    Orange,
+    White,
+    Yellow,
+    Gold,
+    Olive,
+    Plum,
+    Blue,
+    Black,
+}
+
+struct Bag {}
+
+struct Rule {}
