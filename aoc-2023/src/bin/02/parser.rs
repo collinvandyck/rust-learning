2087e14429e4871c4c4aff4518902262e79bfdce
Collin Van Dyck <collinvandyck@gmail.com>
2023-12-02 15:00:24 -0500

    wip

--- aoc-2023/src/bin/02/parser.rs
+++ aoc-2023/src/bin/02/parser.rs
@@ -2,8 +2,9 @@ use crate::{Color, Cubes, Game};
 use nom::{
     branch::alt,
     bytes::complete::{tag, take_while1},
-    combinator, multi,
-    sequence::{self},
+    combinator::{self, map_res},
+    multi::separated_list0,
+    sequence::separated_pair,
     IResult,
 };
 use std::{collections::HashMap, str::FromStr};
@@ -13,32 +14,36 @@ fn is_digit(c: char) -> bool {
 }
 
 fn parse_u64(input: &str) -> IResult<&str, u64> {
-    combinator::map_res(take_while1(is_digit), |f: &str| f.parse::<u64>())(input)
+    let parser = take_while1(is_digit);
+    map_res(parser, |f: &str| f.parse::<u64>())(input)
 }
 
 fn parse_color(input: &str) -> IResult<&str, Color> {
     let parser = alt((tag("blue"), tag("green"), tag("red")));
-    combinator::map_res(parser, |f: &str| Color::from_str(f))(input)
+    let mut parser = map_res(parser, Color::from_str);
+    parser(input)
 }
 
 fn parse_num_color(input: &str) -> IResult<&str, (Color, u64)> {
-    let parser = sequence::separated_pair(parse_u64, tag(" "), parse_color);
-    combinator::map(parser, |(count, color)| (color, count))(input)
+    let parser = separated_pair(parse_u64, tag(" "), parse_color);
+    let mut parser = combinator::map(parser, |(ct, cr)| (cr, ct));
+    parser(input)
 }
 
 fn parse_turn(input: &str) -> IResult<&str, Cubes> {
-    let parser = multi::separated_list0(tag(", "), parse_num_color);
-    combinator::map(parser, |f| Cubes(f.into_iter().collect::<HashMap<_, _>>()))(input)
+    let parser = separated_list0(tag(", "), parse_num_color);
+    let mut parser = combinator::map(parser, |f| Cubes(f.into_iter().collect::<HashMap<_, _>>()));
+    parser(input)
 }
 
 fn parse_turns(input: &str) -> IResult<&str, Vec<Cubes>> {
-    multi::separated_list0(tag("; "), parse_turn)(input)
+    separated_list0(tag("; "), parse_turn)(input)
 }
 
 pub fn parse_game(input: &str) -> IResult<&str, Game> {
-    let (input, _) = nom::bytes::complete::tag("Game ")(input)?;
+    let (input, _) = tag("Game ")(input)?;
     let (input, id) = parse_u64(input)?;
-    let (input, _) = nom::bytes::complete::tag(": ")(input)?;
+    let (input, _) = tag(": ")(input)?;
     let (input, turns) = parse_turns(input)?;
     Ok((input, Game { id, turns }))
 }
