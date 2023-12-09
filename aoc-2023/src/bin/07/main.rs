905e6f070c39f519bffc3f2e0f323c6244025b7f
Collin Van Dyck <collinvandyck@gmail.com>
2023-12-09 13:23:48 -0500

    wip

--- aoc-2023/src/bin/07/main.rs
+++ aoc-2023/src/bin/07/main.rs
@@ -7,6 +7,7 @@ fn main() {
     println!("p1ex={}", total_winnings(example, Mode::Normal));
     println!("p1in={}", total_winnings(input, Mode::Normal));
     println!("p2ex={}", total_winnings(example, Mode::Jokers));
+    println!("p2in={}", total_winnings(input, Mode::Jokers));
 }
 
 fn total_winnings(input: &str, mode: Mode) -> u64 {
@@ -71,28 +72,65 @@ impl Type {
             Type::FiveOfKind(_) => 7,
         }
     }
+    fn upgrade(self, jokers: usize) -> Type {
+        if jokers == 0 {
+            return self;
+        }
+        match self {
+            Type::HighCard(c) => match jokers {
+                1 => Self::OnePair(c),
+                2 => Self::ThreeOfKind(c),
+                3 => Self::FourOfKind(c),
+                _ => Self::FiveOfKind(c),
+            },
+            Type::OnePair(c) => match jokers {
+                1 => Self::ThreeOfKind(c),
+                2 => Self::FourOfKind(c),
+                _ => Self::FiveOfKind(c),
+            },
+            Type::TwoPair(c1, c2) => match jokers {
+                1 => Type::FullHouse(c1, c2),
+                2 => Type::FourOfKind(c1),
+                _ => Type::FiveOfKind(c1),
+            },
+            Type::ThreeOfKind(c) => match jokers {
+                1 => Type::FourOfKind(c),
+                _ => Type::FiveOfKind(c),
+            },
+            Type::FullHouse(c1, _c2) => match jokers {
+                1 => Type::FourOfKind(c1),
+                _ => Type::FiveOfKind(c1),
+            },
+            Type::FourOfKind(c) => Type::FiveOfKind(c),
+            Type::FiveOfKind(c) => Type::FiveOfKind(c),
+        }
+    }
+}
+
+impl Card {
+    fn cmp(&self, other: &Card, mode: Mode) -> Ordering {
+        match mode {
+            Mode::Normal => CARD_VALUES.get(&self.0).cmp(&CARD_VALUES.get(&other.0)),
+            Mode::Jokers => JOKER_VALUES.get(&self.0).cmp(&JOKER_VALUES.get(&other.0)),
+        }
+    }
 }
 
 type CardMap = Lazy<HashMap<char, i32>>;
 static CARD_VALUES: CardMap = Lazy::new(|| "23456789TJQKA".chars().zip(1..).collect());
-impl Ord for Card {
-    fn cmp(&self, other: &Self) -> Ordering {
-        CARD_VALUES.get(&self.0).cmp(&CARD_VALUES.get(&other.0))
-    }
-}
-
-impl PartialOrd for Card {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
+static JOKER_VALUES: CardMap = Lazy::new(|| "J23456789TQKA".chars().zip(1..).collect());
 
 impl Ord for Hand {
     fn cmp(&self, other: &Self) -> Ordering {
-        self.typ
-            .ord()
-            .cmp(&other.typ.ord())
-            .then_with(|| self.cards.cmp(&other.cards))
+        self.typ.ord().cmp(&other.typ.ord()).then_with(|| {
+            let mode = self.mode;
+            self.cards
+                .iter()
+                .zip(other.cards.iter())
+                .map(|(c1, c2)| c1.cmp(c2, mode))
+                .reduce(|a, b| a.then(b))
+                .unwrap_or(Ordering::Equal)
+        })
     }
 }
 
@@ -122,7 +160,9 @@ impl From<&[Card]> for Type {
         }
         let mut counts: Vec<_> = hm.into_iter().map(|(ca, ct)| (*ca, ct)).collect();
         counts.sort_by(|(c1_card, c1_count), (c2_card, c2_count)| {
-            c2_count.cmp(c1_count).then(c2_card.cmp(c1_card))
+            c2_count
+                .cmp(c1_count)
+                .then(c2_card.cmp(c1_card, Mode::Normal)) // TODO: fix this
         });
         use Type::*;
         match counts.as_slice() {
@@ -152,6 +192,10 @@ fn parse_bid(input: &str, mode: Mode) -> Bid {
 fn parse_hand(input: &str, mode: Mode) -> Hand {
     let cards: Vec<_> = input.chars().map(Card).collect();
     let typ = Type::from(cards.as_slice());
+    let typ = match mode {
+        Mode::Normal => typ,
+        Mode::Jokers => typ.upgrade(cards.iter().filter(|c| c.0 == 'J').count()),
+    };
     Hand { cards, typ, mode }
 }
 
