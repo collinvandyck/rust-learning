b285736a1623604dd5e284eff0116b26f2f9bac4
Collin Van Dyck <collinvandyck@gmail.com>
2023-12-09 10:30:37 -0500

    wip

--- aoc-2023/src/bin/06/main.rs
+++ aoc-2023/src/bin/06/main.rs
@@ -1,11 +1,51 @@
 fn main() {
     let example = include_str!("example.txt");
+    let input = include_str!("input.txt");
+    println!("p1ex={}", margin_of_error(example, false));
+    println!("p1in={}", margin_of_error(input, false));
+}
+
+fn margin_of_error(input: &str, squash: bool) -> u64 {
+    let games = parse(input);
+    if squash {}
+    parse(input).into_iter().map(|r| r.ways_to_beat()).product()
+}
+
+fn squash(games: Vec<Race>) -> Race {
+    let mut ts = String::new();
+    let mut ds = String::new();
+    for game in games {
+        ts = format!("{ts}{}", game.time);
+        ds = format!("{ds}{}", game.distance);
+    }
+    Race {
+        time: ts.parse().unwrap(),
+        distance: ds.parse().unwrap(),
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct Race {
     time: u64,
     distance: u64,
+}
+
+impl Race {
+    fn ways_to_beat(&self) -> u64 {
+        (1..self.time)
+            .map(|n| self.distance(n))
+            .filter(|n| *n > self.distance)
+            .count()
+            .try_into()
+            .unwrap()
+    }
+    fn distance(&self, warmup: u64) -> u64 {
+        if warmup == 0 || warmup >= self.time {
+            return 0;
+        }
+        let rem_time = self.time - warmup;
+        warmup * rem_time
+    }
 }
 
 fn parse(input: &str) -> Vec<Race> {
@@ -30,6 +70,33 @@ fn line_to_nums(input: &str) -> impl Iterator<Item = u64> + '_ {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_race() {
+        let race = Race {
+            time: 7,
+            distance: 9,
+        };
+        assert_eq!(race.ways_to_beat(), 4);
+    }
+
+    #[test]
+    fn test_margin_of_error() {
+        let moe = margin_of_error(include_str!("example.txt"), false);
+        assert_eq!(moe, 288);
+    }
+
+    fn test_squash() {
+        let races = parse(include_str!("example.txt"));
+        let race = squash(races);
+        assert_eq!(
+            race,
+            Race {
+                time: 71530,
+                distance: 940200,
+            }
+        );
+    }
 
     #[test]
     fn test_parse() {
