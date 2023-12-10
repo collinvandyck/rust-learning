348e29d4d8bcdd416df5c6c8eef8a7968c12b06f
Collin Van Dyck <collinvandyck@gmail.com>
2023-12-10 16:35:31 -0500

    wip

--- aoc-2023/src/bin/09/main.rs
+++ aoc-2023/src/bin/09/main.rs
@@ -11,18 +11,18 @@ type Num = i64;
 fn main() {
     let example = include_str!("example.txt");
     let input = include_str!("input.txt");
-    println!("p1ex={}", sum_nexts(example));
-    println!("p1in={}", sum_nexts(input));
+    println!("p1ex={}", sum_nexts(example, true));
+    println!("p1in={}", sum_nexts(input, true));
 }
 
-fn sum_nexts(input: &str) -> Num {
+fn sum_nexts(input: &str, forward: bool) -> Num {
     parse(input)
         .into_iter()
-        .map(|nums| next(nums.clone()))
+        .map(|nums| next(nums.clone(), forward))
         .sum()
 }
 
-fn next(nums: Vec<Num>) -> Num {
+fn next(nums: Vec<Num>, forward: bool) -> Num {
     let mut stack = vec![];
     stack.push(nums);
     while !stack_done(&stack) {
@@ -52,10 +52,11 @@ fn stack_done(stack: &Vec<Vec<Num>>) -> bool {
 
 #[test]
 fn test_next() {
+    // test forward directions
     let nums = vec![0, 3, 6, 9, 12, 15];
-    assert_eq!(next(nums), 18);
+    assert_eq!(next(nums, true), 18);
     let nums = vec![1, 3, 6, 10, 15, 21];
-    assert_eq!(next(nums), 28);
+    assert_eq!(next(nums, true), 28);
 }
 
 fn parse(input: &str) -> Vec<Vec<Num>> {
