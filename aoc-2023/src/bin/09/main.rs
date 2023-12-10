bb47de43c5f9e06e95c4e10e5e4fd8e61cc40dcc
Collin Van Dyck <collinvandyck@gmail.com>
2023-12-10 15:20:54 -0500

    wip

--- aoc-2023/src/bin/09/main.rs
+++ aoc-2023/src/bin/09/main.rs
@@ -1,16 +1,20 @@
 #![allow(dead_code, unused)]
 
-use std::iter;
+use std::{
+    iter::{self, Copied, Rev},
+    slice,
+};
 fn main() {
     let example = include_str!("example.txt");
     println!("p1ex={}", sum_nexts(example));
 }
 
 fn sum_nexts(input: &str) -> u64 {
-    parse(input).iter().map(next_value).sum()
+    parse(input).into_iter().map(next_value).sum()
 }
 
-fn next_value(seq: &Seq) -> u64 {
+fn next_value(seq: Seq) -> u64 {
+    let nums: Vec<u64> = seq.diffs().collect();
     todo!()
 }
 
@@ -18,31 +22,29 @@ fn next_value(seq: &Seq) -> u64 {
 fn test_next_value() {
     let seqs = parse("0   3   6   9  12  15");
     assert_eq!(seqs.len(), 1);
-    assert_eq!(next_value(&seqs[0]), 18);
+    let seq = seqs.into_iter().nth(0).unwrap();
+    assert_eq!(next_value(seq), 18);
 }
 
 #[test]
 fn test_diff_iter() {
     let seq = parse_seq("0 3 6 9 12 15");
-
-    let dif: Vec<_> = seq.diff_iter().collect();
+    let dif: Vec<u64> = seq.diffs().collect();
     assert_eq!(dif, &[3, 3, 3, 3, 3]);
-
-    let dif: Vec<_> = seq.diff_iter_slice().collect();
-    assert_eq!(dif, &[3, 3, 3, 3, 3]);
+    let dif: Vec<u64> = Seq(dif).diffs().collect();
+    assert_eq!(dif, &[0, 0, 0, 0]);
 }
 
+#[derive(Debug, Clone)]
 struct Seq(Vec<u64>);
 
 impl Seq {
-    fn diff_iter(&self) -> impl Iterator<Item = u64> + '_ {
-        let iter = self.0.iter().copied();
-        let iter = DiffIter::from_iter(iter);
-        iter
+    fn diffs<'a>(&'a self) -> impl Iterator<Item = u64> + 'a {
+        DiffIter::from_slice(&self.0)
     }
-    fn diff_iter_slice<'a>(&'a self) -> impl Iterator<Item = u64> + 'a {
-        let iter = DiffIter::from_slice(&self.0);
-        iter
+
+    fn diffs_seq(&self) -> Seq {
+        Seq(self.diffs().collect())
     }
 }
 
@@ -66,9 +68,17 @@ where
 }
 
 impl<'a> DiffIter<Box<dyn Iterator<Item = u64> + 'a>> {
-    fn from_slice(s: &'a [u64]) -> Self {
+    fn from_slice_with_alloc(s: &'a [u64]) -> Self {
         let iter = s.iter().rev().copied();
         let mut iter = Box::new(iter);
+        let last = iter.next();
+        Self { iter, last }
+    }
+}
+
+impl<'a> DiffIter<Copied<Rev<slice::Iter<'a, u64>>>> {
+    fn from_slice(s: &'a [u64]) -> Self {
+        let mut iter = s.iter().rev().copied();
         let last = iter.next();
         Self { iter, last }
     }
