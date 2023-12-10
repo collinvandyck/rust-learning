f1f8db81181d37421ff5ada06c495625ac2a4b87
Collin Van Dyck <collinvandyck@gmail.com>
2023-12-10 12:26:31 -0500

    wip

--- aoc-2023/src/bin/09/main.rs
+++ aoc-2023/src/bin/09/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("hi");
+    let example = include_str!("example.txt");
+    println!("p1ex={}", sum_nexts(example));
+}
+
+fn sum_nexts(input: &str) -> u64 {
+    todo!()
+}
+
+struct Seq(Vec<u64>);
+
+fn parse(input: &str) -> Vec<Seq> {
+    input
+        .lines()
+        .map(|line| {
+            line.split(" ")
+                .map(|s| s.trim().parse::<u64>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .map(Seq)
+        .collect()
 }
