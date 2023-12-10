791807dfbc3512d265091449c1a38e90d6010caf
Collin Van Dyck <collinvandyck@gmail.com>
2023-12-10 12:31:34 -0500

    wip

--- aoc-2023/src/bin/09/main.rs
+++ aoc-2023/src/bin/09/main.rs
@@ -11,14 +11,21 @@ fn next_value(_seq: &Seq) -> u64 {
     todo!()
 }
 
+#[test]
+fn test_next_value() {
+    let seqs = parse("0   3   6   9  12  15");
+    assert_eq!(seqs.len(), 1);
+    assert_eq!(next_value(&seqs[0]), 18);
+}
+
 struct Seq(Vec<u64>);
 
 fn parse(input: &str) -> Vec<Seq> {
     input
         .lines()
         .map(|line| {
-            line.split(" ")
-                .map(|s| s.trim().parse::<u64>().unwrap())
+            line.split_whitespace()
+                .map(|s| s.parse::<u64>().unwrap())
                 .collect::<Vec<_>>()
         })
         .map(Seq)
