0206a121e8b89d8ba4a0f3ef5a32f90adeda15a8
Collin Van Dyck <collinvandyck@gmail.com>
2023-12-17 11:15:23 -0500

    wip

--- aoc-2023/src/bin/10/main.rs
+++ aoc-2023/src/bin/10/main.rs
@@ -10,7 +10,7 @@ fn main() {
     println!("p1ex1={}", farthest_distance(ex1));
     println!("p1i1n={}", farthest_distance(in1));
     println!("p2e1x={}", area_enclosed(ex1));
-    println!("p2e2x={}", area_enclosed(ex2));
+    // println!("p2e2x={}", area_enclosed(ex2));
     //println!("p2in={}", area_enclosed(in1));
 }
 
@@ -87,42 +87,48 @@ impl Map {
         let mut iter = pts.iter_mut();
         let mut last: Option<Pt> = None;
         let mut interiors: HashSet<Pt> = HashSet::default();
-        use Tile::*;
         for (idx, pt) in iter.enumerate() {
-            if idx < 5 {
-                println!("{idx}: {pt:?}");
-            }
+            println!("{idx}: {pt:?}");
             match (pt.tile, last.as_ref()) {
-                (HPipe, None) => {
+                (Tile::HPipe, None) => {
                     let tiles = self.ground_tiles(*pt, Dir::Down);
                     if !tiles.is_empty() {
                         self.mark(*pt);
-                        println!("{idx}: {pt:?}");
                         println!("  hpipe (none) ground tiles: {tiles:?}");
                     }
                     interiors.extend(tiles);
                 }
-                (HPipe, Some(last)) => {
-                    let interior_dir = HPipe.interior_dir_for_last(&last.tile);
+                (Tile::HPipe, Some(last)) => {
+                    let interior_dir = Tile::HPipe.interior_dir_for_last(&last.tile);
                     let tiles = self.ground_tiles(*pt, interior_dir);
                     if !tiles.is_empty() {
+                        self.mark(*pt);
                         println!("{idx}: {pt:?}");
+                        println!(
+                            "  interior_dir={interior_dir:?} for last_tile: {:?}",
+                            last.tile
+                        );
                         println!("  hpipe ground tiles: {tiles:?}");
                     }
                     interiors.extend(tiles);
                 }
-                (VPipe, Some(last)) => {
-                    let interior_dir = VPipe.interior_dir_for_last(&last.tile);
+                (Tile::VPipe, Some(last)) => {
+                    let interior_dir = Tile::VPipe.interior_dir_for_last(&last.tile);
                     let tiles = self.ground_tiles(*pt, interior_dir);
                     if !tiles.is_empty() {
+                        self.mark(*pt);
                         println!("{idx}: {pt:?}");
                         println!("  vpipe ground tiles: {tiles:?}");
                     }
                     interiors.extend(tiles);
                 }
-                (BendSW | BendSE | BendNE | BendNW, last) => {}
-                (VPipe, None) => panic!("invalid vpipe with no last"),
+                (Tile::BendSW | Tile::BendSE | Tile::BendNE | Tile::BendNW, last) => {}
+                (Tile::VPipe, None) => panic!("invalid vpipe with no last"),
                 (tile, last) => panic!("invalid tile: {tile:?} last: {last:?}"),
+            }
+            if let Some(last) = last {
+                let dir = pt.dir_to(&last);
+                self.set(pt.x, pt.y, Tile::Dir(dir));
             }
             last.replace(*pt);
         }
@@ -263,13 +269,25 @@ impl std::fmt::Display for Map {
         let s = self
             .pts
             .iter()
-            .map(|pts| pts.iter().map(|pt| format!("{}", pt.tile)).join(""))
+            .map(|pts| {
+                pts.iter()
+                    .map(|pt| {
+                        if self.marks.contains(&pt) {
+                            format!("*")
+                        } else if (self.start.x, self.start.y) == (pt.x, pt.y) {
+                            format!("S")
+                        } else {
+                            format!("{}", pt.tile)
+                        }
+                    })
+                    .join("")
+            })
             .join("\n");
         write!(f, "{s}")
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum Dir {
     Up,
     Down,
@@ -305,6 +323,17 @@ impl Pt {
             dist: 0,
         }
     }
+    fn dir_to(&self, other: &Pt) -> Dir {
+        if other.x > self.x {
+            Dir::Right
+        } else if other.x < self.x {
+            Dir::Left
+        } else if other.y > self.y {
+            Dir::Down
+        } else {
+            Dir::Up
+        }
+    }
 }
 
 impl Debug for Pt {
@@ -324,6 +353,7 @@ enum Tile {
     Ground,
     Start,
     Interior,
+    Dir(Dir),
 }
 
 impl Tile {
@@ -339,12 +369,11 @@ impl Tile {
         }
     }
     fn has(&self, dir: Dir) -> bool {
-        use Tile::*;
         match (self, dir) {
-            (VPipe | BendSW | BendSE, Dir::Up) => true,
-            (VPipe | BendNW | BendNE, Dir::Down) => true,
-            (HPipe | BendSE | BendNE, Dir::Left) => true,
-            (HPipe | BendSW | BendNW, Dir::Right) => true,
+            (Tile::VPipe | Tile::BendSW | Tile::BendSE, Dir::Up) => true,
+            (Tile::VPipe | Tile::BendNW | Tile::BendNE, Dir::Down) => true,
+            (Tile::HPipe | Tile::BendSE | Tile::BendNE, Dir::Left) => true,
+            (Tile::HPipe | Tile::BendSW | Tile::BendNW, Dir::Right) => true,
             _ => false,
         }
     }
@@ -353,7 +382,7 @@ impl Tile {
         match self {
             Tile::VPipe if last.has(Dir::Down) => Dir::Left,
             Tile::VPipe => Dir::Right,
-            Tile::HPipe if last.has(Dir::Right) => Dir::Up,
+            Tile::HPipe if last.has(Dir::Right) => Dir::Down,
             Tile::HPipe => Dir::Up,
             _ => panic!("invalid interior dir for last self:{self:?}"),
         }
@@ -372,6 +401,10 @@ impl std::fmt::Display for Tile {
             Tile::Ground => ".",
             Tile::Start => "S",
             Tile::Interior => "I",
+            Tile::Dir(Dir::Up) => "^",
+            Tile::Dir(Dir::Down) => "v",
+            Tile::Dir(Dir::Left) => "<",
+            Tile::Dir(Dir::Right) => ">",
         };
         write!(f, "{s}")
     }
