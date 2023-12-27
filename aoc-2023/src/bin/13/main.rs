b69c793bf1c1cec46c0e7f008c0d3b0766757295
Collin Van Dyck <collinvandyck@gmail.com>
2023-12-26 23:22:39 -0500

    wip

--- aoc-2023/src/bin/13/main.rs
+++ aoc-2023/src/bin/13/main.rs
@@ -14,8 +14,27 @@ struct Cell {
 #[derive(Debug, Clone, PartialEq, Eq)]
 struct Pattern {
     cells: Vec<Vec<Cell>>,
-    rows: Vec<Vec<char>>,
-    cols: Vec<Vec<char>>,
+    rows: Vec<Stripe>,
+    cols: Vec<Stripe>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct Stripe {
+    chs: Vec<char>,
+}
+
+impl std::ops::Deref for Stripe {
+    type Target = Vec<char>;
+    fn deref(&self) -> &Self::Target {
+        &self.chs
+    }
+}
+
+impl Stripe {
+    fn new(chs: impl Iterator<Item = char>) -> Self {
+        let chs = chs.into_iter().collect_vec();
+        Self { chs }
+    }
 }
 
 impl Pattern {
@@ -33,17 +52,21 @@ impl Pattern {
             .collect_vec();
         let rows = cells
             .iter()
-            .map(|row| row.iter().map(|c| c.ch).collect())
+            .map(|row| row.iter().map(|c| c.ch))
+            .map(Stripe::new)
             .collect_vec();
         let cols = (0..cells.first().map(|r| r.len()).unwrap_or_default())
             .map(|x| {
                 cells
                     .iter()
-                    .map(|row| row.get(x).map(|c| c.ch).expect("bad col"))
-                    .collect_vec()
+                    .map(move |row| row.get(x).map(|c| c.ch).expect("bad col"))
             })
+            .map(|s| Stripe::new(s))
             .collect_vec();
         Pattern { cells, rows, cols }
+    }
+    fn vertical_mirrors(&self) -> usize {
+        todo!()
     }
 }
 
