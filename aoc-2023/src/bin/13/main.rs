e558c651d636695aa2b32d605e4ed7f3b57aa7c9
Collin Van Dyck <collinvandyck@gmail.com>
2023-12-27 11:54:56 -0500

    wip

--- aoc-2023/src/bin/13/main.rs
+++ aoc-2023/src/bin/13/main.rs
@@ -1,8 +1,16 @@
 #![allow(dead_code, unused)]
 
 use itertools::Itertools;
+use tracing::info;
 
-fn main() {}
+fn main() {
+    let ex1 = include_str!("ex1.txt");
+    println!("p1ex1 = {}", summarize_patterns(ex1));
+}
+
+fn summarize_patterns(input: &str) -> usize {
+    parse(input).iter().map(|p| p.mirrors()).sum()
+}
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct Cell {
@@ -66,11 +74,26 @@ impl Pattern {
         Pattern { cells, rows, cols }
     }
     fn mirrors(&self) -> usize {
+        info!("Mirrors");
         fn stripe_reflects(stripes: &[Stripe]) -> usize {
-            todo!()
+            (1..stripes.len())
+                .map(|idx| {
+                    let prev = stripes[0..idx].iter().rev();
+                    let next = stripes[idx..].iter();
+                    let iter = prev.zip(next).take_while(|(a, b)| a == b);
+                    iter.clone().for_each(|(s1, s2)| {
+                        let s1 = s1.chs.iter().collect::<String>();
+                        let s2 = s2.chs.iter().collect::<String>();
+                        info!("{s1} {s2}");
+                    });
+                    iter.count()
+                })
+                .sum()
         }
         let hrz = stripe_reflects(&self.rows);
+        info!("hrz: {hrz}");
         let vrt = stripe_reflects(&self.cols);
+        info!("vrt: {vrt}");
         hrz * 100 + vrt
     }
 }
@@ -82,6 +105,16 @@ fn parse(input: &str) -> Vec<Pattern> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use tracing_test::traced_test;
+
+    #[test]
+    #[traced_test]
+    fn test_ex1_mirrors() {
+        let ex1 = include_str!("ex1.txt");
+        let pats = parse(ex1);
+        let mrs = pats[1].mirrors();
+        assert_eq!(mrs, 1);
+    }
 
     #[test]
     fn test_parse() {
