139b2d0938dc48bfeefface7be8dac40e3d6b013
Collin Van Dyck <collinvandyck@gmail.com>
2023-12-27 16:38:58 -0500

    wip

--- aoc-2023/src/bin/14/main.rs
+++ aoc-2023/src/bin/14/main.rs
@@ -1,1 +1,67 @@
+#![allow(dead_code, unused)]
+
+use itertools::Itertools;
 fn main() {}
+
+struct Map {
+    rows: usize,
+    cols: usize,
+    tiles: Vec<Tile>,
+}
+
+impl Map {
+    fn parse(input: &str) -> Self {
+        let tiles = input
+            .trim()
+            .lines()
+            .map(|row| row.chars().map(Tile::from).collect_vec())
+            .collect_vec();
+        assert!(tiles.iter().map(|r| r.len()).all_equal());
+        let rows = tiles.len();
+        let cols = tiles.first().map(|r| r.len()).unwrap_or_default();
+        Self {
+            rows,
+            cols,
+            tiles: tiles.concat(),
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+enum Tile {
+    Space,
+    Round,
+    Cube,
+}
+
+impl Tile {
+    fn from(ch: char) -> Self {
+        match ch {
+            '.' => Self::Space,
+            'O' => Self::Round,
+            '#' => Self::Cube,
+            _ => panic!("bad ch: {ch}"),
+        }
+    }
+}
+
+enum Dir {
+    North,
+    South,
+    East,
+    West,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let ex1 = include_str!("ex1.txt");
+        let map = Map::parse(ex1);
+        assert_eq!(map.cols, 10);
+        assert_eq!(map.rows, 10);
+        assert_eq!(map.tiles.len(), 100);
+    }
+}
