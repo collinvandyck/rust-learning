089cf563dad5234575a5c57faf565f4086ceaca4
Collin Van Dyck <collinvandyck@gmail.com>
2023-12-28 14:18:13 -0500

    wip

---
+++ aoc-2023/src/bin/16/main.rs
@@ -0,0 +1,99 @@
+#![allow(dead_code, unused)]
+
+use itertools::Itertools;
+
+fn main() {}
+
+struct Map {
+    tiles: Vec<TileXY>,
+    rows: usize,
+    cols: usize,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct TileXY {
+    tile: Tile,
+    point: Point,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Point {
+    x: usize,
+    y: usize,
+}
+
+impl Point {
+    fn new(x: usize, y: usize) -> Self {
+        Self { x, y }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, strum_macros::EnumIs)]
+enum Tile {
+    Space,    // .
+    MirRight, // /
+    MirLeft,  // \
+    SplitV,   // |
+    SplitH,   // -
+}
+
+impl Map {
+    fn parse(input: &str) -> Self {
+        let tiles = input
+            .trim()
+            .lines()
+            .enumerate()
+            .map(|(y, line)| {
+                line.trim()
+                    .chars()
+                    .enumerate()
+                    .map(|(x, ch)| {
+                        let tile = Tile::from_ch(ch);
+                        let point = Point::new(x, y);
+                        TileXY { tile, point }
+                    })
+                    .collect_vec()
+            })
+            .collect_vec();
+        let rows = tiles.len();
+        let cols = tiles.first().map(|r| r.len()).unwrap_or_default();
+        Self {
+            tiles: tiles.concat(),
+            rows,
+            cols,
+        }
+    }
+    fn get(&self, pt: Point) -> Option<&TileXY> {
+        let idx = self.idx(pt);
+        self.tiles.get(idx)
+    }
+    fn idx(&self, pt: Point) -> usize {
+        pt.y * self.cols + pt.x
+    }
+}
+
+impl Tile {
+    fn from_ch(ch: char) -> Self {
+        match ch {
+            '.' => Self::Space,
+            '/' => Self::MirRight,
+            '\\' => Self::MirLeft,
+            '|' => Self::SplitV,
+            '-' => Self::SplitH,
+            _ => panic!("unknown ch: {ch}"),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let ex1 = include_str!("ex1.txt");
+        let map = Map::parse(ex1);
+        assert_eq!(map.cols, 10);
+        assert_eq!(map.rows, 10);
+    }
+}
