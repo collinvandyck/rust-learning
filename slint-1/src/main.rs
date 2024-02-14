5acba39912bc176e8abe113ace10cf0f867b6b6a
Collin Van Dyck <collinvandyck@gmail.com>
2024-02-14 18:43:58 -0500

    wip

---
+++ slint-1/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    MainWindow::new().unwrap().run().unwrap();
+}
+
+slint::slint! {
+    export component MainWindow inherits Window {
+        Text {
+            text: "hello world";
+            color: green;
+        }
+    }
+}
+
