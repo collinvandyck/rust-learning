e9b02cae53bdd4aa3571d0913d1f3ef8efb3d384
Collin Van Dyck <collinvandyck@gmail.com>
2024-02-23 11:52:53 -0500

    wip

--- aoc-2023/src/bin/17/main.rs
+++ aoc-2023/src/bin/17/main.rs
@@ -7,19 +7,39 @@ fn main() {
     let ex = include_str!("ex1.txt");
     let map = Map::parse(ex);
     println!("{map}");
+    assert_eq!(map.get(0, 0), Some(Tile::from('2')));
+    assert_eq!(map.get(0, 1), Some(Tile::from('4')));
 }
 
 struct Map {
-    tiles: Vec<Vec<char>>,
+    tiles: Vec<Vec<Tile>>,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+struct Tile {
+    ch: char,
+    val: u32,
+}
+
+impl From<char> for Tile {
+    fn from(ch: char) -> Self {
+        Self {
+            ch,
+            val: ch.to_digit(10).unwrap(),
+        }
+    }
 }
 
 impl Map {
     fn parse(s: &str) -> Self {
-        let tiles: Vec<Vec<char>> = s.lines().map(|l| l.trim().chars().collect()).collect();
+        let tiles: Vec<Vec<Tile>> = s
+            .lines()
+            .map(|l| l.trim().chars().map(Into::into).collect())
+            .collect();
         assert!(tiles.iter().map(|v| v.len()).all_equal());
         Self { tiles }
     }
-    fn get(&self, row: usize, col: usize) -> Option<char> {
+    fn get(&self, row: usize, col: usize) -> Option<Tile> {
         self.tiles.get(row).and_then(|v| v.get(col)).copied()
     }
     fn cols(&self) -> usize {
@@ -35,7 +55,7 @@ impl Display for Map {
         let s = self
             .tiles
             .iter()
-            .map(|r| r.iter().collect::<String>())
+            .map(|r| r.iter().map(|t| t.ch).collect::<String>())
             .join("\n");
         write!(f, "{s}")
     }
