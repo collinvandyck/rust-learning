3f1fedaebb3b634576ed92ffb610009b070e42c0
Collin Van Dyck <collinvandyck@gmail.com>
2024-02-24 11:00:50 -0500

    wip

--- aoc-2023/src/bin/17/main.rs
+++ aoc-2023/src/bin/17/main.rs
@@ -109,7 +109,6 @@ impl Map {
             let start = Visit::new(&start, 0);
             let mut queue = binary_heap::BinaryHeap::new();
             queue.push(start);
-            queue.push(Visit::new(&goal, 100));
             queue
         };
         if let Some(visit) = queue.pop() {
