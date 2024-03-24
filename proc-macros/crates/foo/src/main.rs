3219c8eb82cafcebeab0fb5f22eee6545538c380
Collin Van Dyck <collinvandyck@gmail.com>
2024-03-24 11:42:41 -0400

    wip

--- proc-macros/crates/foo/src/main.rs
+++ proc-macros/crates/foo/src/main.rs
@@ -1,22 +1,35 @@
-#![allow(unused)]
-
 trait Foo {
     fn foo(&self) -> String;
 }
 
+// deriving Foo implements Foo for the struct and also adds methods for each field.
 #[derive(Debug, foo_derive::Foo)]
 struct Person {
-    #[inst]
     name: &'static str,
+    age: u8,
 }
 
 impl Default for Person {
     fn default() -> Self {
-        Self { name: "Collin" }
+        Self {
+            name: "Collin",
+            age: 39,
+        }
     }
 }
 
 fn main() {
-    Person::default().hi();
-    println!("{}", Person::default().foo());
+    // just a generic person (me)
+    let p = Person::default();
+
+    // silly method added by the derive
+    p.say_hello();
+
+    // methods generated for each field in the struct
+    p.debug_name();
+    p.debug_age();
+
+    // the derive implements Foo for the thing derived
+    let p: &dyn Foo = &p;
+    println!("Foo: {}", p.foo());
 }
--- proc-macros/crates/foo_derive/src/lib.rs
+++ proc-macros/crates/foo_derive/src/lib.rs
@@ -2,30 +2,45 @@
 
 extern crate proc_macro;
 use proc_macro::TokenStream;
-use quote::quote;
+use quote::{format_ident, quote};
 use syn::{parse_macro_input, DeriveInput};
 
-#[proc_macro_derive(Foo, attributes(inst))]
+#[proc_macro_derive(Foo)]
 pub fn my_proc_derive(item: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(item as DeriveInput);
-    // eprintln!("ast: {ast:#?}");
-    let name = ast.ident;
-    let syn::Data::Struct(ref st) = ast.data else {
+    let struct_name = ast.ident;
+    let syn::Data::Struct(syn::DataStruct {
+        fields: syn::Fields::Named(syn::FieldsNamed { named: fields, .. }),
+        ..
+    }) = ast.data
+    else {
         panic!("must be a struct")
     };
+    let methods = fields.iter().filter_map(|f| {
+        f.ident.as_ref().map(|field_ident| {
+            let method_ident = format_ident!("debug_{}", field_ident);
+            quote! {
+                fn #method_ident(&self) {
+                    println!("{} reporting for duty", stringify!(#method_ident));
+                }
+            }
+        })
+    });
     let expanded = quote! {
-        impl #name {
-            fn hi(&self) {
-                println!("hello from {}, {}", stringify!(#name), self.name);
+        impl #struct_name {
+            // interpolate the methods we created for each field.
+            #(#methods)*
+
+            // a one-off silly method
+            fn say_hello(&self) {
+                println!("{}: Hi, {}.", stringify!(#struct_name), self.name);
             }
         }
 
-        /// This is a doc comment
-        impl Foo for #name {
-
-            /// a foo does a foo
+        // implement the trait
+        impl Foo for #struct_name {
             fn foo(&self) -> String {
-                String::from("foooo")
+                format!("foooo from {}", stringify!(#struct_name))
             }
         }
     };
