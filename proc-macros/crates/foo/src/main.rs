5982f2a20333f8bc62bef6f91e42aa354ae09c01
Collin Van Dyck <collinvandyck@gmail.com>
2024-03-24 10:00:36 -0400

    wip

--- proc-macros/crates/foo/src/main.rs
+++ proc-macros/crates/foo/src/main.rs
@@ -1,29 +1,13 @@
 #![allow(unused)]
 
-use macros::{as_is, MyProc};
+use macros::MyProc;
 
-#[derive(MyProc, Default, Debug)]
+#[derive(MyProc)]
 struct Foo {
     #[my_proc]
     name: String,
 }
 
-#[as_is(hi => "foo")]
-fn says_hello() {
-    println!("says_hello()");
-}
+impl Foo {}
 
-impl Foo {
-    fn new() -> Self {
-        Self::default()
-    }
-}
-
-fn main() {
-    let f = Foo {
-        name: String::from("foo"),
-    };
-    println!("mpd: {}", my_proc_derive());
-    println!("foo: {f:#?}");
-    says_hello();
-}
+fn main() {}
