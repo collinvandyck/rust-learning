1d3947e5f98b0fd447c2ab7755f4a8d8790b7117
Collin Van Dyck <collinvandyck@gmail.com>
2024-04-04 17:09:59 -0400

    wip

--- registry/src/main.rs
+++ registry/src/main.rs
@@ -1,3 +1,40 @@
+#![allow(unused)]
+
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", Reg.bar());
+}
+
+pub struct Reg;
+
+#[derive(Debug)]
+pub struct Location {
+    path: &'static str,
+    key: &'static str,
+}
+
+macro_rules! generate {
+    (
+        $path:expr => {
+            $(
+                ($key:expr, $meth:ident)
+            ),*
+        }
+    ) => {
+        impl Reg {
+        $(
+            pub fn $meth(&self) -> Location {
+                Location {
+                    path: $path, key: $key
+                }
+            }
+        )*
+        }
+    };
+}
+
+generate! {
+    "PATH" => {
+        ("bar", bar),
+        ("foo", foo)
+    }
 }
