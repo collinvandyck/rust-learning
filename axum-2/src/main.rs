5c00cfb71202409d69f940bd6c2c749a0b550c9a
Collin Van Dyck <collinvandyck@gmail.com>
2024-04-24 08:20:06 -0400

    wip

--- axum-2/src/main.rs
+++ axum-2/src/main.rs
@@ -1,7 +1,39 @@
+#![allow(unused)]
+
+use std::{net::SocketAddr, time::Duration};
+
+use anyhow::Result;
+use axum::{routing::get, Router};
 use tracing::info;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     tracing_subscriber::fmt().init();
-    info!("Hello, world!");
+
+    let http = Http::start().await?;
+    tokio::time::sleep(Duration::from_secs(1)).await;
+
+    Ok(())
+}
+
+struct Http {
+    addr: SocketAddr,
+    rx: tokio::sync::mpsc::Receiver<std::result::Result<(), std::io::Error>>,
+}
+
+struct Ctrl {}
+
+impl Http {
+    async fn start() -> Result<Self> {
+        let app = Router::new().route("/", get(|| async { "pong" }));
+        let listener = tokio::net::TcpListener::bind("0.0.0.0:0").await?;
+        let addr = listener.local_addr()?;
+        let (tx, rx) = tokio::sync::mpsc::channel(1);
+        info!(?addr, "Listening on");
+        tokio::spawn(async move {
+            let res = axum::serve(listener, app).await;
+            tx.send(res).await;
+        });
+        Ok(Self { addr, rx })
+    }
 }
