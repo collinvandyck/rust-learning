d3bc8579cba695db65e02941ba60e656d9dcd208
Collin Van Dyck <collinvandyck@gmail.com>
2024-04-24 08:41:48 -0400

    wip

--- axum-2/src/main.rs
+++ axum-2/src/main.rs
@@ -1,24 +1,38 @@
 #![allow(unused)]
 
-use std::{io, net::SocketAddr, sync::Arc, time::Duration};
-
 use anyhow::{Context, Result};
 use axum::{routing::get, Router};
-use tokio::sync::{mpsc, Mutex};
+use std::{io, net::SocketAddr, sync::Arc, time::Duration};
+use tokio::{
+    net::TcpListener,
+    select,
+    sync::{mpsc, Mutex},
+    time::sleep,
+};
+use tracing::error;
 use tracing::info;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt().init();
 
+    info!("Starting http server...");
     let http = Http::start().await?;
-    tokio::time::sleep(Duration::from_secs(1)).await;
+
+    select! {
+        res = http.wait() => {
+            if let Err(err) = res {
+                error!("http failed: {err}");
+            } else {
+                info!("http quit");
+            }
+        }
+    }
 
     Ok(())
 }
 
-type IoResult = std::result::Result<(), io::Error>;
-type IoResultRx = mpsc::Receiver<IoResult>;
+type IoResultRx = mpsc::Receiver<Result<()>>;
 type HttpWait = Arc<tokio::sync::Mutex<IoResultRx>>;
 
 struct Http {
@@ -26,17 +40,37 @@ struct Http {
     rx: HttpWait,
 }
 
-struct Ctrl {}
+struct App {
+    router: Option<Router<()>>,
+}
+
+impl App {
+    fn new() -> Self {
+        let mut app = App { router: None };
+        let router = Router::<()>::new().route("/", get(|| async { "pong" }));
+        app.router.replace(router);
+        app
+    }
+
+    async fn serve(mut self, listener: TcpListener) -> Result<()> {
+        axum::serve(listener, self.router.take().unwrap())
+            .await
+            .context("axum quit")?;
+        Ok(())
+    }
+
+    fn pong(&self) {}
+}
 
 impl Http {
     async fn start() -> Result<Self> {
-        let app = Router::new().route("/", get(|| async { "pong" }));
         let listener = tokio::net::TcpListener::bind("0.0.0.0:0").await?;
         let addr = listener.local_addr()?;
-        let (tx, rx) = tokio::sync::mpsc::channel(1);
         info!(?addr, "Listening on");
+        let app = App::new();
+        let (tx, rx) = tokio::sync::mpsc::channel(1);
         tokio::spawn(async move {
-            let res = axum::serve(listener, app).await;
+            let res = app.serve(listener).await;
             tx.send(res).await;
         });
         Ok(Self {
@@ -45,6 +79,7 @@ impl Http {
         })
     }
 
+    /// returns a future that will produce a result when the axum server quits.
     async fn wait(&self) -> Result<()> {
         self.rx
             .lock()
