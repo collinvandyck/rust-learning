dc1e1e0d1b5f40b1a454b0108e070ac79f9584d7
Collin Van Dyck <collinvandyck@gmail.com>
2024-04-28 16:01:51 -0400

    wip

--- wasm-game-of-life/src/lib.rs
+++ wasm-game-of-life/src/lib.rs
@@ -1,5 +1,6 @@
-mod utils;
+#![allow(unused)]
 
+mod utils;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -18,4 +19,33 @@ pub fn greet(name: &str) {
 pub enum Cell {
     Dead = 0,
     Alive = 1,
+}
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+impl Universe {
+    fn get_index(&self, row: u32, col: u32) -> usize {
+        (row * self.width + col) as usize
+    }
+
+    fn live_neighbor_count(&self, row: u32, col: u32) -> u8 {
+        let mut count = 0;
+        for dr in [self.height - 1, 0, 1].iter().cloned() {
+            for dc in [self.width - 1, 0, 1].iter().cloned() {
+                if dr == 0 && dc == 0 {
+                    continue;
+                }
+                let nr = (row + dr) % self.height;
+                let nc = (col + dc) % self.width;
+                let idx = self.get_index(nr, nc);
+                count += self.cells[idx] as u8;
+            }
+        }
+        count
+    }
 }
