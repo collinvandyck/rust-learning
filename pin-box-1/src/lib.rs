b15a5b401346047d5c1789d5f4b804a1bea7ae9e
Collin Van Dyck <collinvandyck@gmail.com>
2024-07-21 22:07:07 -0400

    Finish with the pin/box tutorial.

--- pin-box-1/src/lib.rs
+++ pin-box-1/src/lib.rs
@@ -1,5 +1,8 @@
 #![allow(unused)]
 
+//! See: https://developerlife.com/2024/07/16/pin-box-dynamic-duo/
+//!
+
 use crossterm::style::Stylize;
 
 /// given a pointer `$p` it prints:
@@ -34,6 +37,8 @@ mod tests {
     use crossterm::style::Stylize;
     use serial_test::serial;
 
+    use crate::assert_three_equal;
+
     #[test]
     #[serial]
     fn print_ptr_addr_size() {
@@ -58,5 +63,147 @@ mod tests {
             x.to_string().blue().underlined(),
             x_size.to_string().magenta().italic().on_black()
         );
+
+        let x_addr_3 = print_ptr_addr_size!(&x);
+        println!(
+            "x: {}, x_addr_3: {}",
+            x.to_string().blue().underlined(),
+            x_addr_3.red().italic().on_black()
+        );
+    }
+
+    #[test]
+    #[serial]
+    fn move_a_box() {
+        let b_1 = Box::new(255u8);
+        let b_1_addr = print_ptr_addr_size!(b_1.as_ref()); // pointee (heap)
+        let b_1_ptr_addr = print_ptr_addr_size!(&b_1); // pointer (stack)
+        println!(
+            "1. {}: {}, {} (pointee, heap): {}, {} (ptr, stack): {}",
+            "b_1".green(),
+            b_1.to_string().blue().underlined(),
+            "b_1_addr".green(),
+            b_1_addr.clone().magenta().italic().on_black(),
+            "b_1_ptr_addr".green(),
+            b_1_ptr_addr.clone().magenta().italic().on_black()
+        );
+
+        let b_2 = b_1;
+        let b_2_addr = print_ptr_addr_size!(b_2.as_ref()); // pointee (heap)
+        let b_2_ptr_addr = print_ptr_addr_size!(&b_2); // pointer (stack)
+        println!(
+            "2. {}: {}, {} (pointee, heap): {}, {} (ptr, stack): {}",
+            "b_2".green(),
+            b_2.to_string().blue().underlined(),
+            "b_2_addr".green(),
+            b_2_addr.clone().magenta().italic().on_black(),
+            "b_2_ptr_addr".green(),
+            b_2_ptr_addr.clone().magenta().italic().on_black()
+        );
+
+        // the heap memory allocation does not change/move
+        assert_eq!(b_1_addr, b_2_addr);
+        // the stack address does change (boxes aka pointers have move)
+        assert_ne!(b_1_ptr_addr, b_2_ptr_addr);
+    }
+
+    #[test]
+    #[serial]
+    fn swap_box_contents() {
+        let mut b_1 = Box::new(100u8);
+        let mut b_2 = Box::new(200u8);
+
+        let og_b1_addr = print_ptr_addr_size!(b_1.as_ref());
+        let og_b2_addr = print_ptr_addr_size!(b_2.as_ref());
+
+        assert_eq!(*b_1, 100u8);
+        assert_eq!(*b_2, 200u8);
+
+        println!(
+            "1. {}: {}, {} (pointee, heap): {}, {} (ptr, stack): {}",
+            "b_1".green(),
+            b_1.to_string().blue().underlined(),
+            "b_1_addr".green(),
+            og_b1_addr.clone().red().italic().on_black(),
+            "b_1_ptr_addr".green(),
+            print_ptr_addr_size!(&b_1)
+                .clone()
+                .magenta()
+                .italic()
+                .on_black()
+        );
+        println!(
+            "2. {}: {}, {} (pointee, heap): {}, {} (ptr, stack): {}",
+            "b_2".green(),
+            b_2.to_string().blue().underlined(),
+            "b_2_addr".green(),
+            og_b2_addr.clone().red().italic().on_black(),
+            "b_2_ptr_addr".green(),
+            print_ptr_addr_size!(&b_2)
+                .clone()
+                .magenta()
+                .italic()
+                .on_black()
+        );
+
+        std::mem::swap(&mut b_1, &mut b_2);
+        println!("{}", "Swapped b_1 and b_2".cyan().underlined());
+
+        let new_b1_addr = print_ptr_addr_size!(b_1.as_ref());
+        let new_b2_addr = print_ptr_addr_size!(b_2.as_ref());
+
+        assert_eq!(*b_1, 200u8);
+        assert_eq!(*b_2, 100u8);
+        assert_eq!(og_b1_addr, new_b2_addr);
+        assert_eq!(og_b2_addr, new_b1_addr);
+
+        println!(
+            "3. {}: {}, {} (pointee, heap): {}, {} (ptr, stack): {}",
+            "b_1".green(),
+            b_1.to_string().blue().underlined(),
+            "b_1_addr".green(),
+            new_b1_addr.clone().red().italic().on_black(),
+            "b_1_ptr_addr".green(),
+            print_ptr_addr_size!(&b_1)
+                .clone()
+                .magenta()
+                .italic()
+                .on_black()
+        );
+        println!(
+            "4. {}: {}, {} (pointee, heap): {}, {} (ptr, stack): {}",
+            "b_2".green(),
+            b_2.to_string().blue().underlined(),
+            "b_2_addr".green(),
+            new_b2_addr.clone().red().italic().on_black(),
+            "b_2_ptr_addr".green(),
+            print_ptr_addr_size!(&b_2)
+                .clone()
+                .magenta()
+                .italic()
+                .on_black()
+        );
+    }
+
+    #[test]
+    #[serial]
+    fn box_and_pin_dynamic_duo() {
+        let b_1 = Box::new(100u8);
+        // pointee
+        let b_1_addr = print_ptr_addr_size!(b_1.as_ref());
+        println!("b_1_addr:   {b_1_addr}");
+        let p_b_1 = std::boxed::Box::<u8>::into_pin(b_1);
+        // pinned
+        let p_b_1_addr = print_pin_addr_size!(p_b_1.as_ref());
+        println!("p_b_1_addr: {p_b_1_addr}");
+
+        let b_2 = p_b_1;
+        let b_2_addr = print_pin_addr_size!(b_2);
+        println!("b_2_addr:   {b_2_addr}");
+
+        // pointee has not moved!
+        assert_eq!(b_2_addr, b_1_addr);
+        // pointer has not moved!
+        assert_three_equal(&b_1_addr, &p_b_1_addr, &b_2_addr);
     }
 }
